303e321c022e95f46d5bc779f9630305c5cbf2b4
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-19 23:19:55 -0700

    adding desc

--- Section-1.1/exercise1-06.scm
+++ Section-1.1/exercise1-06.scm
@@ -1,6 +1,32 @@
 ;;
 ;; Execise 1.6
 ;;
+;; Alyssa P. Hacker doesn't see why "if" needs to be provded as a special form. "Why can't I just 
+;; define it as an ordinary procedure in terms of "cond"?" she asks. Alyssa's friend Eva Lu Ator
+;; claims this can indeed be done, and she defines a new version of "if":
+;;
+;; (define (new-if predicate then-clause else-clause)
+;;  (cond (predicate then-clause)
+;;        (else else-clause)))
+;;
+;; Eva demonstrates the program for Alyssa:
+;;
+;; (new-if (= 2 3) 0 5)
+;; ==> 5
+;; 
+;; (new-if (= 1 1) 0 5)
+;; ==> 0
+;;
+;; Delighted, Alyssa uses "new-if" to rewrite the square-root program:
+;;
+;; (define (sqrt-iter guess x)
+;;  (new-if (good-enough? guess x)
+;;           guess
+;;           (sqrt-iter (improve guess x)
+;;                       x)))
+;;
+;; What happens when Alyssa attempts to use this to compute square roots? Explain.
+;;
 
 ;;
 ;; Define the new "if" predicate as follows:
