8d870a71c176949fdfd082861fd384aadec43497
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-19 22:56:38 -0700

    formatting

--- Section-1.1/exercise1-04.scm
+++ Section-1.1/exercise1-04.scm
@@ -1,8 +1,8 @@
 ;;
 ;; Exercise 1.4
 ;;
-;; Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this 
-;; observation to describe the behavior of the following procedure:
+;; Observe that our model of evaluation allows for combinations whose operators are compound expressions. 
+;; Use this observation to describe the behavior of the following procedure:
 ;;
 ;; (define (a-plus-abs-b a b)
 ;;  ((if (> b 0) + -) a b))
