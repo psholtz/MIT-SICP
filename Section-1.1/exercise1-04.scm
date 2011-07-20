8f749b9a9b10fd9c27962f1998d64a820f672e14
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-19 22:56:12 -0700

    adding desc

--- Section-1.1/exercise1-04.scm
+++ Section-1.1/exercise1-04.scm
@@ -1,6 +1,12 @@
 ;;
 ;; Exercise 1.4
 ;;
+;; Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this 
+;; observation to describe the behavior of the following procedure:
+;;
+;; (define (a-plus-abs-b a b)
+;;  ((if (> b 0) + -) a b))
+;;
 
 ;; Procedure adds absolute value of b to a. 
 (define (a-plus-abs-b a b)
