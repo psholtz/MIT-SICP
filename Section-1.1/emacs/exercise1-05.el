1e485697546c976474e4c383d79da456c807cd16
psholtz <paul.sholtz@gmail.com>
2011-07-21 16:28:32 -0700

    updating formatting

--- Section-1.1/emacs/exercise1-05.el
+++ Section-1.1/emacs/exercise1-05.el
@@ -1,6 +1,26 @@
 ;;
 ;; Exercise 1.5
 ;;
+;; Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is using 
+;; applicative-order evaluation or normal-order evaluation. He defines the following two procedures:
+;;
+;; (define (p) (p))
+;;
+;; (define (test x y)
+;;  (if (= x 0)
+;;       0
+;;       y))
+;;
+;; Then he evaluates the expression:
+;;
+;; (test 0 (p))
+;;
+;; What behavior will Ben observe with an interpreter that uses applicative-order evaluation? What 
+;; behavior will he observe with an interpreter that uses normal-order evaluation? Explain your answer.
+;; (Assume that the evaluation rule for the special form "if" is the same whether the interpreter
+;; is using normal or applicative order: the predicate expression is evaluated first, and the result
+;; determines whether to evaluate the consequent or the alternative expression).
+;;
 
 ;;
 ;; Define Ben Bitdiddle's two procedures:
--- Section-1.1/emacs/exercise1-08a.el
+++ Section-1.1/emacs/exercise1-08a.el
@@ -1,7 +1,16 @@
 ;;
 ;; Exercise 1.8
 ;;
-;; Implement a procedure to calculate cube roots.
+;; Newton's method for cube roots is based on the fact that if y is 
+;; an approximation to the cube root of x, then a better approximation 
+;; is given by the value
+;;
+;;       (x/y^2) + 2y 
+;;       ------------ 
+;;            3 
+;;
+;; Use this formula to implement a cube-root procedure analogous to 
+;; the square-root procedure.
 ;;
 
 ;; define the "cube" form 
