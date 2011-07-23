0fa781c581c9d1e901966fc54c25eb3ae4d46b6d
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-23 01:05:42 -0700

    adding format

--- Section-1.2/mit-scheme/exercise1-09.scm
+++ Section-1.2/mit-scheme/exercise1-09.scm
@@ -1,6 +1,10 @@
 ;;
 ;; Exercise 1.9
 ;;
+;; Each of the following two procedures defines a method for adding two positive integers in terms
+;; of the procedures `inc`, which increments its argument by 1, and `dec`, which decrements its 
+;; argument by 1.
+;;
 
 ;; 
 ;; Supporting procedures 
