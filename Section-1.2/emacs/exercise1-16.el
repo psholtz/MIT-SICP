350df26e57ed52e7c989dadb2709d6ad982f3618
psholtz <paul.sholtz@gmail.com>
2011-07-25 11:06:24 -0700

    fixing bug

--- Section-1.2/emacs/exercise1-16.el
+++ Section-1.2/emacs/exercise1-16.el
@@ -16,7 +16,7 @@
 
 (defun fast-expt-iter (b n a)
   (cond ((= n 0) a)
-	((even? n) (square (fast-expt-iter b (/ n 2) a)))
+	((even? n) (fast-expt-iter (square b) (/ n 2) a))
 	(t
 	 (fast-expt-iter b (- n 1) (* a b)))))
 
