5c0808f0d68b3555f535b8b8a13cce96629aefde
psholtz <paul.sholtz@gmail.com>
2011-07-25 09:06:53 -0700

    updating exercise

--- Section-1.2/mit-scheme/exercise1-10.scm
+++ Section-1.2/mit-scheme/exercise1-10.scm
@@ -1,6 +1,36 @@
 ;;
 ;; Exercise 1.10
 ;;
+;; The following procedure computs a mathematical function called Ackerman's function:
+;;
+;; (define (A x y)
+;;  (cond ((= y 0) 0)
+;;        ((= x 0) (* 2 y))
+;;        ((= y 1) 2)
+;;        (else  (A (- x 1)
+;;                  (A x (- y 1)))))
+;;
+;; What are the values of the following expressions?
+;;
+;; (A 1 10)
+;; 
+;; (A 2 4)
+;;
+;; (A 3 3)
+;;
+;; Consider the following procedures, where A is the procedure defined above:
+;;
+;; (define (f n) (A 0 n))
+;;
+;; (define (g n) (A 1 n))
+;;
+;; (define (h n) (A 2 n))
+;;
+;; (define (k n) (* 5 n n))
+;;
+;; Give consise mathematical definitions for the functions computed by the procedures
+;; f, g and h for positive integer values of n. For example, (k n) computes 5n^2.
+;;
 
 ;;
 ;; Ackerman function
