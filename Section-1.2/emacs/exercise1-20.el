701cd859606df9be4ed183fb38e32481060dda10
psholtz <paul.sholtz@gmail.com>
2011-07-26 15:34:57 -0700

    updating answer

--- Section-1.2/emacs/exercise1-20.el
+++ Section-1.2/emacs/exercise1-20.el
@@ -8,3 +8,11 @@
 ;; "remainder" operations that are actually performed. How many remainder operations are actually performed
 ;; in the normal-order evaluation of "(gcd 206 40)"? In the applicative-order evaluation?
 ;;
+
+;;
+;; First define the "gcd" procedure:
+;;
+(defun gcd (a b)
+  (if (= b 0)
+      a
+    (gcd b (% a b))))
