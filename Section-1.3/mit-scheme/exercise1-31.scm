86ad76db5b946e9f0b1d820e5308b982ceea46b8
psholtz <paul.sholtz@gmail.com>
2011-07-27 15:33:37 -0700

    updating

--- Section-1.3/mit-scheme/exercise1-31.scm
+++ Section-1.3/mit-scheme/exercise1-31.scm
@@ -134,3 +134,24 @@
 	result
 	(iter (next a) (* result (term a)))))
   (iter a 1))
+
+;;
+;; We can try testing this by re-running our factorial expressions:
+;;
+(factorial 0)
+;; --> 1
+
+(factorial 1)
+;; --> 1
+
+(factorial 2)
+;; --> 2
+
+(factorial 3)
+;; --> 6
+
+(factorial 4)
+;; --> 24
+
+(factorial 5)
+;; --> 120
