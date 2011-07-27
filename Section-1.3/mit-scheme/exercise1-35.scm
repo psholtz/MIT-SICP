fc86569c33bc40101b8fd5dbf5ec40f8824d9e0e
psholtz <paul.sholtz@gmail.com>
2011-07-27 11:57:13 -0700

    updating answer

--- Section-1.3/mit-scheme/exercise1-35.scm
+++ Section-1.3/mit-scheme/exercise1-35.scm
@@ -1,9 +1,13 @@
 ;;
 ;; Exercise 1.35
 ;;
-;; Show that the golden ratio phi (section 1.2.2) is a fixed point of the transformation x -> 1 + 1/x, and use this fact to compute phi by means of the fixed-point procedure.
+;; Show that the golden ratio phi (section 1.2.2) is a fixed point of the transformation x -> 1 + 1/x, 
+;; and use this fact to compute phi by means of the fixed-point procedure.
 ;;
 
+;;
+;; Define the "fixed-point" procedure:
+;;
 (define (fixed-point f first-guess)
   (define tolerance 0.00001)
   (define (close-enough? v1 v2)
@@ -15,4 +19,10 @@
 	  (try next))))
   (try first-guess))
 
+;;
+;; Define "phi" as a fixed point:
+;;
 (define phi (fixed-point (lambda (y) (+ 1 (/ 1 y))) 1.0))
+
+phi
+;; --> 1.61803279
