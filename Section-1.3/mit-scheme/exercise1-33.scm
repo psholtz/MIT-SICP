200dcdf0174e1bcb4d0278fb955de39084f2f2a6
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-27 22:13:36 -0700

    fixing bugs

--- Section-1.3/mit-scheme/exercise1-33.scm
+++ Section-1.3/mit-scheme/exercise1-33.scm
@@ -19,7 +19,7 @@
 (define (filtered-accumulate combiner null-value term a next b predicate)
   (if (> a b)
       null-value
-      (combiner (if (predicate (term a))
+      (combiner (if (predicate a)
 		    (term a)
 		    null-value)
 		(filtered-accumulate combiner null-value term (next a) next b predicate))))
