1c042e95b363117e051d91d191609b097a96bf52
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-28 22:05:01 -0700

    updating 42

--- Section-1.3/mit-scheme/exercise1-42.scm
+++ Section-1.3/mit-scheme/exercise1-42.scm
@@ -1,4 +1,21 @@
+;;
+;; Exercise 1.42
+;;
+;; Let f and g be two one-argument functions. The "composition" f after g is defined to be the function 
+;; x --> f(g(x)). Define a procedure "compose" that implements composition. For example, if "inc is a 
+;; procedure that adds 1 to its argument.
+;;
+;; ((compose square inc) 6)
+;; --> 49
 
-
+;; 
+;; Definition of the "compose" procedure:
+;;
 (define (compose f g)
   (lambda (x) (f (g x))))
+
+;;
+;; Run the sample given in text:
+;;
+((compose square inc) )
+;; --> 49
