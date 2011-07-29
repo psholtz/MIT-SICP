65f5e51209cb262255e405a160733745492afc2b
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-28 22:24:42 -0700

    updating

--- Section-1.3/mit-scheme/exercise1-37.scm
+++ Section-1.3/mit-scheme/exercise1-37.scm
@@ -1,10 +1,23 @@
-(define (cont-fact n d k)
-  (define (iter i t)
-    (if (= i 0)
-	t
-	(iter (- i 1) (/ (n i) (+ (d i) t)))))
-  (iter k 0))
+;; 
+;; Recursive:
+;;
+(define (cont-frac n d k)
+  (define (cont-frac-iter i)
+    (cond ((= i k) (/ (n i) (d i)))
+	  (else
+	   (/ (n i) (+ (d i) (cont-frac-iter (+ i 1)))))))
+  (cont-frac-iter 1))
 
-(cont-fact (lambda (i) 1.0)
-	   (lambda (i) 1.0)
-	   20)
+;;
+;; Iterative:
+;;
+(define (cont-frac n d k)
+  (define (term i t)
+    (/ (n i) (+ (d i) t)))
+  (define (cont-frac-iter i t)
+    (cond ((= i 1) (term i t))
+	  (else
+	   (cont-frac-iter (- i 1) (term i t)))))
+  (cont-frac-iter k 0))
+
+    
