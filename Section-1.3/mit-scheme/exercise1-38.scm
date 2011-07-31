c72b40ae8bb171b26dbaf478ad1f62e0158ffce4
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-30 21:03:25 -0700

    updating

--- Section-1.3/mit-scheme/exercise1-38.scm
+++ Section-1.3/mit-scheme/exercise1-38.scm
@@ -1,12 +1,70 @@
 ;;
 ;; Exericse 1.38
 ;;
-(define (d i)
-  (let ((r (remainder (- i 2) 3))
-	(n (floor (/ i 3))))
-    (cond ((= r 0) (* (+ n 1.0) 2.0))
-	  (else 1.0))))
 
-(define (n i) 1.0)
+;; 
+;; Let's use the iterative definition of the "cont-frac" procedure, 
+;; as this is going to be more efficient.
+;;
+(define (cont-frac n d k)
+  (define (term i t)
+    (/ (n i) (+ (d i) t)))
+  (define (cont-frac-iter i t)
+    (cond ((= i 1) (term i t))
+	  (else
+	   (cont-frac-iter (- i 1) (term i t)))))
+  (cont-frac-iter k 0))
 
-(cont-frac n d 10)
+;; 
+;; Define the numerator and denominator procedures:
+;;
+(define n (lambda (x) 1.0))
+(define d (lambda (x)
+	    (let ((r (remainder (- x 2) 3))
+		  (n (floor (/ (- x 2) 3))))
+	      (cond ((= r 0) (* (+ n 1.0) 2.0))
+		    (else 1.0)))))
+
+;;
+;; The continued fraction is supposed to give an expression for e-2. 
+;; Let's add 2 to the continued fraction to get e, and let's carry 
+;; out the continued fraction for 10 iterations, to see how close 
+;; we get to e:
+;;
+(+ 2 (cont-frac n d 10))
+;; --> 2.7182817182817183
+
+;;
+;; Looks pretty good!
+;;
+
+;;
+;; Let's implement the same "test" procedure as in Exercise 1.37, 
+;; to see how many iterations we have to carry out to get an 
+;; approximation that is accurate to 4 decimal places:
+;;
+(define (test)
+  (define tolerance 0.0001)  ;; 4 digits of accuracy
+  (define e 2.718281828)
+  (define target e)
+  (define (test-iter k)
+    (let ((value (+ 2 (cont-frac n d k))))
+      (if (< (abs (- value target)) tolerance)
+	  k
+	  (test-iter (+ k 1)))))
+  (test-iter 1))
+
+(test)
+;; --> 7
+
+;;
+;; That is, 7 iterations are required to get an approximation to e that
+;; is accurate to 4 decimal places.
+;;
+(define e 2.718281828)
+
+(+ 2 (cont-frac n d 7))
+;; --> 2.7183098591549295
+
+(abs (- e (+ 2 (cont-frac n d 7))))
+;; --> 2.803115e-5
