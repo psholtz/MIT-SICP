f98001fac84bd4ec085f3444448c2e654b7ee965
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-30 20:34:25 -0700

    adding desc

--- Section-1.3/mit-scheme/exercise1-37.scm
+++ Section-1.3/mit-scheme/exercise1-37.scm
@@ -1,7 +1,47 @@
 ;;
 ;; Exercise 1.37
 ;;
-;; [xx]
+;; An infinite continued fraction is an expression of the form:
+;;
+;; f =                   N1
+;;      ----------------------------------
+;;        D1     +           N2
+;;                   ------------------
+;;                    D2  +    N3
+;;                          --------
+;;                          D3 + ... 
+;;
+;; As an example, one can show that the infinite continued fraction expansion with the 
+;; N(i) and the D(i) all equal to 1 produces 1/phi, where phi is the golden ration 
+;; (descibed in section 1.2.2). One way to approximate an infinite continued fraction 
+;; is to truncate the expansion after a given number of terms. Such a truncation -- 
+;; a so-called "k-term finite continued fraction" -- has the form:
+;;
+;;                  N1
+;; --------------------------------
+;;  D1      +         N2
+;;             ----------------
+;;               ...   + NK
+;;                       -- 
+;;                       DK
+;; 
+;; Suppose that n and d are procedures of one argument (the term index i) that return
+;; the N(i) and D(i) of the terms of the continued fraction. Define a procedure cont-frac
+;; such that evaluating (cont-frac n d k) computes the value of the k-term finite 
+;; continued fraction. Check your procedure by approximating 1/phi using
+;;
+;; (cont-frac (lambda (i) 1.0)
+;;            (lambda (i) 1.0)
+;;            k)
+;;
+;; for successive values of k. How large must you make k in order to get an approximation
+;; that is accurate to 4 decimal places?
+;;
+
+;;
+;; (b) If your cont-frac procedure generates a recursive process, write one that generates an 
+;; iterative process. if it generates an iterative process, write one that generates a recursive
+;; process.
 ;;
 
 ;; 
