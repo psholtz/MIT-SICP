fe1f7b8821956323edd2726371e632e8cf854716
Paul Sholtz <paul.sholtz@gmail.com>
2011-07-30 21:57:33 -0700

    adding desc

--- Section-1.3/mit-scheme/exercise1-38.scm
+++ Section-1.3/mit-scheme/exercise1-38.scm
@@ -1,6 +1,11 @@
 ;;
 ;; Exericse 1.38
 ;;
+;; In 1737, the Swiss mathematician Leonhard Euler published a memoir De Fractionibus
+;; Continuis, which included a continued fraction expansion for e-2, where e is the 
+;; base of the natural logarithm. In this fraction, the N(i) are all 1, and the D(i)
+;; are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, ... Write a program that uses your
+;; "cont-frac" procedure from exercise 1.37 to approximate e, based on Euler's expansion.
 
 ;; 
 ;; Let's use the iterative definition of the "cont-frac" procedure, 
