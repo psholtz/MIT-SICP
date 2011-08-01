28aa5c33f1446de808d8c12564b047ff08197c7d
psholtz <paul.sholtz@gmail.com>
2011-08-01 15:44:23 -0700

    updating

--- Section-1.3/mit-scheme/exercise1-44.scm
+++ Section-1.3/mit-scheme/exercise1-44.scm
@@ -60,19 +60,108 @@
 ;;
 ;; This is what we expect, since (/ (+ 0.0 3.0 0.0) 3.0) evaluates to 1.0
 ;;
+(= (/ (+ 0 3 0) 3.0) ((smooth impulse) 0))
 
+;;
+;; Let's examine the call graph for ((smooth impulse) 0) to see how the 
+;; interpreter arrives at this answer:
+;;
+-----------------------------
+((smooth impulse) 0)
+----------------------------- 
+((lambda (x)
+   (average 
+    (impulse (- x dx))
+    (impulse x)
+    (impulse (+ x dx)))) 0)
+------------------------------ 
+(average (impulse (- 0 dx))
+	 (impulse 0)
+	 (impulse (+ 0 dx)))
+------------------------------  
+(average 0 3 0)
+------------------------------ 
+1.0
+------------------------------ 
+
+;;
+;; Now let's try to compose the "smooth" function with itself,  
+;; and see what we get for an answer:
+;;
 ((smooth (smooth impulse)) 0)
 ;; ==> 1.0
 
+;;
+;; This may seem a bit surprising. If ((smooth impulse) 0) is equal 
+;; to 1, we might naively assume that ((smooth (smooth impulse)) 0) 
+;; should be equal to 1/3 (i.e., cuts the value of (smooth impulse)
+;; at zero down again by 1/3). 
+;;
+;; To see why this is not the case, let's expand the call graph 
+;; for ((smooth (smooth impulse)) 0):
+;;
+----------------------------------
+((smooth (smooth impulse)) 0)
+---------------------------------- 
+((smooth
+  (lambda (x)
+    (average 
+     (impulse (- x dx))
+     (impulse x)
+     (impulse (+ x dx))))) 0)
+----------------------------------
+((lambda (y)
+   (average 
+    ((lambda (x)
+       (average
+	(impulse (- x dx))
+	(impulse x)
+	(impulse (+ x dx))))
+     (- y dx))
+    ((lambda (x)
+       (average 
+	(impulse (- x dx))
+	(impulse x)
+	(impulse (+ x dx))))
+     y)
+    ((lambda (x)
+       (average
+	(impulse (- x dx))
+	(impulse x)
+	(impulse (+ x dx))))
+     (+ y dx)))) 0)
+---------------------------------- 
+(average
+ ((lambda (x)
+    (average
+     (impulse (- x dx))
+     (impulse x)
+     (impulse (+ x dx))))
+  (- 0 dx))
+ ((lambda (x)
+    (average
+     (impulse (- x dx))
+     (impulse x)
+     (impulse (+ x dx))))
+  0)
+ ((lambda (x)
+    (average
+     (impulse (- x
+
+;;
+;; Let's look at a couple more n-fold recursive calls of "smooth":
+;;
 ((smooth (smooth (smooth impulse))) 0)
-;; ==> 
+;; ==> 0.777777777777777
 
 ((smooth (smooth (smooth (smooth impulse)))) 0)
-;; ==>
+;; ==> 0.703703703703703
 
-(= (/ (+ 0 1 0) 3.0) ((smooth impulse) 0))
+((smooth (smooth (smooth (smooth (smooth impulse))))) 0)
+;; ==> 0.629629629629629
 
-
+;;
+;; Now let's implement the "repeated" abstraction as described in the text.
 ;;
 ;; Give definition of "repeated" procedure:
 ;;
@@ -94,25 +183,3 @@
 ;;
 (define (smooth-n-times f n)
   ((repeated smooth n) f))
-
-
-;;
-;; +++++ 
-;; HERE IS THE CODE
-;;
-((smooth (smooth impulse)) 0)
-
-((smooth
-  (lambda (x)
-    (average
-     (impulse (- x dx))
-     (impulse x)
-     (impulse (+ x dx))))) 0)
-
-((lambda (y)
-   ((average
-     (lambda (x)
-       (average 
-	(impulse (- x dx))
-	(impulse x)
-	(impulse (+ x dx)))
