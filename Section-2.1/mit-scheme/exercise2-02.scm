4186970f8922f13e19c4fbdabbc64b2f4848e3fd
psholtz <paul.sholtz@gmail.com>
2011-08-03 09:34:53 -0700

    updating

--- Section-2.1/mit-scheme/exercise2-02.scm
+++ Section-2.1/mit-scheme/exercise2-02.scm
@@ -1,6 +1,24 @@
 ;;
 ;; Exercise 2.2
 ;;
+;; Consider the problem of representing line segments in a place. Each segment is represented
+;; as a pair of points: a starting point and an ending point. Define a constructor "make-segment"
+;; and selectors "start-segment" and "end-segment" that define a representation of segments
+;; in terms of points. Furthermore, a point can be represented as a pair of numbers: the 
+;; x-coordinate and the y-coordinate. Accordingly, specify a constructor "make-point" and 
+;; selectors "x-point" and "y-point" that define this representation. Finally, using your 
+;; selectors and constructors, define a procedure "midpoint-segment" that takes a line
+;; segment as argument and returns its midpoint (the point whose coordinates are the average
+;; of the coordinates of the endpoints). To try your procedures, you'll need a way to print points:
+;;
+;; (define (print-point p)
+;;  (newline)
+;;  (display "(")
+;;  (display (x-point p))
+;;  (display ",")
+;;  (display (y-point p))
+;;  (display ")"))
+;;
 
 ;;
 ;; Define the "segment" procedures:
@@ -65,3 +83,18 @@
 (define s (make-segment p1 p2))
 (print-point (midpoint-segment s))
 ;; ==> (0,0)
+
+;;
+;; Looks good so far. Let's run some more, arbitrary unit tests:
+;;
+(define p1 (make-point 1 5))
+(define p2 (make-point 3 -7))
+(define s (make-segment p1 p2))
+(print-point (midpoint-segment s))
+;; ==> (2,-1)
+
+(define p1 (make-point 10 20))
+(define p2 (make-point -2 -6))
+(define s (make-segment p1 p2))
+(print-point (midpoint-segment s))
+;; ==> (4,7)
