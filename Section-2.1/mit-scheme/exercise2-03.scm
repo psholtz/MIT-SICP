47946efd898ac0a219da65b36ed6519496643019
psholtz <paul.sholtz@gmail.com>
2011-08-04 09:45:14 -0700

    Adding

--- Section-2.1/mit-scheme/exercise2-03.scm
+++ Section-2.1/mit-scheme/exercise2-03.scm
@@ -5,7 +5,7 @@
 ;;
 ;; Let's bring in the procedures from Exercise 2.2 that we'll be using here.
 ;;
-;; The point procedures:
+;; First the point procedures:
 ;;
 (define (make-point x y)
   (cons x y))
@@ -13,7 +13,7 @@
 (define (y-point p) (cdr p))
 
 ;;
-;; And the segment procedures:
+;; and also the segment procedures:
 ;;
 (define (make-segment start-point end-point)
   (cons start-point end-point))
@@ -23,49 +23,64 @@
   (cdr s))
 
 ;;
-;; Give two points of the rectangle:
-;; 
-;; (a) upper-left corner; and 
-;; (b) lower-right corner;
-;; 
+;; If we want to calculate the perimeter and area of a rectangle, we'll 
+;; have to be able to calculate the length of a line segment, and also
+;; the distance between two points:
+;;
+(define (distance-points p1 p2)
+  (sqrt (+ (square (- (x-point p1) (x-point p2))) (square (- (y-point p1) (y-point p2))))))
+(define (length-segment s)
+  (distance-points (start-segment s) (end-segment s)))
+
+;;
+;; For our first representation of a rectangle, let's define it using 
+;; two points: (a) the upper-left hand corner, and (b) the lower right-hand
+;; corner. This information should encode everything we need to be able 
+;; to draw and represent the rectangle.
+;;
 (define (make-rect p1 p2)
   (cons p1 p2))
-(define (upper-left-rect r)
+ 
+;;
+;; Next, let's define selectors that give us the 4-points of the rectangle.
+;; These procedures will be "implementation specific" in terms of the
+;; representation we have chosen, but all other procedures we define above
+;; these should be sufficiently abstract to work with any specific way 
+;; of implementing construction of rectangles.
+;;
+;; We avoid defining any of these procedures in terms of the others (even though
+;; we could), to avoid the possibility of (infinite) cycles. 
+;;
+(define (upper-left-corner-rect r)
   (car r))
-(define (upper-right-rect r)
-  (make-point (x-point (lower-right-rect r)) (y-point (upper-left-rect r))))
-(define (lower-left-rect r)
-  (make-point (x-point (upper-left-rect r)) (y-point (lower-right-rect r))))
-(define (lower-right-rect r)
+
+(define (upper-right-corner-rect r)
+  (make-point (x-point (cdr r)) (y-point (car r))))
+
+(define (lower-left-corner-rect r)
+  (make-point (x-point (car r)) (y-point (cdr r))))
+
+(define (lower-right-corner-rect r)
   (cdr r))
 
 ;;
-;; define a distance metrics for the space we are workign w/
+;; Let's defien the "width segment" to the top horizontal segment of the rectangle
 ;;
-(define (distance p1 p2)
-  (sqrt (+ (square (- (x-point p1) (x-point p2))) (square (- (y-point p1) (y-point p2))))))
-
-(define (length segment)
-  (distance (start-segment segment) (end-segment segment)))
-;;
-;; 
 (define (width-segment-rect r)
-  (make-segment (upper-left-rect r) (upper-right-rect r)))
+  (make-segment (upper-left-corner-rect r) (upper-right-corner-rect r)))
 (define (height-segment-rect r)
-  (make-segment (upper-left-rect r) (lower-left-rect r)))
+  (make-segment (upper-left-corner-rect r) (lower-left-corner-rect r)))
 
 ;;
-;; Now define the generalized selectors, and define the "perimeter" and 
-;; "area" procedures in terms of these generalized selectors.
+;; Now let's define the "width" and "height" of the rectangle. 
 ;;
-;; These represent the "abstraction" barriers.
+;; These should be real numbers, not line segments represented by points.
 ;;
 (define (width-rect r)
- 
+  (length (width-segment r)))
 
-  (make-segment (upper-left-rect r) (upper-right-rect r)))
 (define (height-rect r)
-  (make-segment (upper-left-rect r) (lower-left-rect r)))
+  (length (height-segment r)))
 
 ;;
 ;; Now define the "Area" and "perimeter" procedures;
@@ -75,6 +90,10 @@
 (define (perimeter-rect r)
   (+ (* 2 (width-rect r)) (* 2 (height-rect r))))
 
+;;
+;; PUT UNIT TESTS HERE
+;;
+
 
 ;;
 ;; Now define a second representation
@@ -82,7 +101,6 @@
 ;;
 (define (make-rect w h)
   (cons w h))
-(Define 
 
 
   
