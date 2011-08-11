4811ea526fb909cf7cc0da3366a2b043bcaf7f65
psholtz <paul.sholtz@gmail.com>
2011-08-11 16:27:34 -0700

    Adding recitation

--- Recitations-F2007/Recitation-05/mit-scheme/exercise-03.scm
+++ Recitations-F2007/Recitation-05/mit-scheme/exercise-03.scm
@@ -1,11 +1,20 @@
 ;;
-;; Exercise 2
+;; Exercise 3
+;;
+;; Create a data abstraction for points in a place. It should have a constructor, 
+;; "(make-point x y)", which returns a point, and two selectors "(point-x p)" 
+;; and "(point-y p)" which return the x- and y-coordinates.
 ;;
 
-
+;;
+;; Define constructor:
+;;
 (define (make-point x y)
   (cons x y))
 
+;;
+;; Define selectors:
+;;
 (define (point-x p)
   (car p))
 
