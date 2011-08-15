70fa8d4b1f8d9ed9b4c8d3913f9d80889a4e4daa
psholtz <paul.sholtz@gmail.com>
2011-08-15 12:22:22 -0700

    adding exercise

---
+++ Recitations-F2007/Recitation-07/mit-scheme/exercise-01.scm
@@ -0,0 +1,34 @@
+;;
+;; Exercise 1
+;;
+;; Write a function "occurrences" that takes a number and a list and counts the number
+;; of times the number appears in the list. Write two versions -- one that uses "filter"
+;; and one that uses "fold-right". For example:
+;;
+;; (occurrences 1 (list 1 2 1 1 3)) ==> 3
+;;
+
+;;
+;; First let's define "occurrences" in a "naive" way, using simple recursion:
+;;
+(define (occurrences num a)
+  (if (null? a)
+      0
+      (if (= num (car a))
+	  (+ 1 (occurrences num (cdr a)))
+	  (occurrences num (cdr a)))))
+
+;;
+;; Unit tests:
+;;
+(occurrences 1 (list 1 2 1 1 3))
+;; ==> 3
+
+(occurrences 2 (list 1 2 1 1 3))
+;; ==> 1
+
+(occurrences 3 (list 1 2 1 1 3))
+;; ==> 1
+
+(occurrences 4 (list 1 2 1 1 3))
+;; ==> 0
