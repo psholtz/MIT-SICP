9cea645a52e32c26ee15dee2751197cf163e432b
psholtz <paul.sholtz@gmail.com>
2011-08-22 11:25:31 -0700

    adding

---
+++ Recitations-F2007/Recitation-04/mit-scheme/exercise-01.scm
@@ -0,0 +1,22 @@
+;;
+;; Exercise 1
+;;
+;; Give order notation for the following:
+;;
+;; (a) 5n^2 + n
+;; (b) sqrt(n) + n
+;; (c) 3^n * n^2
+;; (d) log(57)n + 1/n^2
+;;
+
+;;
+;; (a) 5n^2 + n
+;;
+
+O(n^2)
+
+;;
+;; (b) sqrt(n) + n
+;;
+
+O(n)
