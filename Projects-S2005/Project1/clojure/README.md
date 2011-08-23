41fc319b96ecdc8d8f4a677843a64403d12e81cc
psholtz <paul.sholtz@gmail.com>
2011-08-23 09:09:58 -0700

    adding

---
+++ Projects-S2005/Project1/clojure/README.md
@@ -0,0 +1,1 @@
+Basebot implemented in Clojure.
---
+++ Projects-S2005/Project1/clojure/basebot.clj
@@ -0,0 +1,42 @@
+;; ++++++++++++++++++++++++ 
+;; PROJECT 1
+;;
+;; Clojure implementation.
+;; ++++++++++++++++++++++++
+(defn square [x] (* x x))
+
+(def gravity 9.8)  ;; in m/s
+(def pi 3.14159)
+
+;; ++++++++++++++++++++++++++++ 
+;; PROBLEM 1
+;;
+;; Simple kinematics equation.
+;; ++++++++++++++++++++++++++++
+(def position
+  (fn [a v u t]
+    (+
+     (* 0.5 a (square t))
+     (* v t)
+     u)))
+
+;;
+;; Run some unit tests
+;;
+(= (position 0 0 0 0) 0)
+(= (position 0 0 20 0) 20)
+(= (position 0 5 10 10) 60)
+(= (position 2 2 2 2) 10)
+(= (position 5 5 5 5) 92.5)
+(= (position 0 -10 0 1) -10)
+
+;; +++++++++++++++++++++++++++++ 
+;; PROBLEM 2
+;;
+;; Implement quadratic formula.
+;; +++++++++++++++++++++++++++++
+(defn root1 [a b c]
+  (let ((d (discriminant a b c)))
+    (if (< d 0)
+      '()
+      (/ (+ (* -1 b) (sqrt d)) (* 2 a)))))
