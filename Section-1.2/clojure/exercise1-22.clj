e17259441b49be0864d466faecbd6c940a98b7e0
psholtz <paul.sholtz@gmail.com>
2011-09-13 13:30:24 -0700

    got the cond syntax in clojure correct

--- Section-1.2/clojure/exercise1-22.clj
+++ Section-1.2/clojure/exercise1-22.clj
@@ -29,8 +29,8 @@
   (= (mod b a) 0))
 
 (defn find-divisor [n test-divisor]
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
+  (cond (> (square test-divisor) n) n
+        (divides? test-divisor n) test-divisor
         :else (find-divisor n (+ test-divisor 1))))
 
 (defn smallest-divisor [n]
