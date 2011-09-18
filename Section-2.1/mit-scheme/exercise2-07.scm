597155ff07ccfb3bd4a16f1c2aa9d2f1595ba77b
Paul Sholtz <paul.sholtz@gmail.com>
2011-09-17 17:04:59 -0700

    updating scheme interval arithemetic

--- Section-2.1/mit-scheme/exercise2-07.scm
+++ Section-2.1/mit-scheme/exercise2-07.scm
@@ -1,9 +1,21 @@
 ;;
 ;; Exercise 2.7
 ;;
+;; Alyssa's program is incomplete because she has not specified the implementation of
+;; the interval abstraction. Here is a definition of the interval constructor:
+;;
+;; (define (make-interval a b) (cons a b))
+;;
+;; Define selectors "upper-bound" and "lower-bound" to complete the implementation.
 ;; 
 
+;;
+;; Constructor:
+;;
 (define (make-interval a b) (cons a b))
 
+;;
+;; Selectors:
+;;
 (define (lower-bound x) (car x))
 (define (upper-bound x) (cdr x))
