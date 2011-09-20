f09aee86887f1017963f60c51c5945fe25d8a255
psholtz <paul.sholtz@gmail.com>
2011-09-20 15:38:49 -0700

    updating

---
+++ Section-1.3/clojure/exercise1-42.clj
@@ -0,0 +1,22 @@
+;;
+;; Exercise 1.42
+;;
+;; Let f and g be two one-argument functions. The "composition" f after g is defined to be the function
+;; x --> f(g(x)). Define a procedure "compose" that implements composition. For example, if "inc is a
+;; procedure that adds 1 to its argument.
+;;
+;; ((compose square inc) 6)
+;; --> 49
+
+;;
+;; Definition of the "compose" procedure:
+;;
+(defn compose [f g]
+  (fn [x] (f (g x))))
+
+;;
+;; Run the sample given in text:
+;;
+(defn square [x] (* x x))
+((compose square inc) 6)
+;; ==> 49
