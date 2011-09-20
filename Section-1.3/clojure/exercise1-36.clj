d9cf3e291b3b5fc95541553bf97f8cff13fcd0d7
psholtz <paul.sholtz@gmail.com>
2011-09-20 09:53:03 -0700

    updating clojure

---
+++ Section-1.3/clojure/exercise1-36.clj
@@ -0,0 +1,119 @@
+;;
+;; Exercise 1.36
+;;
+;; Modify "fixed-point" so that it prints the sequence of approximations it generates, using the "newline"
+;; and "display" primitives shown in exercise 1.22. Then find a solution to x^x = 1000 by finding a fixed
+;; point of x |--> log(1000)/log(x). (Use Scheme's primitive "log" procedure, which computes natural logarithms).
+;; Compare the number of steps this takes with and without average damping. (Note that you cannot start
+;; "fixed-point" with a guess of 1, as this would cause division by log(1)=0).
+;;
+
+;;
+;; Define the modified "fixed-point" procedure:
+;;
+(defn fixed-point [f first-guess]
+  (def tolerance 0.00001)
+  (defn close-enough? [v1 v2]
+    (< (Math/abs (- v1 v2)) tolerance))
+  (defn try-guess [guess number]
+    (let [next (f guess)]
+      (print "Guess number ")
+      (print number)
+      (print ": ")
+      (println guess)
+      (if (close-enough? guess next)
+        next
+        (try-guess next (+ number 1)))))
+  (try-guess first-guess 1))
+
+;;
+;; Use it to calculate phi:
+;;
+(def phi (fixed-point (fn [y] (+ 1.0 (/ 1.0 y))) 1.0))
+;; ==> Guess number 1: 1.0
+;; ==> Guess number 2: 2.0
+;; ==> Guess number 3: 1.5
+;; ==> Guess number 4: 1.6666666666666665
+;; ==> Guess number 5: 1.6
+;; ==> Guess number 6: 1.625
+;; ==> Guess number 7: 1.6153846153846154
+;; ==> Guess number 8: 1.619047619047619
+;; ==> Guess number 9: 1.6176470588235294
+;; ==> Guess number 10: 1.6181818181818182
+;; ==> Guess number 11: 1.6179775280898876
+;; ==> Guess number 12: 1.6180555555555556
+;; ==> Guess number 13: 1.6180257510729614
+;; ==> Guess number 14: 1.6180371352785146
+;; ==> #'user/phi
+
+;;
+;; Use it to calculate x as given in the text:
+;;
+(def x (fixed-point (fn [y] (/ (Math/log 1000) (Math/log y))) 2.0))
+;; ==> Guess number 1: 2.0
+;; ==> Guess number 2: 9.965784284662087
+;; ==> Guess number 3: 3.004472209841214
+;; ==> Guess number 4: 6.279195757507157
+;; ==> Guess number 5: 3.759850702401539
+;; ==> Guess number 6: 5.215843784925895
+;; ==> Guess number 7: 4.182207192401397
+;; ==> Guess number 8: 4.8277650983445906
+;; ==> Guess number 9: 4.387593384662677
+;; ==> Guess number 10: 4.671250085763899
+;; ==> Guess number 11: 4.481403616895052
+;; ==> Guess number 12: 4.6053657460929
+;; ==> Guess number 13: 4.5230849678718865
+;; ==> Guess number 14: 4.577114682047341
+;; ==> Guess number 15: 4.541382480151454
+;; ==> Guess number 16: 4.564903245230833
+;; ==> Guess number 17: 4.549372679303342
+;; ==> Guess number 18: 4.559606491913287
+;; ==> Guess number 19: 4.552853875788271
+;; ==> Guess number 20: 4.557305529748263
+;; ==> Guess number 21: 4.554369064436181
+;; ==> Guess number 22: 4.556305311532999
+;; ==> Guess number 23: 4.555028263573554
+;; ==> Guess number 24: 4.555870396702851
+;; ==> Guess number 25: 4.555315001192079
+;; ==> Guess number 26: 4.5556812635433275
+;; ==> Guess number 27: 4.555439715736846
+;; ==> Guess number 28: 4.555599009998291
+;; ==> Guess number 29: 4.555493957531389
+;; ==> Guess number 30: 4.555563237292884
+;; ==> Guess number 31: 4.555517548417651
+;; ==> Guess number 32: 4.555547679306398
+;; ==> Guess number 33: 4.555527808516254
+;; ==> Guess number 34: 4.555540912917957
+;; ==> #'user/x
+
+;;
+;; It can be shown that 4.55554 ^ 4.55554 = 1000.0, so it looks like we have a
+;; good approximation to the correct answer.
+;;
+
+;;
+;; This implementation did not use average damping.
+;;
+;; Without avereage damping, it requires 34 iterations to arrive at an answer.
+;;
+
+;;
+;; Let's modify our definition of x so that it uses average damping:
+;;
+(defn average [x y] (/ (+ x y) 2.0))
+
+(def x (fixed-point (fn [y] (average y (/ (Math/log 1000) (Math/log y)))) 2.0))
+;; ==> Guess number 1: 2.0
+;; ==> Guess number 2: 5.9828921423310435
+;; ==> Guess number 3: 4.922168721308343
+;; ==> Guess number 4: 4.628224318195455
+;; ==> Guess number 5: 4.568346513136242
+;; ==> Guess number 6: 4.5577305909237005
+;; ==> Guess number 7: 4.555909809045131
+;; ==> Guess number 8: 4.555599411610624
+;; ==> Guess number 9: 4.5555465521473675
+;; ==> #'user/x
+
+;;
+;; With average damping it only requires 9 iterations to arrive at the same answer.
+;; 
