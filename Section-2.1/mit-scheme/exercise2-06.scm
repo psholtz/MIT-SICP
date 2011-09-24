6dc1899e0691f1179496e241a577cee640fce8f5
psholtz <paul.sholtz@gmail.com>
2011-09-23 17:48:55 -0700

    updating the church numeral scheme problem

--- Section-2.1/mit-scheme/exercise2-06.scm
+++ Section-2.1/mit-scheme/exercise2-06.scm
@@ -1,9 +1,24 @@
 ;;
 ;; Exercise 2.6
 ;;
+;; In case representing pairs as procedures wasn't mind-boggling enough, consider that, in 
+;; a language that can manipulate procedures, we can get by without numbers (at least insofar
+;; as nonnegative integers are concerned) by implementing 0 and the opereation of adding 1 as
+;;
+;; (define zero (lambda (f) (lambda (x) x)))
+;; (define (add-1 n) (lambda (f) (lambda (x) (f ((n f) x))))) 
 ;; 
+;; This representation is known as "Church numerals" after its inventor, Alonzo Church, the 
+;; logician who invented the lambda calculus.
+;;
+;; Define "one" and "two" directly (not in terms of "zero" and "add-1") (Hint: Use substitution
+;; to evaluate (add-1 zero). Give a direct definition of the addition procedure + (not in terms
+;; of repeated application of "add-1").
+;;
  
-
+;;
+;; First define the procedures "zero" and "add-1":
+;;
 (define zero (lambda (f) (lambda (x) x)))
 
 (define (add-1 n)
