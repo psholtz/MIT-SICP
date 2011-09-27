88bdefcf738914a7e5b6b2d39172f7feecaf9f9b
psholtz <paul.sholtz@gmail.com>
2011-09-27 10:47:19 -0700

    updating scheme

--- Section-2.2/mit-scheme/exercise2-28.scm
+++ Section-2.2/mit-scheme/exercise2-28.scm
@@ -1,9 +1,21 @@
 ;;
 ;; Exercise 2.28
 ;;
+;; Write a procedure "fringe" that takes as arguments a tree (represented as a list) and 
+;; returns a list whose elements are all the leaves of the tree arranged in left-to-right
+;; order. For example, 
+;; 
+;; (define x (list (list 1 2) (list 3 4)))
+;;
+;; (fringe x)
+;; (1 2 3 4)
+;;
+;; (fringe (list x x))
+;; (1 2 3 4 1 2 3 4)
+;;
 
 ;;
-;; this is a solution, but there may be better ones:
+;; Define the "fringe" procedure:
 ;;
 (define (fringe x)
   (define (fringe-iter y)
@@ -14,5 +26,15 @@
 	  (else '())))
   (fringe-iter x))
 
+;;
+;; Run some unit tests:
+;;
+(define x (list (list 1 2) (list 3 4)))
+
+(fringe x)
+;; ==> (1 2 3 4)
+
+(fringe (list x x))
+;; ==> (1 2 3 4 1 2 3 4)
 
 
