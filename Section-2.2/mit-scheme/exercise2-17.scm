24fdb6f53a9e9bf2a26c334983e5d21c32447062
psholtz <paul.sholtz@gmail.com>
2011-09-27 10:37:41 -0700

    updating scheme

--- Section-2.2/mit-scheme/exercise2-17.scm
+++ Section-2.2/mit-scheme/exercise2-17.scm
@@ -1,12 +1,24 @@
 ;;
 ;; Exercise 2.17
 ;;
+;; Define a procedure "last-pair" that returns the list that contains only the last 
+;; element of a given (nonempty) list:
+;;
+;; (last-pair (list 23 72 149 34))
+;;
 
+;;
+;; Define the "last-pair" procedure:
+;;
 (define last-pair
   (lambda (x)
-    (cond ((null? x) '())
+    (cond ((null? x) '())  ;; <-- should never hit this case, but "stop" just in case
 	  ((null? (cdr x)) (list (car x)))
 	  (else
 	   (last-pair (cdr x))))))
 
-      
+;;
+;; Run a unit test:
+;;
+(last-pair (list 23 72 149 34))
+;; ==> (34)
