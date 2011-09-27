b7ff96cadd5d9c7abbb353d596a52b5128de6b7b
psholtz <paul.sholtz@gmail.com>
2011-09-27 10:05:16 -0700

    adding scheme exercise

--- Section-2.2/mit-scheme/exercise2-25.scm
+++ Section-2.2/mit-scheme/exercise2-25.scm
@@ -1,17 +1,35 @@
 ;;
 ;; Exercise 2.25
 ;;
+;; Give combinations of "car"s and "cdr"s that will pick 7 from each of the following lists:
+;;
 
-
+;;
+;; Question (a)
+;;
 (define a (list 1 3 (list 5 7) 9))
 
-
+;;
+;; Solution (a)
+;;
 (car (cdr (car (cdr (cdr a)))))
 
+;;
+;; Question (b)
+;;
 (define b (list (list 7)))
 
+;;
+;; Solution (b)
+;;
 (car (car b))
 
+;;
+;; Question (c)
+;;
 (define c (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
 
+;;
+;; Solution (c)
+;
 (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr c))))))))))))
