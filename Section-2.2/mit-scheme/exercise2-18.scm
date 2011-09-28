7572e82614864da46546e670b0c708222b61623f
psholtz <paul.sholtz@gmail.com>
2011-09-27 18:11:50 -0700

    updating scheme

--- Section-2.2/mit-scheme/exercise2-18.scm
+++ Section-2.2/mit-scheme/exercise2-18.scm
@@ -1,6 +1,16 @@
+;;
+;; Exercise 2.18
+;;
+;; Define a procedure "reverse" that takes a list as argument and returns a list of the 
+;; same elements in reverse order:
+;;
+;; (reverse (list 1 4 9 16 25))
+;; ==> (25 16 9 4 1)
+;;
 
-
-
+;;
+;; Implement the reverse procedure:
+;;
 (define (reverse original)
   (if (null? original)
       '()
@@ -12,3 +22,20 @@
 	new-copy
 	(reverse-iter (cdr original) new-copy))))
 
+;;
+;; Run a unit test:
+;;
+(reverse (list 1 4 9 16 25))
+;; ==> (25 16 9 4 1)
+
+;;
+;; Let's try a few more unit tests:
+;;
+(reverse '())
+;; ==> ()
+(reverse (list 1))
+;; ==> (1)
+(reverse (list 1 2))
+;; ==> (2 1)
+(reverse (list 1 2 3))
+;; ==> (3 2 1)
