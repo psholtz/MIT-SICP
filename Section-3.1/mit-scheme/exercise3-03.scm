a38a8c1af483ec3a2742110959aab0aee48c4990
psholtz <paul.sholtz@gmail.com>
2011-10-06 09:58:14 -0700

    updating scheme

---
+++ Section-3.1/mit-scheme/exercise3-03.scm
@@ -0,0 +1,58 @@
+;;
+;; Exercise 3.3
+;;
+;; Modify the "make-account" procedure so that it creates password-protected accounts.
+;; That is, "make-account" should take a symbol as an additional argument, as in 
+;;
+;; (define acc (make-account 100 'secret-password))
+;;
+;; The resulting account object should procefss a request only if it is accompanied by
+;; the password with which the account was created, and should otherwise return a 
+;; complaint:
+;;
+;; ((acc 'secret-password 'withdraw) 40)
+;; 60
+;;
+;; ((acc 'some-other-password 'deposit) 50)
+;; "Incorrect password")
+;;
+
+;;
+;; Define the procedure:
+;;
+(define (make-account pwd balance)
+  (let ((password pwd))
+    (define (withdraw amount)
+      (if (>= balance amount)
+	  (begin (set! balance (- balance amount))
+		 balance)
+	  (display "Insufficient funds")))
+    (define (deposit amount)
+      (set! balance (+ balance amount))
+      balance)
+    (define (dispatch pwd m)
+      (cond ((eq? pwd password)
+	     (cond ((eq? m 'withdraw) withdraw)
+		   ((eq? m 'deposit) deposit)
+		   (else
+		    (error "Unknown request -- ACCOUNT" m))))
+	    (else
+	     (lambda (x) (display "Incorrect password")))))
+    dispatch))
+
+;;
+;; Run the unit tests:
+;;
+(define account (make-account 'secret 1000))
+
+((account 'secret 'withdraw) 100)
+;; ==> 900
+
+((account 'wrong 'withdraw) 200)
+;; ==> "Incorrect password"
+
+((account 'secret 'withdraw) 10000)
+;; ==> "Insufficient funds"
+
+((account 'secret 'deposit) 300)
+;; ==> 1200
