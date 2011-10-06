99bd36440b9874d76cb0c38d2ef075ec46dfcb50
psholtz <paul.sholtz@gmail.com>
2011-10-06 09:29:45 -0700

    updating scheme

--- Section-3.1/exercise3-01.scm
+++ Section-3.1/exercise3-01.scm
@@ -1,6 +1,18 @@
 ;;
 ;; Exercise 3.1
 ;;
+;; An "accumulator" is a procedure that is called repeatedly with a single numeric argument
+;; and accumulates its argument into a sum. Each time it is called, it returns the currently
+;; accumulated sum. Write a procedure "make-accumulator" that generates accumulators, each
+;; maintaining an independent sum. The input to "make-accumulator" should specify the value
+;; of the sum; for example
+;;
+;; (define A (make-accumulator 5))
+;; (A 10)
+;; ==> 15
+;; (A 10)
+;; ==> 25
+;;
 
 (define (make-accumulator balance)
   (lambda (amount)
