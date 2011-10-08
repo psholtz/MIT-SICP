0369450add7826ffb2c455d3392658be759ffb68
psholtz <paul.sholtz@gmail.com>
2011-10-07 17:10:22 -0700

    updating clojure

--- Section-1.2/clojure/exercise1-28b.clj
+++ Section-1.2/clojure/exercise1-28b.clj
@@ -83,8 +83,8 @@
 (defn statistics [number times]
   (defn display-statistics []
     (println)
-    (print "Statistics for the prime ")
-    (println number)
+    (println "Statistics for the prime ")
+    ;;(println number) ;; dont display number, routine runs faster
     (print "(")
     (def value (gather-n-statistics number times true))
     (println ")")
@@ -117,3 +117,97 @@
 (def billion (fast-expt 10 9))
 (def ten-billion (* 10 billion))
 (def hundred-billion (* 100 billion))
+
+;;
+;; Let's collect some statistics for primes near 1 billion:
+;;
+(statistics (+ billion 7) 10)
+;; ==> (1 1 1 1 0 1 1 1 1 1)
+;; ==> Mean: 0.9 milliseconds
+;; ==> Std Dev: 0.31622776601683794
+
+(statistics (+ billion 9) 10)
+;; ==> (0 0 0 1 0 2 0 1 3 0)
+;; ==> Mean: 0.7 milliseconds
+;; ==> Std Dev: 1.0593499054713802
+
+(statistics (+ billion 21) 10)
+;; ==> (1 0 0 0 0 0 0 0 0 0)
+;; ==> Mean: 0.1 milliseconds
+;; ==> Std Dev: 0.31622776601683794
+
+;;
+;; Collect statistics for primes around 10 billion:
+;;
+(statistics (+ ten-billion 19) 10)
+;; ==> (0 0 1 0 1 0 1 1 0 1)
+;; ==> Mean: 0.5 milliseconds
+;; ==> Std Dev: 0.5270462766947299
+
+(statistics (+ ten-billion 33) 10)
+;; ==> (1 0 1 0 1 0 1 0 0 1)
+;; ==> Mean: 0.5 milliseconds
+;; ==> Std Dev: 0.5270462766947299
+
+(statistics (+ ten-billion 61) 10)
+;; ==> (0 0 1 0 0 1 0 0 0 1)
+;; ==> Mean: 0.3 milliseconds
+;; ==> Std Dev: 0.4830458915396479
+
+;;
+;; Collect statistics for primes around 100 billion:
+;;
+(statistics (+ hundred-billion 3) 10)
+;; ==> (0 0 0 0 0 0 0 0 1 0)
+;; ==> Mean: 0.1 milliseconds
+;; ==> Std Dev: 0.31622776601683794
+
+(statistics (+ hundred-billion 19) 10)
+;; ==> (1 0 3 1 0 0 1 0 0 0)
+;; ==> Mean: 0.6 milliseconds
+;; ==> Std Dev: 0.9660917830792959
+
+(statistics (+ hundred-billion 57) 10)
+;; ==> (0 0 1 0 0 1 0 0 1 0)
+;; ==> Mean: 0.3 milliseconds
+;; ==>Std Dev: 0.4830458915396479
+
+;;
+;; Collect statistics for primes around a google:
+;;
+(statistics (+ google 267) 10)
+;; ==> (26 5 6 6 28 11 7 6 6 7)
+;; ==> Mean: 10.8 milliseconds
+;; ==> Std Dev: 8.702490065109718
+
+(statistics (+ google 949) 10)
+;; ==> (46 5 6 6 10 5 5 5 6 7)
+;; ==> Mean: 10.1 milliseconds
+;; ==> Std Dev: 12.705641791477253
+
+(statistics (+ google 1243) 10)
+;; ==> (6 17 6 6 5 5 5 7 6 5)
+;; ==> Mean: 6.8 milliseconds
+;; ==>Std Dev: 3.6453928305312844
+
+;;
+;; Collect statistics for primes around a google-squared:
+;;
+(statistics (+ google-squared 357) 10)
+;; ==> (34 24 21 23 22 21 24 22 24 22)
+;; ==> Mean: 23.7 milliseconds
+;; ==> Std Dev: 3.802046232695699
+
+(statistics (+ google-squared 627) 10)
+;; ==> (24 32 21 23 30 22 22 24 22 23)
+;; ==> Mean: 24.3 milliseconds
+;; ==> Std Dev: 3.683295625749672
+
+(statistics (+ google-squared 799) 10)
+;; ==> (23 23 24 22 23 22 26 23 23 22)
+;; ==> Mean: 23.1 milliseconds
+;; ==> Std Dev: 1.1972189997378646
+
+;;
+;; These are GOOD performance numbers.. better (substantially) than the MIT-Scheme.
+;;
