818746763689082cc1c0f7017dfbff486e7878c0
psholtz <paul.sholtz@gmail.com>
2011-11-14 13:03:30 -0800

    updating answer

--- Section-2.2/mit-scheme/exercise2-19.scm
+++ Section-2.2/mit-scheme/exercise2-19.scm
@@ -2,6 +2,33 @@
 ;; Exercise 2.19
 ;;
 
+;;
+;; For reference, the "original" definitions of "cc" and "first-denomination" are given below:
+;;
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (= kinds-of-coins 0)) 0)
+	(else
+	 (+ (cc amount
+		(- kinds-of-coins 1))
+	    (cc (- amount 
+		   (first-denomination kinds-of-coins))
+		kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+	((= kinds-of-coins 2) 5)
+	((= kinds-of-coins 3) 10)
+	((= kinds-of-coins 4) 25)
+	((= kinds-of-coins 5) 50)))
+
+;;
+;; We will be re-defining these procedures below.
+;;
+
 (define us-coins (list 50 25 10 5 1))
 (define uk-coins (list 100 50 20 10 5 2 1 0.5))
 
@@ -12,7 +39,7 @@
 
 (define (cc amount coin-values)
   (cond ((= amount 0) 1)
-	((or (< amount 0) (no-more? coin-values) ) 0)
+	((or (< amount 0) (no-more? coin-values)) 0)
 	(else
 	 (+ (cc amount
 		(except-first-denomination coin-values))
@@ -21,10 +48,10 @@
 		coin-values)))))
 
 (define (first-denomination coin-values)
-  '())
+  (car coin-values))
 
 (define (except-first-denomination coin-values) 
-  '())
+  (cdr coin-values))
 
 (define (no-more? coin-values)
-  '())
+  (null? coin-values))
