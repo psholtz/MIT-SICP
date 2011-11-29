06edc32cfcc839f2ffe8f43fc358f701a97d37c1
psholtz <paul.sholtz@gmail.com>
2011-11-29 10:10:57 -0800

    updating scheme

--- Section-2.2/mit-scheme/exercise2-23.scm
+++ Section-2.2/mit-scheme/exercise2-23.scm
@@ -1,7 +1,23 @@
 ;;
 ;; Exercise 2.23
 ;;
+;; The procedure "for-each" is similar to "map". It takes as arguments a procedure and a list 
+;; of elements. However, rather than forming a list of the results, "for-each" just applies the 
+;; procedure to each of the elements in turn, from left to right. The values returned by applying
+;; the procedure to the elements are not used at all -- "for-each" is used with procedures that 
+;; perfom an action, such as printing. For example:
+;;
+;; (for-each (lambda (x) (newline) (display x))
+;;           (list 57 321 88))
+;;
+;; 57
+;; 321
+;; 88
+;;
 
+;;
+;; Define the procedure:
+;;
 (define (for-each func items)
   (define (for-each-iter work answer)
     (if (null? work)
@@ -11,3 +27,12 @@
 			       (list (func (car work)))))))
   (for-each-iter items '()))
 
+;;
+;; Try out the new procedure:
+;;
+(for-each (lambda (x) (newline) (display x))
+	  (list 57 321 88))
+;; ==> 57 
+;; ==> 321
+;; ==> 88
+;; ==> '()
