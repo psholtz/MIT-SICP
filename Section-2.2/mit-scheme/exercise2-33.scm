7dde4ac4382b63257e9f93b4404072c0d2fd2cfb
psholtz <paul.sholtz@gmail.com>
2011-11-30 14:41:02 -0800

    updating answer

--- Section-2.2/mit-scheme/exercise2-33.scm
+++ Section-2.2/mit-scheme/exercise2-33.scm
@@ -45,7 +45,21 @@
 ;; ==> (1 2 3 4 5 6)
 
 ;;
-;; (c) Next define the custom version of "length":
+;; (c) Next define the custom version of "length".
+;;
+;; This was the tricky one (for me). The best way to think about it is to expand how we want 
+;; accumulate to operate, using an "abstract" "op" procedure, and then define the procedure
+;; in terms of the behavior we want to model:
+;;
+;; (length (list 1 2 3))
+;; (op 1 (op 2 (op 3 0)))
+;; (op 1 (op 2 1))
+;; (op 1 2)
+;; (op 3)
+;;
+;; We see that at each step (in the substitution), we want to "answer" to be one greater 
+;; than the second argument to "op". Clearly, then, the procedure "op" that we desire
+;; can be defined simply in terms of (lambda (x y) (+ 1 y)):
 ;;
 (define (length sequence)
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
