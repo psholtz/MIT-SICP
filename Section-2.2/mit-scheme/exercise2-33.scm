d9c5ef06b437688ccc36b7e22cc962d9dfeda531
psholtz <paul.sholtz@gmail.com>
2011-11-30 14:42:40 -0800

    updating answer

--- Section-2.2/mit-scheme/exercise2-33.scm
+++ Section-2.2/mit-scheme/exercise2-33.scm
@@ -4,9 +4,12 @@
 ;; Fill in the missing expressions to complete the following definitions of some basic
 ;; list-manipulation operations as accumulations:
 ;;
-;; "map"
-;; "append"
-;; "length"
+;; (define (map p sequence)
+;;  (accumulate (lambda (x y) <??>) nil sequence))
+;; (define (append seq1 seq2)
+;;  (accumulate cons <??> <??>))
+;; (define (length sequence)
+;;  (accumulate <??> 0 sequence))
 ;;
 
 ;;
