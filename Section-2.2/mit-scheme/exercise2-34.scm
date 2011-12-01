c5fa951fd6a8e092da36006d45fb682e8a46b575
psholtz <paul.sholtz@gmail.com>
2011-11-30 18:16:30 -0800

    updating

---
+++ Section-2.2/mit-scheme/exercise2-34.scm
@@ -0,0 +1,9 @@
+;;
+;; Exercise 2.34
+;;
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+		(+ this-coeff (* x higher-terms)))
+	      0
+	      coefficient-sequence))
