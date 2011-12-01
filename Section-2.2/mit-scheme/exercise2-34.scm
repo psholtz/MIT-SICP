9e5282b8209c30746bc92d55e7bcc0b5878cfc23
psholtz <paul.sholtz@gmail.com>
2011-11-30 18:17:30 -0800

    updating horner scheme

--- Section-2.2/mit-scheme/exercise2-34.scm
+++ Section-2.2/mit-scheme/exercise2-34.scm
@@ -2,6 +2,16 @@
 ;; Exercise 2.34
 ;;
 
+;;
+;; Define the "accumulate" procedure:
+;;
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+
+
 (define (horner-eval x coefficient-sequence)
   (accumulate (lambda (this-coeff higher-terms)
 		(+ this-coeff (* x higher-terms)))
