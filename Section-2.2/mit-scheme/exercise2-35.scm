345f2b02cd0ea45d4e675e5d0a5d20541d321c8a
psholtz <paul.sholtz@gmail.com>
2011-12-01 16:08:07 -0800

    updating the exercise

---
+++ Section-2.2/mit-scheme/exercise2-35.scm
@@ -0,0 +1,26 @@
+;;
+;; Exercise 2.35
+;;
+
+;;
+;; The original definition of "count-leaves":
+;;
+(define (count-leaves x)
+  (cond ((null? x) 0)
+	((not (pair? x)) 1)
+	(else
+	 (* (count-leaves (car x))
+	    (count-leaves (cdr x))))))
+
+;;
+;; Definition of accumulate:
+;;
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+
+;;
+;; New definition of "count-leaves":
+;;
