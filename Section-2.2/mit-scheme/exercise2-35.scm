e43709ea87e359eba13cf1bc7f436efc2e184dd0
psholtz <paul.sholtz@gmail.com>
2011-12-05 14:39:59 -0800

    updating

--- Section-2.2/mit-scheme/exercise2-35.scm
+++ Section-2.2/mit-scheme/exercise2-35.scm
@@ -24,3 +24,10 @@
 ;;
 ;; New definition of "count-leaves":
 ;;
+(define (count-leaves t)
+  (accumulate (lambda (x y) (+ x y)) 
+	      0
+	      (map (lambda (x)
+		     (if (pair? x)
+			 (count-leaves x)
+			 1)) t)))
