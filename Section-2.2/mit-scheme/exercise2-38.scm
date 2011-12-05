b7c44d89b05bc1f0874f43a587abf27b58e46215
psholtz <paul.sholtz@gmail.com>
2011-12-05 15:22:25 -0800

    updating scheme

--- Section-2.2/mit-scheme/exercise2-38.scm
+++ Section-2.2/mit-scheme/exercise2-38.scm
@@ -1,6 +1,25 @@
 ;;
 ;; Exercise 2.38
 ;;
+;; The "accumulate" procedure is also known as "fold-right", because it combines the first element of the sequence
+;; with the result of combining all the elements to the right. There is also a "fold-left", which is similar to "fold-right", 
+;; except that it combines elements working in the opposite direction:
+;;
+;; (define (fold-left op initial sequence)
+;;  (define (iter result rest)
+;;   (if (null? rest)
+;;       result
+;;       (iter (op result (car rest))
+;;             (cdr rest))))
+;;   (iter initial sequence))
+;;
+;; What are the values of 
+;; 
+;; (fold-right / 1 (list 1 2 3))
+;; (fold-left / 1 (list 1 2 3))
+;; (fold-right list '() (list 1 2 3))
+;; (fold-left list '() (list 1 2 3))
+;;
 
 ;;
 ;; The "fold-left" procedure as defined in the exercise:
