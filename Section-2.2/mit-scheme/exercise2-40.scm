fa4f278f5dbb061aeba8a815f26218233bb1fc7f
psholtz <paul.sholtz@gmail.com>
2011-12-06 10:51:06 -0800

    updatig

--- Section-2.2/mit-scheme/exercise2-40.scm
+++ Section-2.2/mit-scheme/exercise2-40.scm
@@ -1,6 +1,10 @@
 ;;
 ;; Exercise 2.40
 ;;
+;; Define a procedure "unique-pairs" that, given an integer n, generates the 
+;; sequence of pairs (i,j) with 1 <= j < i <= n. Use "unique-pairs" 
+;; to simplify the definition of "prime-sum-pairs" given above.
+;;
 
 ;;
 ;; First (re-)define the "accumulate" procedure:
