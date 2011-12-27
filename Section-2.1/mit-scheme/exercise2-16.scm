6b78f02f0ffdba852808b07c783475414f941bcc
psholtz <paul.sholtz@gmail.com>
2011-12-27 10:58:40 -0800

    updating scheme

--- Section-2.1/mit-scheme/exercise2-16.scm
+++ Section-2.1/mit-scheme/exercise2-16.scm
@@ -1,6 +1,10 @@
 ;;
 ;; Exercise 2.16
 ;;
+;; Explain, in general, why equivalent algebraic expressions may lead to different answers. 
+;; Can you devise an interval-arithmetic package that does not have this shortcoming, or 
+;; is this task impossible? (Warning: This problem is very difficult).
+;;
 
 ;;
 ;; The first thing I did was look around to see if there are other "interval arithmetic" packages
