10705c77b40fa618d8788e0af6436b77421ceaff
psholtz <paul.sholtz@gmail.com>
2011-12-27 09:27:10 -0800

    adding emacs exercise

--- Section-2.1/emacs/exercise2-14.el
+++ Section-2.1/emacs/exercise2-14.el
@@ -168,3 +168,108 @@
 ;; However, running these intervals through the two parallel resistor calculations
 ;; gives wildly divergent answers:
 ;;
+(par1 x y)
+;; ==> (1.5555 . 14.)
+(par2 x y)
+;; ==> (3.23077 . 6.74074)
+
+;;
+;; The "center" of par1 does not even lie within the bounds of par2!
+;;
+(center (par1 x y))
+;; ==> 7.777777
+(center (par2 x y))
+;; ==> 4.98575
+(> (center (par1 x y)) (upper-bound (par2 x y)))
+;; ==> t
+
+;;
+;; So clearly it's true that the "same" algebraic expression can give rise to "different"
+;; answers uing the interval arithmetic package. However, the answers seem to "converge"
+;; when using intervals where the percentage width is a "small" fraction of the center value.
+;; For these more "practical" types of intervals, the two procedures are not so far apart.
+;;
+
+;;
+;; However, the problem also asks us to calculate A/A for a range of intervals. 
+;;
+;; Let's do this, using the two "extreme" examples: one where the percentage width is a 
+;; tiny fraction of the center value, and one where the percentage width is an egregiously
+;; large fracation of the center value.
+;;
+(setq x (make-center-percent 40000 0.005))
+(setq y (make-center-percent 65000 0.0125))
+
+(div-interval x x)
+;; ==> (0.99005 . 1.01005)
+(div-interval y y)
+;; ==> (0.97531 . 1.02536)
+
+(center (div-interval x x))
+;; ==> 1.00005
+(center (div-interval y y))
+;; ==> 1.0003
+
+(percent (div-interval x x))
+;; ==> 9.999975e-3
+(percent (div-interval y y))
+;; ==> 2.4996e-2
+
+;;
+;; Clearly these values are constrained quite tightly around 1.00, which is what we 
+;; would expect for a "properly functioning" interval arithmetic library.
+;;
+
+;;
+;; Moreover, as 40000 / 65000 = 0.61538 and 65000 / 40000 = 1.625, we would expect
+;; the respective intervals to "multiply out" to something similar as well:
+;;
+(center (div-interval x y))
+;; ==> 0.615519
+(center (div-interval y x))
+;; ==> 1.625152
+
+;;
+;; and indeed, that is the (roughly) result we obtain (to within an excellent tolerance).
+;;
+
+;;
+;; Now let's try the same exercise with the more "widely divergent" intervals:
+;;
+(setq x (make-center-percent 10 0.3))
+(setq y (make-center-percent 10 0.4))
+
+(center (div-interval x x))
+;; ==> 1.1978
+(center (div-interval y y))
+;; ==> 1.38095
+
+(percent (div-interval x x))
+;; ==> 0.550459
+(percent (div-interval y y))
+;; ==> 0.6896551
+
+;;
+;; Let's try to compose the two intervals, in each case we would anticipate getting 1:
+;;
+(center (div-interval x y))
+;; ==> 1.3333333
+(center (div-interval y x))
+;; ==> 1.230769
+
+(percent (div-interval x y))
+;; ==> 0.625
+(percent (div-interval y x))
+;; ==> 0.625
+
+;;
+;; Clearly, in all these cases, the values we obtain are (quite) far from what we 
+;; would expect, were the interval arithmetic library functioning properly.
+;;
+
+;;
+;; The problem statement indicates that "you will get the most insight by using intervals
+;; whose width is a small percentage of the center value." I am not able to reproduce these
+;; results. Rather, the widest divergence appears to occur when the percentage width is 
+;; very large in relation to the center value.
+;;
