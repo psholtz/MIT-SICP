b98ee9dd19e517febc2eb0c748e9974b034712ec
psholtz <paul.sholtz@gmail.com>
2011-12-27 10:58:11 -0800

    updating emacs

--- Section-2.1/emacs/exercise2-16.el
+++ Section-2.1/emacs/exercise2-16.el
@@ -1,4 +1,10 @@
-
+;;
+;; Exercise 2.16
+;;
+;; Explain, in general, why equivalent algebraic expressions may lead to different answers. 
+;; Can you devise an interval-arithmetic package that does not have this shortcoming, or 
+;; is this task impossible? (Warning: This problem is very difficult).
+;;
 
 ;;
 ;; Constructors and Selectors:
