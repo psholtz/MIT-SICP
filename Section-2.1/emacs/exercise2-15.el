13a560a25ebad7a10d6873763cdd6b4c2e7fea9c
psholtz <paul.sholtz@gmail.com>
2011-12-27 10:42:03 -0800

    adding exercise

--- Section-2.1/emacs/exercise2-15.el
+++ Section-2.1/emacs/exercise2-15.el
@@ -1,4 +1,14 @@
 ;;
+;; Exericse 2.15
+;;
+;; Eva Lu Ator, another user, has also noticed the different intervals computed by different but
+;; algebraically equivalent expressions. She says that a formula to compute with intervals using
+;; Alyssa's system will produce tighter error bounds if it can be written in such a form that no
+;; variable that represents an uncertain number is repeated. Thus, she says, "par2" is a better
+;; program for parallel resistances than "par1". Is she right? Why?
+;;
+
+;;
 ;; We observed in the last exercise that the "par2" procedure seemed to produce tighter
 ;; error bounds than the "par1" procedure. Let's go back and look more closely at this:
 ;;
