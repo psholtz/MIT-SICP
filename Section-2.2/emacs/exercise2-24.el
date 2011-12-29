6fa0e4ec61f56e7fbda2f4e1eb5d00e0aa2ef6c5
Paul Sholtz <paul.sholtz@gmail.com>
2011-12-28 19:44:58 -0800

    updating emcas

--- Section-2.2/emacs/exercise2-24.el
+++ Section-2.2/emacs/exercise2-24.el
@@ -1,6 +1,11 @@
 ;;
 ;; Exercise 2.24
 ;;
+;; Suppose we evaluate the expression (list 1 (list 2 (list 3 4))). Give the result printed
+;; by the interpreter, the corresponding box-and-pointer structure, and the interpretation
+;; of this as a tree.
+;;
+
 
 ;;
 ;; Typing in (list 1 (list 2 (list 3 4))) into the interpreter will result in:
