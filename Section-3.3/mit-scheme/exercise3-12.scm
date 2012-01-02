e486e35b9bf3e945dd1d51d9818cd3917890f3bc
psholtz <paul.sholtz@gmail.com>
2012-01-02 09:58:56 -0800

    updating exercise

--- Section-3.3/mit-scheme/exercise3-12.scm
+++ Section-3.3/mit-scheme/exercise3-12.scm
@@ -45,3 +45,20 @@
 ;;
 ;; What are the missing <response>s? Draw box-and-pointer diagrams to explain your answer.
 ;;
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+z
+;; ==> (a b c d)
+(cdr x)
+;; ==> (b)
+
+(define w  (append! x y))
+w
+;; ==> (a b c d)
+(cdr x)
+;; ==> (b c d)
+
+;;
+;; TODO --> draw box and poitner
+;;
