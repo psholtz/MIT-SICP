b643304c0cde9321ec45aa0e2cd2918f45969974
psholtz <paul.sholtz@gmail.com>
2012-01-02 14:14:23 -0800

    updating scheme

---
+++ Section-3.1/exercise3-13.scm
@@ -0,0 +1,52 @@
+;;
+
+;; [working]
+
+;;
+;; If we were to simply evaluate an expression like (define z (list 'a 'b 'c)), 
+;; the corresponding box-and-pointer diagram would look like:
+;;
+       --- ---       --- ---       --- --- 
+z --> | * | * | --> | * | * | --> | * | / |  
+       --- ---       --- ---       --- ---
+        |             |             |
+        v             v             v
+       ---           ---           --- 
+      | a |         | b |         | c |
+       ---           ---           ---
+
+;;
+;; Note that the "cdr" of the last pair is nil, or "/" in the diagram.
+;;
+
+;;
+;; We desire instead the box-and-pointer diagram for (define z (make-cycle (list 'a 'b 'c))), 
+;; where the "make-cycle" procedure is defined as:
+;;
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+         --------------------------------
+        |                                |
+        v                                |
+       --- ---       --- ---       --- ---
+z --> | * | * | --> | * | * | --> | * | * |
+       --- ---       --- ---       --- ---
+        |             |             |
+        v             v             v
+       ---           ---           ---
+      | a |         | b |         | c |
+       ---           ---           ---
+
+;;
+;; What happens if we try to evaluate (last-pair z)? 
+;;
+
+;;
+;; The "last-pair" procedure is defined as:
+;;
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
