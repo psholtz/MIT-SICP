bf638072bed67224204bcba0ea98aef3b4739541
psholtz <paul.sholtz@gmail.com>
2012-01-02 15:11:02 -0800

    updating scheme

--- Section-3.3/mit-scheme/exercise3-13.scm
+++ Section-3.3/mit-scheme/exercise3-13.scm
@@ -1,6 +1,19 @@
 ;;
-
-;; [working]
+;; Exercise 3.13
+;;
+;; Consider the following "make-cycle" procedure, which uses the "last-pair" procedure
+;; defined in Exercise 3.12:
+;;
+;; (define (make-cycle x)
+;;  (set-cdr! (last-pair x) x)
+;;  x)
+;;
+;; Draw a box-and-pointer diagram that shows the structure z created by
+;;
+;; (define z (make-cycle (list 'a 'b 'c)))
+;;
+;; What happens if we try to compute (last-pair z)?
+;;
 
 ;;
 ;; If we were to simply evaluate an expression like (define z (list 'a 'b 'c)), 
