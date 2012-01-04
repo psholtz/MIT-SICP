fa4bae4be6fb43be24932bd96b7778408d9db915
psholtz <paul.sholtz@gmail.com>
2012-01-04 14:02:50 -0800

    updating streams scheme

---
+++ Section-3.5/README.md
@@ -0,0 +1,9 @@
+Section 3.5
+=========== 
+
+Convenient to have the code from Chapters 1, 2 handy: 
+
+<pre>
+(define (expmod base exp m)
+  
+</pre>
---
+++ Section-3.5/README.scm
@@ -0,0 +1,25 @@
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	 (remainder (* base (expmod base (- exp 1) m))
+		    m))))
+
+(define (prime? n)
+  (define (get-random-a)
+    (+ 2 (random (- n 4))))
+  (define (test a)
+    (= (expmod a (- n 1) n) 1))
+  (cond ((= n 2) #t)
+	((= n 3) #t)
+	((= n 4) #f)
+	((= n 5) #t)
+	(else
+	 (and (test (- a 1))
+	      (test (- a 2))
+	      (test (get-random-a))
+	      (test (get-random-a))
+	      (test (get-random-a))))))
