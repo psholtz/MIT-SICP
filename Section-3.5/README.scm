e2aee531afd43ab87b56fe57714bf40a68efc82d
psholtz <paul.sholtz@gmail.com>
2012-01-04 14:55:46 -0800

    updating readme

--- Section-3.5/README.scm
+++ Section-3.5/README.scm
@@ -33,6 +33,26 @@
 ;; ++++++++++++++++++++++
 ;; LIST MANIPULATION CODE 
 ;; ++++++++++++++++++++++ 
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+	    (map proc (cdr items)))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+	((predicate (car sequence))
+	 (cons (car sequence)
+	       (filter predicate (cdr sequence))))
+	(else 
+	 (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+
 
 ;;
 ;; Compare the two summation procedures:
