63fd9da4f673150293de1b77d71beabefa3f488c
psholtz <paul.sholtz@gmail.com>
2012-01-20 15:59:15 -0800

    updating calculus

--- Projects-S2012/Project1/mit-scheme/calculus.scm
+++ Projects-S2012/Project1/mit-scheme/calculus.scm
@@ -125,7 +125,7 @@
 ;; Looks like the approximations are pretty good.
 ;;
 
-;; [working --> do more integrals here]
+;; [WORKING --> do more integrals here]
 
 ;;
 ;; Iterative function definition:
@@ -136,7 +136,7 @@
       (if (>= (+ x dx) x2)
 	  (+ (bitfunc-rect x (+ x dx)) total)
 	  (+ (bitfunc-rect x (+ x dx)) (integrate (+ x dx) total))))
-    (integrate x1 0)))
+    (integrate x1 0.0)))
 
 ;;
 ;; For the unit tests, we can simply verify that the recursive version gives 
@@ -149,27 +149,89 @@
 (= (bitfunc-integral-iter 1000 0 1) (bitfuc-integral-recur 1000 0 1))
 ;; ==> #t
 
-
-;;
-;; Run some unit tests:
-;;
-
-;; [working]
+;; [WORKING --> more definite integrals]
 
 ;; ++++++++++++++++++++++++ 
 ;; Problem 4
 ;; 
 ;; Integrating any function
 ;; ++++++++++++++++++++++++ 
-;; (working --> check the bounds)
+
+;; 
+;; Iterative (i.e., tail-recursive) is more efficient, so let's use that model:
+;;
 (define (integral func num-steps x1 x2)
-  (let ((dx (* 1.0 (/ (- x2 x1) num-stemps))))
+  ;; calculate the area under the functions
+  (define (func-rect x1 x2)
+    (let ((dx (- x2 x1)))
+      (* (func x1) dx)))
+
+  ;; define the recursive process to numerically calculate integral
+  (let ((dx (* 1.0 (/ (- x2 x1) num-steps))))
     (define (integrate x total)
-      (let ((value (func x)))
-	(if (>= x x2)
-	    (+ value total)
-	    (integrate (+ x dx) (+ total value)))))
+      (if (>= (+ x dx) x2)
+	  (+ (func-rect x (+ x dx)) total)
+	  (+ (func-rect x (+ x dx)) (integrate (+ x dx) total))))
     (integrate x1 0.0)))
+
+;;
+;; Define some procedures, and calculate their integrals:
+;;
+(define (square x) (* x x))
+(define (cube x) (* x x))
+
+;;
+;; For reference:
+;;
+;; Int square(x) = (1/3) x^3
+;; Int cube(x) = (1/4) x^4
+;; Int sin(x) = -cos(x)
+;;
+
+;;
+;; Run some unit tests:
+;;
+(integral square 10 0 1)
+;; ==> 0.385
+(integral square 100 0 1)
+;; ==> 0.32835
+(integral square 1000 0 1)
+;; ==> 0.3328335
+(integral square 10000 0 1)
+;; ==> 0.33383335
+
+;;
+;; The answer clearly converges to the proper answer, but it takes 10,000 steps to get 
+;; 3 digits of accuracy. Perhaps we could do better with a different integration algorithm.
+;;
+
+(integral cube 10 0 1)
+;; ==> 0.3025
+(integral cube 100 0 1)
+;; ==> 0.245025
+(integral cube 1000 0 1)
+;; ==> 0.24950025
+(integral cube 10000 0 1)
+;; ==> 0.2500500025
+
+;;
+;; Again, the returned values converge to the expect answer, but the converge is rather slow.
+;;
+(define pi (* 4 (atan 1.0)))
+;; ==> 3.14159...
+
+(integral sin 10 0 pi)
+;; ==> 1.983523...
+(integral sin 100 0 pi)
+;; ==> 1.9998355...
+(integral sin 1000 0 pi)
+;; ==> 1.99999835...
+(integral sin 10000 0 pi)
+;; ==> 1.99999999...
+
+;;
+;; Here the convergence seems to be somewhat better than for the polynomial functions.
+;;
 
 
 ;; +++++++++++++++++++++ 
@@ -177,17 +239,28 @@
 ;;
 ;; Area of a unit circle
 ;; +++++++++++++++++++++ 
-(define (square x) (* x x))
 (define (approx-pi num-steps)
-  (* 
-   4
-   (integral (lambda (x) (sqrt (- 1 (square x)))) num-steps 0 1)))
+  (* 4 (integral (lambda (x) (sqrt (- 1 (square x)))) num-steps 0 1)))
+
+(approx-pi 10)
+;; ==> 3.30451883322...
+(approx-pi 100)
+;; ==> 3.16041703...
+(approx-pi 1000)
+;; ==> 3.143555467...
+(approx-pi 10000)
+;; ==> 3.1417914777...
+
+;;
+;; Again, the response converges to the proper answer, but the convergence seems rather slow.
+;;
 
 ;; ++++++++++++++++++++++++++++++++++++ 
 ;; Problem 6  
 ;; 
 ;; Integrating with pieces of any shape
 ;; ++++++++++++++++++++++++++++++++++++ 
+;; [WORKING --> not quite right yet]
 ;; area under rectangle
 (define (rectangle func x1 x2)
   (let ((dx (- x2 x1)))
@@ -199,7 +272,13 @@
     (* dx (/ (+ (func x1) (func x2)) 2.0))))
 
 (define (integral-with piece func num-steps x1 x2)
-  '())
+  (let ((dx (* 1.0 (/ (- x2 x1) num-steps))))
+    (define (integrate x total)
+      (if (>= (+ x dx) x2)
+	  (+ (piece func x (+ x dx)) total)
+	  (+ (piece func x (+ x dx)) (integrate (+ x dx) total))))
+    (integrate x1 0)))
+
 
 ;; ++++++++++++++++++++++++++ 
 ;; Problem 7
