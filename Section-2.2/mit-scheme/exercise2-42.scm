05257a79d18845f72064270ace080263cf744a22
psholtz <paul.sholtz@gmail.com>
2012-01-26 15:16:19 -0800

    fixing typo in scheme

--- Section-2.2/mit-scheme/exercise2-42.scm
+++ Section-2.2/mit-scheme/exercise2-42.scm
@@ -21,13 +21,13 @@
     (if (= k 0)
 	(list empty-board)
 	(filter
-	 (lambda (positions (safe? k positions))
-	   (flatmap
-	    (lambda (rest-of-queens)
-	      (map (lambda (new-row)
-		     (adjoin-position new-row k rest-of-queens))
-		   (enumerate-interval 1 board-size)))
-	    (queen-cols (- k 1)))))))
+	 (lambda (positions) (safe? k positions))
+	 (flatmap
+	  (lambda (rest-of-queens)
+	    (map (lambda (new-row)
+		   (adjoin-position new-row k rest-of-queens))
+		 (enumerate-interval 1 board-size)))
+	  (queen-cols (- k 1))))))
   (queen-cols board-size))
 
 ;;
