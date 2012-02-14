9d8f6ca55314bc6be1e2f385ce3f526d7d3a0fc3
psholtz <paul.sholtz@gmail.com>
2012-02-13 17:22:31 -0800

    updating clojure documentation

--- Section-1.2/clojure/exercise1-24.clj
+++ Section-1.2/clojure/exercise1-24.clj
@@ -10,22 +10,32 @@
 ;;
 ;; First let's define the code that allows us to check for primes:
 ;;
-(defn square [x] (* x x))
+(defn square
+  {:doc "Return square of the argument"}
+  [x] (* x x))
 
-(defn expmod [base exp m]
+(defn expmod
+  {:doc "Return ((base ^ exp) mod m)"}
+  [base exp m]
   (cond (= exp 0) 1
         (even? exp) (rem (square (expmod base (/ exp 2) m)) m)
         :else (rem (* base (expmod base (- exp 1) m)) m)))
 
-(defn random [n]
+(defn random
+  {:doc "Return random integer, less than n"}
+  [n]
   (Math/floor (rand n)))
 
-(defn fermat-test [n]
+(defn fermat-test
+  {:doc "Run a fermat test for testing primality"}
+  [n]
   (defn try-it [a]
     (= (expmod a n n) a))
   (try-it (+ 1 (random (- n 1)))))
 
-(defn fast-prime? [n times]
+(defn fast-prime?
+  {:doc "Test n for primality, and run the test 'times' times"}
+  [n times]
   (cond (= times 0) true
         (fermat-test n) (fast-prime? n (- times 1))
         :else false))
