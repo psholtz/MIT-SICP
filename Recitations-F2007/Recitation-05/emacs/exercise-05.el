6dfb2a23e80bb58b0b3523ef7f2ae7a0c8263e5b
Paul Sholtz <paul.sholtz@gmail.com>
2012-02-19 19:37:28 -0800

    updating emacs recitaitons

--- Recitations-F2007/Recitation-05/emacs/exercise-05.el
+++ Recitations-F2007/Recitation-05/emacs/exercise-05.el
@@ -27,8 +27,8 @@
 (defun slope (line-segment)
   (let ((start (line-segment-start line-segment))
 	(end (line-segment-end line-segment)))
-    (let ((dx (- (point-x start) (point-x end)))
-	  (dy (- (point-y start) (point-y end))))
+    (let ((dx (* 1.0 (- (point-x start) (point-x end))))
+	  (dy (* 1.0 (- (point-y start) (point-y end)))))
       (if (= dx 0)
 	  '()
 	;; use 1.0 multiplier to make it into decimal
@@ -83,3 +83,159 @@
 ;;
 (= (slope (make-line-segment p1 p3)) (slope (make-line-segment p3 p1)))
 ;; ==> t
+
+(= (slope (make-line-segment p2 p4)) (slope (make-line-segment p4 p2)))
+;; ==> t
+
+(= (slope (make-line-segment p2 p3)) (slope (make-line-segment p3 p2)))
+;; ==> t
+
+;;
+;; The test has to be defined slightly differently for "vertical" (i.e., 
+;; infinite slope) line segments.
+;;
+(and (null (slope (make-line-segment p1 p2)))
+     (null (slope (make-line-segment p2 p1))))
+;; ==> t
+
+;;
+;; Finally, let's test some lines that have differing slopes
+;;
+(setq origin (make-point 0 0))
+(setq q1 (make-point 1 2))
+(setq q2 (make-point 1 3))
+(setq q3 (make-point 2 1))
+(setq q4 (make-point 3 1))
+
+(slope (make-line-segment origin q1))
+;; ==> 2.0
+(slope (make-line-segment origin q2))
+;; ==> 3.0
+(slope (make-line-segment origin q3))
+;; ==> 0.5
+(slope (make-line-segment origin q4))
+;; ==> 0.33333333333
+
+;;
+;; Next we want to determine the y-intercept of the line segment, 
+;; which would give us the "b" in the y=mx+b formulation.
+;;
+(defun y-intercept (line-segment)
+  (let ((p (line-segment-start line-segment))
+	(m (slope line-segment)))
+    (cond ((not (null m))
+	   (let ((x (point-x p))
+		 (y (point-y p)))
+	     (- y (* m x)))))))
+
+;;
+;; Run some unit tests:
+;;
+(y-intercept d1)
+;; ==> nil
+(y-intercept d2)
+;; ==> -1.0
+(y-intercept d3)
+;; ==> nil
+(y-intercept d4)
+;; ==> 1.0
+(y-intercept d5)
+;; ==> 0.0
+(y-intercept d6)
+;; ==> 0.0
+
+;; 
+;; If the lines are parallel, they won't intersect! 
+;;
+;; It will be useful to check for this, so let's define it:
+;;
+(defun parallel? (line-segment-1 line-segment-2)
+  (cond ((and (null (slope line-segment-1)) (null (slope line-segment-2))) t)
+	((and (null (slope line-segment-1)) (not (null (slope line-segment-2)))) '())
+	((and (null (slope line-segment-2)) (not (null (slope line-segment-1)))) '())
+	(t
+	 (if (= (slope line-segment-1) (slope line-segment-2))
+	     t
+	   '()))))
+
+;;
+;; Run the unit tests:
+;;
+(parallel? d1 d1)
+;; ==> nil
+(parallel? d1 d3)
+;; ==> t
+(parallel? d1 d4)
+;; ==> nil
+(parallel? d1 d5)
+;; ==> nil
+(parallel? d1 d6)
+;; ==> nil
+
+(parallel? d2 d3)
+;; ==> nil
+(parallel? d2 d4)
+;; ==> t
+(parallel? d2 d5)
+;; ==> nil
+(parallel? d2 d6)
+;; ==> nil
+
+(parallel? d3 d4)
+;; ==> nil
+(parallel? d3 d5)
+;; ==> nil
+(parallel? d3 d6)
+;; ==> nil
+
+(parallel? d4 d5)
+;; ==> nil
+(parallel? d4 d6)
+;; ==> nil
+
+(parallel? d5 d6)
+;; ==> nil
+
+(parallel? d1 d1)
+;; ==> t
+(parallel? d2 d2)
+;; ==> t
+(parallel? d3 d3)
+;; ==> t
+(parallel? d4 d4)
+;; ==> t
+(parallel? d5 d5)
+;; ==> t
+(parallel? d6 d6)
+;; ==> t
+
+;; 
+;; We're getting close to the final solution.
+;; 
+;; Next let's define a procedure called "intersect" - it will take two 
+;; line segments as arguments, and determine the point at which the 
+;; corresponding lines intersect (if they do intersect), or else indicate 
+;; to the user that the lines are parallel (and hence do not intersect).
+;;
+(defun intersect (line-segment-1 line-segment-2)
+  (if (parallel? line-segment-1 line-segment-2)
+      (display "The lines are parallel!")
+    (let ((m1 (slope line-segment-1))
+	  (m2 (slope line-segment-2))
+	  (b1 (y-intercept line-segment-1))
+	  (b2 (y-intercept line-segment-2)))
+      (cond ((null? m1)
+	     (let ((x (point-x (line-segment-start line-segment-1))))
+	       (make-point x (+ (* m2 x) b2))))
+	    ((null? m2)
+	     (let ((x (point-x (line-segment-start line-segment-2))))
+	       (make-point x (+ (* m1 x) b1))))
+	    (t
+	     (let ((x (/ (- b2 b1) (- m1 m2))))
+	       (make-point x 
+			   (+ (* m1 x) b1))))))))
+
+;;
+;; Run the unit tests:
+;;
+(intersect d1 d1)
