435a266b69c90e1968536389fcbe0e4e553e4247
psholtz <paul.sholtz@gmail.com>
2012-02-21 17:06:17 -0800

    adding exercise

--- Recitations-F2007/Recitation-06/mit-scheme/exercise-02.scm
+++ Recitations-F2007/Recitation-06/mit-scheme/exercise-02.scm
@@ -1,30 +1,6 @@
 ;;
 ;; Exercise 2
 ;;
-
-;; ++++++++++++++++ 
-;; Core Definitions
-;; ++++++++++++++++ 
-(define (make-units C L H)
-  (list C L H))
-(define get-units-C car)
-(define get-units-L cadr)
-(define get-units-H caddr)
-
-(define (make-class number units)
-  (list number units))
-(define get-class-number car)
-(define get-class-units cadr)
-(define (get-class-total-units class)
-  (let ((units (get-class-units class)))
-    (+ 
-     (get-units-C units)
-     (get-units-L units)
-     (get-units-H units))))
-(define (same-class? c1 c2)
-  (= (get-class-number c1) (get-class-number c2)))
-
-;;
 ;; Write a selector that when given a class and a schedule, returns a new schedule
 ;; including the new class
 ;;
--- Recitations-F2007/Recitation-06/mit-scheme/exercise-03.scm
+++ Recitations-F2007/Recitation-06/mit-scheme/exercise-03.scm
@@ -1,30 +1,6 @@
 ;;
 ;; Exercise 3
 ;;
-
-;; ++++++++++++++++ 
-;; Core Definitions
-;; ++++++++++++++++ 
-(define (make-units C L H)
-  (list C L H))
-(define get-units-C car)
-(define get-units-L cadr)
-(define get-units-H caddr)
-
-(define (make-class number units)
-  (list number units))
-(define get-class-number car)
-(define get-class-units cadr)
-(define (get-class-total-units class)
-  (let ((units (get-class-units class)))
-    (+ 
-     (get-units-C units)
-     (get-units-L units)
-     (get-units-H units))))
-(define (same-class? c1 c2)
-  (= (get-class-number c1) (get-class-number c2)))
-
-;;
 ;; Write a selector that takes in a schedule and returns the total number of units in that schedule
 ;;
 (define (total-scheduled-units sched)
