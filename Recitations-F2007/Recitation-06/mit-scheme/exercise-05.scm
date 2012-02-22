0547952d419dbdbb4c5c31bc468c54eaf434f9ba
psholtz <paul.sholtz@gmail.com>
2012-02-22 15:42:23 -0800

    updating exercise 5, scheme

--- Recitations-F2007/Recitation-06/mit-scheme/exercise-05.scm
+++ Recitations-F2007/Recitation-06/mit-scheme/exercise-05.scm
@@ -6,11 +6,34 @@
 ;;
 (define (credit-limit schedule max-credits)
   (define (iter elems)
-    (let ((class (car elems))
-	  (credits (total-scheduled-units elems)))
-      (if (>= credits max-credits)
-	  (iter (drop-class elems (get-class-number class)))
-	  elems)))
+    (if (null? elems)
+	'()
+	(let ((class (car elems))
+	      (credits (total-scheduled-units elems)))
+	  (if (>= credits max-credits)
+	      (iter (drop-class elems (get-class-number class)))
+	      elems))))
   (iter schedule))
+
+;;
+;; Run some unit tests:
+;;
+schedule
+;; ==> ((calc-101 (4 4 4)) (algb-152 (3 3 3)) (diff-201 (3 3 3)))
+(total-scheduled-units schedule)
+;; ==> 30
+
+(credit-limit schedule 40)
+;; ==> ((calc-201 (4 4 4)) (algb-152 (3 3 3)) (diff-201 (3 3 3)))
+(credit-limit schedule 31)
+;; ==> ((calc-201 (4 4 4)) (algb-152 (3 3 3)) (diff-201 (3 3 3)))
+(credit-limit schedule 30)
+;; ==> ((algb-152 (3 3 3)) (diff-201 (3 3 3)))
+(credit-limit schedule 20)
+;; ==> ((algb-152 (3 3 3)) (diff-201 (3 3 3)))
+(credit-limit schedule 5)
+;; ==> ()
+(credit-limit schedule 0)
+;; ==> ()
 
 ;; ** Working --> order of growth 
