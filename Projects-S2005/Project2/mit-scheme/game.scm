bac87ba6590ebb28027f3aaef81e616e1daf6977
psholtz <paul.sholtz@gmail.com>
2012-03-18 10:23:19 -0700

    updating game theory'

--- Projects-S2005/Project2/mit-scheme/game.scm
+++ Projects-S2005/Project2/mit-scheme/game.scm
@@ -5,19 +5,64 @@
 ;; 
 ;; Definition of "extract-entry"
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++ 
+
+;;
+;; The *game-association-list* is defined as follows:
+;;
+(define *game-association-list*
+  (list (list (list "c" "c") (list 3 3))
+	(list (list "c" "d") (list 0 5))
+	(list (list "d" "c") (list 5 0))
+	(list (list "d" "d") (list 1 1))))
+
+;;
+;; We can extract a specific entry in this list by using the "list-ref" procedure. 
+;;
+;; For example:
+;;
+(list-ref *game-association-list* 0)
+;; ==> (("c" "c") (3 3))
+(list-ref *game-association-list* 1)
+;; ==> (("c" "d") (0 5))
+
+;;
+;; and so on. To extract the entry associated with a specific play, we need to extract 
+;; the "car" of the entry, and make sure that both elements of this "car" correspond 
+;; to both elements of the argument play. 
+;;
+;; We define our "extract-entry" procedure as follows:
+;;
 (define (extract-entry play *game-association-list*)
+  ;; 
+  ;; Returns "true" if the play matches the entry:
+  ;;
   (define (compare play entry)
-    (and (string=? (car play) (car (car entry)))
-	 (string=? (cadr play) (cadr (car entry)))))
+    (let ((test (car entry)))
+      (and (string=? (car play) (car test))
+	   (string=? (cadr play) (cadr test)))))
+
   (let
+      ;; 
+      ;; Get references to each entry in the *game-association-list*:
+      ;;
       ((first (list-ref *game-association-list* 0))
        (second (list-ref *game-association-list* 1))
        (third (list-ref *game-association-list* 2))
        (fourth (list-ref *game-association-list* 3)))
-    (cond ((compare play first) first)
-	  ((compare play second) second)
-	  ((compare play third) third)
-	  ((compare play fourth) fourth)
-	  (else '()))))
+   
+    ;; 
+    ;; If we find a match, return that specific entry:
+    ;;
+    (cond 
+     ((compare play first) first)
+     ((compare play second) second)
+     ((compare play third) third)
+     ((compare play fourth) fourth)
+     (else '()))))
+
+;;
+;; We can test our procedure as follows:
+;;
+
 
 
