7983e264262948606a716cef9c65031558c73b14
psholtz <paul.sholtz@gmail.com>
2012-04-24 18:08:28 -0700

    updating clojure

--- Section-2.1/clojure/exercise2-03.clj
+++ Section-2.1/clojure/exercise2-03.clj
@@ -59,10 +59,75 @@
 ;; We want our constructor to signal an error if the user supplies rectangle dimensions
 ;; which are invalid:
 ;;
-(defn make-rectangle [upper-left-x upper-left-y lower-right-x lower-right-y]
+(defn make-rectangle
+  {:doc "Return two point structure representing upper left point, and lower right point of the rectangle."}
+  [upper-left-x upper-left-y lower-right-x lower-right-y]
   (cond (and (> lower-right-x upper-left-x) (> upper-left-y lower-right-y))
          (cons (make-point upper-left-x upper-left-y)
                (list (make-point lower-right-x lower-right-y)))
          :else
          (println "Error: rectangle dimensions are invalid!")))
-        
+
+;;
+;; Selectors
+;;
+(defn upper-left {:doc "Return upper left point of rectangle."} [rect] (first rect))
+
+(defn lower-right {:doc "Return lower right point of rectangle."} [rect] (second rect))
+
+(defn width
+  {:doc "Return width of the rectangle."}
+  [rect]
+  (let [p1 (upper-left rect)
+        p2 (lower-right rect)]
+    (let [p3 (make-point (x-point p2)
+                         (y-point p1))]
+      (length-segment (make-segment p1 p3)))))
+
+(defn height
+  {:doc "Return height of the rectangle."}
+  [rect]
+  (let [p1 (upper-left rect)
+        p2 (lower-right rect)]
+    (let [p3 (make-point
+              (x-point p1)
+              (y-point p2))]
+      (length-segment (make-segment p1 p3)))))
+
+;;
+;; Now define the "perimeter" and "area" procedures:
+;;
+(defn perimeter
+  {:doc "Return perimeter of the rectangle."}
+  [rect]
+  (+ (* 2 (width rect)) (* 2 (height rect))))
+
+(defn area
+  {:doc "Return area of the rectangle."}
+  [rect]
+  (* (width rect) (height rect)))
+
+;;
+;; Note that we can change the representational model for the rectangles, so long as
+;; we do not alter how the "width" and "height" procedures behave.
+;;
+
+;;
+;; Let's run some use cases.
+;;
+;; The following cases should all fail, owing to bad dimensions.
+;;
+(def r1 (make-rectangle 0 0 0 0))
+;; ==>
+(def r1 (make-rectangle 0 1 0 0))
+;; ==>
+(def r1 (make-rectangle 0 0 1 0))
+;; ==>
+(def r1 (make-rectangle 0 0 -1 0))
+;; ==>
+(def r1 (make-rectangle 0 0 0 -1))
+;; ==>
+(def r1 (make-rectangle 0 0 -1 -1))
+;; ==>
+(def r1 (make-rectangle 0 0 1 1))
+;; ==>
