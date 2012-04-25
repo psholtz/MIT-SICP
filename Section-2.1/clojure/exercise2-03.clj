6341eaa84900cb8dbb3663f3a6d09c7244a02c67
psholtz <paul.sholtz@gmail.com>
2012-04-24 17:54:14 -0700

    updating clojure

--- Section-2.1/clojure/exercise2-03.clj
+++ Section-2.1/clojure/exercise2-03.clj
@@ -43,3 +43,26 @@
   (print (y-point p))
   (print ")")
   (println ""))
+
+;;
+;; Lets start by implementing Model (1).
+;;
+;; We will construct the rectangle by specifying:
+;;
+;; (a) upper-left-x
+;; (b) upper-left-y
+;; (c) lower-right-x
+;; (d) lower-right-y
+;;
+;; We create a data structure to hold these two points (upper-left, and lower-right)
+;;
+;; We want our constructor to signal an error if the user supplies rectangle dimensions
+;; which are invalid:
+;;
+(defn make-rectangle [upper-left-x upper-left-y lower-right-x lower-right-y]
+  (cond (and (> lower-right-x upper-left-x) (> upper-left-y lower-right-y))
+         (cons (make-point upper-left-x upper-left-y)
+               (list (make-point lower-right-x lower-right-y)))
+         :else
+         (println "Error: rectangle dimensions are invalid!")))
+        
