0eff08e89d17666da93d4aa3daf3550dc5e9b5d9
psholtz <paul.sholtz@gmail.com>
2012-04-24 17:10:52 -0700

    updating clojure

--- Section-2.1/clojure/exercise2-02.clj
+++ Section-2.1/clojure/exercise2-02.clj
@@ -31,3 +31,28 @@
 
 (defn x-point {:doc "Accessor to get x-coordinate of a point."} [p] (first p))
 (defn y-point {:doc "Accessor to get y-coordinate of a point."} [p] (second p))
+
+;;
+;; Define code for finding the midpoints of segments:
+;;
+(defn average {:doc "Return average of x and y."} [x y] (/ (+ x y) 2.0))
+
+(defn midpoint-segment
+  {:doc "Return the point that is at the midpoint of the argument line segment."}
+  [s]
+  (let [p1 (start-segment s)
+        p2 (end-segment s)]
+    (make-point
+     (average (x-point p1) (x-point p2))
+     (average (y-point p1) (y-point p2)))))
+
+;;
+;; Define the display procedure:
+;;
+(defn print-point [p]
+  (print "(")
+  (print (x-point p))
+  (print ",")
+  (print (y-point p))
+  (print ")")
+  (println ""))
