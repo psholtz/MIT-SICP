b0d3f7940aaf6d30f4ef35b260cb9892d8375c6e
psholtz <paul.sholtz@gmail.com>
2012-04-26 10:22:45 -0700

    updating exercise

--- Recitations-F2007/Recitation-05/clojure/exercise-01.clj
+++ Recitations-F2007/Recitation-05/clojure/exercise-01.clj
@@ -20,5 +20,45 @@
 ;;
 ;; Unlike traditional versions of Lisp, in Clojure lists are not the primary data
 ;; structure. Data structures in Clojure can implement the ISeq interface, and
-;; the "seq?" procedure checks whether an object implements ISeq. When "cons" is
-;; used in Clojure, the [...]
+;; the "seq?" procedure checks whether an object implements ISeq.
+;;
+;; The ISeq interface is defined as follows:
+;;
+;; public interface ISeq extends IPersistentCollection, Sequential {
+;;
+;;   Object first();
+;;
+;;   ISeq next();
+;;
+;;   ISeq more();
+;;
+;;   ISeq cons(Object obj);
+;;
+;; }
+;;
+;; Many classes will extends ASeq, which provides an abstract implementation
+;; of ISeq:
+;;
+;; public abstract class ASeq extends Obj implements ISeq, List, Streamable {
+;;
+;;   public ISeq more() {
+;;     ISeq s = next();
+;;     if ( s == null ) return LazySeq.EMPTY;
+;;     return s; 
+;;   }
+;;
+;;   public ISeq cons(Object o) {
+;;     return new Cons(o,this);
+;;   }
+;;
+;;   //
+;;   // Additional implementation details
+;;   //
+;; }
+;;
+;; Two good examples for seeing how the ISeq interface is implemented are
+;; clojure.lang.StringSeq, and clojure.lang.IteratorSeq.
+;; 
+;; At any rate, the manner in which clojure implements "cons" precludes
+;; answering the question as posed in this question.
+;;
