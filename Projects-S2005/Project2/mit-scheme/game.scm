4a5ab4000c65f96db2af1bdaac4fbfab12f0da95
psholtz <paul.sholtz@gmail.com>
2012-04-26 17:43:13 -0700

    adding game

--- Projects-S2005/Project2/mit-scheme/game.scm
+++ Projects-S2005/Project2/mit-scheme/game.scm
@@ -254,6 +254,30 @@
     (if (> ds cs) "d" "c")))
 
 ;;
+;; For any one particular game play, the code here makes:
+;;
+;;  (1) a "linear" walk down the "other-history" list;
+;;  (2) a second "linear" walk down the "other-history" list; 
+;;
+;; So for each game play, if the "other-history" is of size k, 
+;; the procedure executes in O(2*k) time.
+;; 
+;; In other words, when the history is of length 1, the play 
+;; executes in time 2*1. When the history is of length 2, the 
+;; play executes in time 2*2. When the history is of length k, 
+;; the play executes in time 2*k. 
+;;
+;; We are executing a total of n plays. That means the total 
+;; time to execute all n plays is:
+;;
+;; T(n) = 2 * ( 1 + 2 + ... + n )
+;; T(n) = 2 * n * ( n + 1 ) / 2
+;; T(n) = n * (n+1)
+;;
+;; In O-notation, this procedure will execute in O(n^2) time.
+;;
+
+;;
 ;; Alyssa's new definition of EGALITARIAN is given as:
 ;;
 (define (EGALITARIAN my-history other-history)
@@ -265,7 +289,34 @@
 	   (majority-loop cs (+ 1 ds) (rest-of-plays hist)))))
   (majority-loop 0 0 other-history))
 
-;; [working] --> analysis
+;;
+;; Using this procedure, for any one particular game play, we 
+;; still make a "linear" walk down the length of the "other-history"
+;; list, but for each game play, we only make one linear walk down 
+;; this list, not two. 
+;;
+;;
+;; Hence, for each game play, if the "history" is of size k, the 
+;; procedure executes in O(k) time.
+;;
+;; In other words, we expect the procedure to execute roughly twice
+;; as fast as the previous EGALITARIAN procedure.
+;;
+;; We are executing a total of n plays. That means the total 
+;; time to execute all n plays is:
+;;
+;; T(n) = 1 + 2 + .. + n
+;; T(n) = n * (n+1) / 2
+;;
+;; In O-notation, this procedure will execute in O(n^2) time.
+;;
+;; In other words, in O-notation, this procedure will executes in 
+;; roughly the same order of magnitude time as the previous 
+;; procedure: it scales as n^2, where n is the number of game plays.
+;; However, there is some (considerable) savings in this procedure, 
+;; owing to the fact that each game play executes in roughly 1/2
+;; the time that it took using the first procedure.
+;;
 
 ;;
 ;; As a test, let's implement a "timed-play-loop" procedure that
@@ -273,7 +324,7 @@
 ;; so we can see whether program execution actually performs the 
 ;; way we would predict.
 ;;
-(define (timed-play-loop strat0 strat1)
+(define (timed-play-loop strat0 strat1 times)
 
   ;;
   ;; Play-loop-iter procedure for executing the game play an arbitrary number of times
@@ -292,7 +343,7 @@
   ;; Bracket execution loop for timing purposes
   ;;
   (let ((start (real-time-clock)))
-    (timed-play-loop-iter 0 the-empty-history the-empty-history 1000)
+    (timed-play-loop-iter 0 the-empty-history the-empty-history times)
     (let ((finish (real-time-clock)))
       (display "Timing: ")
       (display (- finish start)))))
