c01086f1e2a58ff30b3d0bbf06ab8add2e477994
psholtz <paul.sholtz@gmail.com>
2012-04-30 18:05:09 -0700

    updating clojure

---
+++ Section-2.1/clojure/exercise2-04.clj
@@ -0,0 +1,68 @@
+;;
+;; Exercise 2.4
+;;
+;; Here is an alternative procedural representation of pairs. For this representation,
+;; verify that (car (cons x y)) yields x for any objects x and y.
+;;
+;; (define (cons x y)
+;;  (lambda (m) (m x y)))
+;;
+;; (define (car z)
+;;  (z (lambda (p q) p)))
+;;
+;; What is the corresponding definition of cdr?
+;;
+(defn cons [x y] (fn [m] (m x y)))
+
+(defn car [z] (z (fn [p q] p)))
+
+;;
+;; We can prove this result by using the evaluation model of evaluation:
+;;
+;; We begin with the expression for (car (cons x y)):
+;;
+(car (cons x y))
+
+;;
+;; Replacing (car z) with its definition in terms of the lambda function:
+;;
+((cons x y) (fn [p q] p))
+
+;;
+;; Expanding (cons x y) in terms of its definition in terms of lambda:
+;;
+((fn [m] (m x y)) (fn [p q] p))
+
+;;
+;; This expression looks formidable, but it yields the answer we are seeking.
+;; Specifically, the left-most subexpression (the operator) is simply an
+;; (anonymous) procedure which takes one argument: that argument itself being
+;; a procedure which takes two arguments.
+;;
+;; The syntax of this expression is therefore consistent.
+;;
+;; Although it deviates slightly from the strict model of substitution evaluation,
+;; it might make things clearer if we made the following definition:
+;;
+(def f (fn [p q] p))
+
+;;
+;; The above expression then can be written as:
+;;
+((fn [m] (m x y)) f)
+
+;;
+;; Which reduces to:
+;;
+(f x y)
+
+x
+
+;;
+;; Which is the result we were seeking to prove.
+;;
+
+;;
+;; The corresponding definition of cdr would be:
+;;
+(defn cdr [z] (z (fn [p q] q)))
