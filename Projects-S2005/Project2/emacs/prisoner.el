a577a950a2bd51571e0dd88f1eacdb35ac44a445
psholtz <paul.sholtz@gmail.com>
2012-05-02 14:41:05 -0700

    Adding emacs version

---
+++ Projects-S2005/Project2/emacs/prisoner.el
@@ -0,0 +1,23 @@
+;; 
+;;  The play-loop procedure takes as its  arguments two prisoner's
+;;  dilemma strategies, and plays an iterated game of approximately
+;;  one hundred rounds.  A strategy is a procedure that takes
+;;  two arguments: a history of the player's previous plays and 
+;;  a history of the other player's previous plays.  The procedure
+;;  returns either a "c" for cooperate or a "d" for defect.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun play-loop (strat0 strat1)
+  (defun play-loop-iter (strat0 strat1 count history0 history1 limit)
+    (cond ((= count limit) (print-out-results history0 history1 limit))
+	  (t
+	   (let ((result0 (strat0 history0 history1))
+		 (result1 (strat1 history1 history0)))
+	     (play-loop-iter strat0 
+			     strat1 
+			     (+ count 1) 
+			     (extend-history result0 history0) 
+			     (extend-history result1 history1)
+			     limit)))))
+  (play-loop-iter strat0 strat1 0 the-empty-history the-empty-history (+ 90 (random 21))))
+
