6a9778a4c0fea5ac960c59513d2dfde34580d11b
psholtz <paul.sholtz@gmail.com>
2012-05-09 11:17:19 -0700

    updating game

--- Projects-S2005/Project2/emacs/game.el
+++ Projects-S2005/Project2/emacs/game.el
@@ -323,3 +323,90 @@
 	  (if (or (string= result1 "c") (string= result2 "c"))
 	      "c"
 	    "d"))))))
+
+;;
+;; Let's run some unit tests, to make sure the procedure works the way we want.
+;; For this game strategy, "our" history does not enter into the calculation. 
+;; We can pad our strategy with a random number "x":
+;;
+(setq temp-my-0 the-empty-history)
+(setq temp-my-1 (list "x"))
+(setq temp-my-2 (list "x" "x"))
+(setq temp-my-3 (list "x" "x" "x"))
+(setq temp-my-4 (list "x" "x" "x" "x"))
+
+;;
+;; Run the actual unit tests:
+;;
+(EYE-FOR-TWO-EYES temp-my-0 the-empty-history)
+;; ==> "c"
+
+(EYE-FOR-TWO-EYES temp-my-1 (list "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-1 (list "d"))
+;; ==> "c"
+
+(EYE-FOR-TWO-EYES temp-my-2 (list "c" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-2 (list "c" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-2 (list "d" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-2 (list "d" "d"))
+;; ==> "d"
+
+(EYE-FOR-TWO-EYES temp-my-3 (list "c" "c" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-3 (list "c" "c" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-3 (list "c" "d" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-3 (list "c" "d" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-3 (list "d" "c" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-3 (list "d" "c" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-3 (list "d" "d" "c"))
+;; ==> "d" 
+(EYE-FOR-TWO-EYES temp-my-3 (list "d" "d" "d"))
+;; ==> "d"
+
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "c" "c" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "c" "c" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "c" "d" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "c" "d" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "d" "c" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "d" "c" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "d" "d" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "c" "d" "d" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "c" "c" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "c" "c" "d"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "c" "d" "c"))
+;; ==> "c"
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "c" "d" "d"))
+;; ==> "c" 
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "d" "c" "c"))
+;; ==> "d"
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "d" "c" "d"))
+;; ==> "d"
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "d" "d" "c"))
+;; ==> "d"
+(EYE-FOR-TWO-EYES temp-my-4 (list "d" "d" "d" "d"))
+;; ==> "d"
+
+;;
+;; Looks like the strategy works as advertised. 
+;;
+;; Let's run the strategy against the other strategies, and compile the results:
+;;
