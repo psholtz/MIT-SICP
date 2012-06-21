82a801d2d2632ca43cadd23f4be3c62a848a997c
psholtz <paul.sholtz@gmail.com>
2012-06-21 09:50:09 -0700

    updating scheme

---
+++ Projects-S2005/Project2/mit-scheme/prisoner3.scm
@@ -0,0 +1,40 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+;; Code to use in the three-person game
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+(define *game-association-list*
+  (list (list (list "c" "c" "c") (list 4 4 4))
+        (list (list "c" "c" "d") (list 2 2 5))
+        (list (list "c" "d" "c") (list 2 5 2))
+        (list (list "d" "c" "c") (list 5 2 2))
+        (list (list "c" "d" "d") (list 0 3 3))
+        (list (list "d" "c" "d") (list 3 0 3))
+        (list (list "d" "d" "c") (list 3 3 0))
+        (list (list "d" "d" "d") (list 1 1 1))))
+
+
+;; in expected-values: #f = don't care 
+;;                      X = actual-value needs to be #f or X 
+(define (test-entry expected-values actual-values) 
+   (cond ((null? expected-values) (null? actual-values)) 
+         ((null? actual-values) #f) 
+         ((or (not (car expected-values)) 
+              (not (car actual-values)) 
+              (= (car expected-values) (car actual-values))) 
+          (test-entry (cdr expected-values) (cdr actual-values))) 
+         (else #f))) 
+
+(define (is-he-a-fool? hist0 hist1 hist2) 
+   (test-entry (list 1 1 1) 
+               (get-probability-of-c 
+                (make-history-summary hist0 hist1 hist2))))
+
+(define (could-he-be-a-fool? hist0 hist1 hist2)
+  (test-entry (list 1 1 1)
+              (map (lambda (elt) 
+                      (cond ((null? elt) 1)
+                            ((= elt 1) 1)  
+                            (else 0)))
+                   (get-probability-of-c (make-history-summary hist0 
+                                                               hist1
+                                                               hist2)))))
+
