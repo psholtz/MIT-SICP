ceaa91a6ddebc8b308fff0752eb68328c509fdec
psholtz <paul.sholtz@gmail.com>
2012-07-02 18:01:48 -0700

    updating emacs

--- Section-2.2/emacs/exercise2-30.el
+++ Section-2.2/emacs/exercise2-30.el
@@ -1,5 +1,12 @@
 ;;
-
+;; Exercise 2.30
+;;
+;; Define a procedure "square-tree" analogous to the "square-list" procedure of 
+;; Exercise 2.21. That is, "square-tree" should behave as follows:
+;;
+;; (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+;; ;; ==> (1 (4 (9 16) 25) (36 49))
+;;
 
 ;;
 ;; Define the "square" form:
@@ -20,7 +27,7 @@
 ;; Run some unit tests:
 ;;
 (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
-;; ==>
+;; ==> (1 (4 (9 16) 25) (36 49))
 
 ;;
 ;; Define "square-tree" using map and lambda:
@@ -36,4 +43,4 @@
 ;; Run some unit tests:
 ;;
 (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
-;; ==>
+;; ==> (1 (4 (9 16) 25) (36 49))
