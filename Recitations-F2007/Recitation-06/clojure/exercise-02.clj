fcfa06d4f805e4a441d7ab4e330a076a62584484
psholtz <paul.sholtz@gmail.com>
2012-09-20 17:56:45 -0700

    updating clojure

---
+++ Recitations-F2007/Recitation-06/clojure/exercise-02.clj
@@ -0,0 +1,62 @@
+;;
+;; Working definitions
+;;
+(defn make-units [C L H] (list C L H))
+(defn get-units-C [x] (first x))
+(defn get-units-L [x] (first (rest x)))
+(defn get-units-H [x] (first (rest (rest x))))
+
+(defn make-class [number units] (list number units))
+(defn get-class-number [x] (first x))
+(defn get-class-units [x] (first (rest x)))
+
+(defn get-class-total-units [class]
+  (let [units (get-class-units class)]
+    (+ (get-units-C units)
+       (get-units-L units)
+       (get-units-H units))))
+
+(defn same-class? [c1 c2]
+  (= (get-class-number c1) (get-class-number c2)))
+
+;;
+;; Previous solutions
+;;
+(defn empty-schedule [] '())
+
+;;
+;; Exercise 2
+;;
+;; Write a selector that when given a class and a schedule, returns a
+;; new schedule including the new class:
+;;
+(defn
+  ^{:doc "'schedule' must already be a list"}
+  add-class [class schedule]
+  (concat schedule (list class)))
+
+;;
+;; Run some unit tests.
+;;
+;; First define some units and classes:
+;;
+(def u1 (make-units 3 3 3))
+(def calc1 (make-class 101 u1))
+(def calc2 (make-class 102 u1))
+
+;;
+;; Now try to build a schedule using these:
+;;
+(def s (add-class calc1 (empty-schedule)))
+;; ==> #'user/s
+(def s (add-class calc2 s))
+;; ==> #'user/s
+
+;;
+;; Inspect the schedule:
+;;
+(first s)
+;; ==> (101 (3 3 3))
+
+(first (rest s))
+;; ==> (102 (3 3 3))
