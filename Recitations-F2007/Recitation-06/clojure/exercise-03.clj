3c2382a03120383c19c23391964c38f480e70f71
psholtz <paul.sholtz@gmail.com>
2012-09-21 12:22:28 -0700

    updating clojure

--- Recitations-F2007/Recitation-06/clojure/exercise-03.clj
+++ Recitations-F2007/Recitation-06/clojure/exercise-03.clj
@@ -1,6 +1,8 @@
 ;;
 ;; Working definitions
 ;;
+(ns sicp.clojure.lisp)
+
 (defn make-units [C L H] (list C L H))
 (defn get-units-C [x] (first x))
 (defn get-units-L [x] (first (rest x)))
@@ -88,3 +90,63 @@
 ;; ==> #'user/s1
 (total-scheduled-units s1)
 ;; ==> 30
+ 
+;; =========
+;; [WORKING] (order of growth)
+;; =========
+
+;; =============================================================================
+
+;;
+;; Working definitions (Joy-style)
+;;
+(ns sicp.clojure.joy)
+
+(defn make-units [C L H] {:C C :L L :H H})
+(defn get-units-C [x] (x :C))
+(defn get-units-L [x] (x :L))
+(defn get-units-H [x] (x :H))
+
+(defn make-class [number units] {:number number :units units})
+(defn get-class-number [x] (x :number))
+(defn get-class-units [x] (x :units))
+
+(defn get-class-total-units [class]
+  (let [units (get-class-units class)]
+    (+ (get-units-C units)
+       (get-units-L units)
+       (get-units-H units))))
+
+(defn same-class? [c1 c2]
+  (= (get-class-number c1) (get-class-number c2)))
+
+;;
+;; Previous solutions
+;;
+(defn empty-schedule [] [])
+
+(defn
+  ^{:doc "'schedule' must already be a vector"}
+  add-class [class schedule]
+  (conj schedule class))
+
+;;
+;; Solution
+;;
+(defn
+  ^{:doc "Return the total number of units in an entire schedule."}
+  total-scheduled-units [schedule]
+
+  (defn
+    ^{:doc "Iterative process to recursively count the number of units."}
+    total-scheduled-units-iter [total working]
+    (if (empty? working)
+      total
+      (let [current-class (first working)]
+        (total-scheduled-units-iter
+         (+ total
+            (get-class-total-units current-class))
+         (subvec working 1)))))
+
+  ;; Invoke the iterative process
+  (total-scheduled-units-iter 0 schedule))
