908dfa5326aee60665bf236dcce1c72a43daf4ea
Paul Sholtz <paul.sholtz@gmail.com>
2012-10-07 21:48:43 -0700

    updating python

---
+++ Recitations-F2007/Recitation-06/python/exercise01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+#
+# Working definitions (python)
+#
+def make_units(C,L,H):
+  return [C,L,H]
+def get_units_C(x):
+  return x[0]
+def get_units_L(x):
+  return x[1]
+def get_units_H(x):
+  return x[2]
+
+def make_class(number,units):
+  return [number,units]
+def get_class_number(x):
+  return x[0]
+def get_class_units(x):
+  return x[1]
+
+def get_class_total_units(klass):
+  units = get_class_units(klass)
+  return get_units_C(units) + get_units_L(units) + get_units_H(units)
+
+def same_class(klass1,klass2):
+  return get_class_number(klass1) == get_class_number(klass2)
+
