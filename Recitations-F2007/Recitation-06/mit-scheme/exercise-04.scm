6d49c67538e8945690c8157f1e839032a0a2d151
Paul Sholtz <paul.sholtz@gmail.com>
2012-11-27 10:26:00 -0800

    updating scheme

--- Recitations-F2007/Recitation-06/mit-scheme/exercise-04.scm
+++ Recitations-F2007/Recitation-06/mit-scheme/exercise-04.scm
@@ -1,3 +1,40 @@
+;;
+;; Working definitions
+;;
+(define (make-units C L H)
+ (list C L H))
+(define get-units-C car)
+(define get-units-L cadr)
+(define get-units-H caddr)
+
+(define (make-class number units)
+ (list number units))
+(define get-class-number car)
+(define get-class-units cadr)
+
+(define (get-class-total-units class)
+ (let ((units (get-class-units class)))
+  (+ 
+   (get-units-C units)
+   (get-units-L units)
+   (get-units-H units))))
+
+(define (same-class? c1 c2)
+ (equal? (get-class-number c1) (get-class-number c2)))
+
+;;
+;; Previous solutions
+;;
+(define (empty-schedule) '())
+(define (add-class class schedule)
+  (append schedule (list class)))
+(define (total-scheduled-units schedule)
+  (define (iter seq total)
+    (if (null? seq)
+	total
+	(let ((class (car seq)))
+	      (iter (cdr seq) (+ total (get-class-total-units class))))))
+  (iter schedule 0))
 
 ;;
 ;; Exercise 4
@@ -17,6 +54,15 @@
 	      (iter (cdr elems))
 	      (append (list class) (iter (cdr elems)))))))
   (iter schedule))
+
+;;
+;; A more efficient way to implement the "drop-class" procedure would be 
+;; to use "filter":
+;;
+(define (drop-class schedule classnum)
+  (define (predicate class)
+    (not (equal? (get-class-number class) classnum)))
+  (filter predicate schedule))
 
 ;;
 ;; Run some unit tests:
@@ -67,5 +113,7 @@
 ;; ==> ((calc-101 (4 4 4)) (algb-152 (3 3 3)))
 
 ;;
-;; Working --> orders of growth
+;; The order of growth for the more efficient version of "drop-class"
+;; which uses the "filter" procedure is linear in both space and time, 
+;; that is, O(n) where n is the length of the list structure "schedule".
 ;;
