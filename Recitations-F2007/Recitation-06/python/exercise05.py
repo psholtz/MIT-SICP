505c92755ae350f4ac08adf0c6b2ec24575f574c
psholtz <paul.sholtz@gmail.com>
2012-11-26 17:17:45 -0800

    updating python

--- Recitations-F2007/Recitation-06/python/exercise05.py
+++ Recitations-F2007/Recitation-06/python/exercise05.py
@@ -49,13 +49,7 @@ def total_scheduled_units(schedule):
   # Make the recursive call to the inner iterative function
   return total_scheduled_units_iter(0,schedule)
 
-#
-# Exercise 4
-#
-# Write a procedure that drops a particular class from a schedule
-#
 def drop_class(schedule,classnum):
-  """ Drop a particular class from a schedule, identify class with "classnum" """
   #
   # Define the predicate used to filter the schedule.
   #
@@ -66,36 +60,3 @@ def drop_class(schedule,classnum):
   # Filter out the classes we want to delete:
   #
   return filter(predicate, schedule)
-
-#
-# Run some unit tests:
-#
-calculus_1 = make_class("CALC-101", make_units(4,4,4))
-calculus_2 = make_class("CALC-102", make_units(4,4,4))
-algebra = make_class("ALGB-152", make_units(3,3,3))
-diff_eqs = make_class("DIFF-201", make_units(3,3,3))
-
-s1 = empty_schedule()
-s1 = add_class(calculus_1, s1)
-s1 = add_class(algebra, s1)
-s1 = add_class(diff_eqs, s1)
-
-#
-# Introspecting s1:
-#
-# [['CALC-101', [4, 4, 4]], ['ALGB-152', [3, 3, 3]], ['DIFF-201', [3, 3, 3]]]
-#
-print "Schedule: ", s1
-
-#
-# Now try to drop a class:
-#
-s1 = drop_class(s1, "CALC-101")
-print "Schedule: ", s1
-# ==> [['ALGB-152', [3, 3, 3]], ['DIFF-201', [3, 3, 3]]]
-
-#
-# The order of growth is linear in both time and space in the variable
-# "schedule", that is, it is O(n) where "n" is the length of the list
-# structure "schedule".
-#
