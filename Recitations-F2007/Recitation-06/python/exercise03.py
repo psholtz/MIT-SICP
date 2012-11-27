e6f1af1e950677b30a9b4304214f23d54dbadb6b
psholtz <paul.sholtz@gmail.com>
2012-11-26 16:32:36 -0800

    updating python

--- Recitations-F2007/Recitation-06/python/exercise03.py
+++ Recitations-F2007/Recitation-06/python/exercise03.py
@@ -47,6 +47,42 @@ def total_scheduled_units(schedule):
   
   def total_scheduled_units_iter(total,working):
     """ Iterative process to recursively count the number of units """
-    return total
+    if not working:
+      return total
+    else:
+      return total_scheduled_units_iter(total + get_class_total_units(working[0]), working[1:])
 
+  # Make the recursive call to the inner iterative function
   return total_scheduled_units_iter(0,schedule)
+
+#
+# Run some unit tests:
+#
+calculus_1 = make_class("CALC-101", make_units(4,4,4))
+calculus_2 = make_class("CALC-102", make_units(4,4,4))
+algebra = make_class("ALGB-152", make_units(3,3,3))
+diff_eqs = make_class("DIFF-201", make_units(3,3,3))
+
+print "Calculus 101 Total Units: ", get_class_total_units(calculus_1)
+# ==> 12 
+print "Calculus 102 Total Units: ", get_class_total_units(calculus_2)
+# ==> 12
+print "Algebra 152 Total Units: ", get_class_total_units(algebra)
+# ==> 9
+print "Differential Equations 201 Total Units: ", get_class_total_units(diff_eqs)
+# ==> 9
+
+print
+
+s1 = empty_schedule()
+print "Total Scheduled Units: ", total_scheduled_units(s1)
+
+s1 = add_class(calculus_1, s1)
+print "Total Scheduled Units: ", total_scheduled_units(s1)
+
+s1 = add_class(algebra, s1)
+print "Total Scheduled  Units: ", total_scheduled_units(s1)
+
+s1 = add_class(diff_eqs, s1)
+print "Total Scheduled Units: ", total_scheduled_units(s1)
+
