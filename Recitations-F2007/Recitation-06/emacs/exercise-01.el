a516a6f310a45b6d8cc5fdc5950f8323809ecf8b
Paul Sholtz <paul.sholtz@gmail.com>
2012-11-27 15:28:03 -0800

    updating emacs

---
+++ Recitations-F2007/Recitation-06/emacs/exercise-01.el
@@ -0,0 +1,34 @@
+;;
+;; Working definitions
+;;
+(defun make-units (C L H)
+ (list C L H))
+(defun get-units-C (x) (car x))
+(defun get-units-L (x) (cadr x))
+(defun get-units-H (x) (cadr (cdr x)))
+
+(defun make-class (number units)
+ (list number units))
+(defun get-class-number (x) (car x))
+(defun get-class-units (x) (cadr x))
+
+(defun get-class-total-units (class)
+ (let ((units (get-class-units class)))
+  (+ 
+   (get-units-C units)
+   (get-units-L units)
+   (get-units-H units))))
+
+(defun same-class? (c1 c2)
+ (equal (get-class-number c1) (get-class-number c2)))
+
+;;
+;; Exercise 1
+;;
+;; Define a constructor "empty-schedule" that returns an empty schedule
+;;
+(defun empty-schedule () '())
+
+;; 
+;; The order of growth is constant in both time and space.
+;;
