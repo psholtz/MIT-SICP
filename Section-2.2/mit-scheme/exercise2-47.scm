0058936f5c1c8fa1398855b202ec0f828a6fe907
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-05 13:34:24 -0800

    updating scheme

--- Section-2.2/mit-scheme/exercise2-47.scm
+++ Section-2.2/mit-scheme/exercise2-47.scm
@@ -1,11 +1,16 @@
 ;;
 ;; Exercise 2.47
 ;;
-;; [WORKING]
+;; Here are two possible constructors for frames:
 ;;
-
+;;  (define (make-frame origin edge1 edge2)
+;;   (list origin edge1 edge2))
 ;;
-;; 
+;;  (define (make-frame origin edge1 edge2)
+;;   (cons origin (cons edge1 edge2)))
+;;
+;; For each constructor supply the appropriate selectors to produce an implementation for frames.
+;;
 
 ;;
 ;; First constructor and supporting selectors:
@@ -26,11 +31,11 @@
 ;; First we need the supporting vector operations from Exercise 2.46:
 ;;
 (define (make-vect x y)
-  (list x y))
+  (cons x y))
 (define (xcor-vect p)
   (car p))
 (define (ycor-vect p)
-  (cadr p))
+  (cdr p))
 
 (define origin (make-vect -2 1))
 (define edge1 (make-vect -1 2))
@@ -41,13 +46,13 @@
 ;;
 (define f (make-frame origin edge1 edge2))
 f
-;; ==> ((-2 1) (-1 2) (4 3))
+;; ==> ((-2 . 1) (-1 . 2) (4 . 3))
 (origin-frame f)
-;; ==> (-2 1)
+;; ==> (-2 . 1)
 (edge1-frame f)
-;; ==> (-1 2)
+;; ==> (-1 . 2)
 (edge2-frame f)
-;; ==> (4 3)
+;; ==> (4 . 3)
 
 ;;
 ;; Second constructor and supporting selectors:
@@ -67,11 +72,26 @@ f
 ;;
 (define f (make-frame origin edge1 edge2))
 f
-;; ==> ((-2 1) (-1 2) 4 3)
+;; ==> ((-2 . 1) (-1 . 2) 4 . 3)
 
 (origin-frame f)
-;; ==> (-2 1)
+;; ==> (-2 . 1)
 (edge1-frame f)
-;; ==> (-1 2)
+;; ==> (-1 . 2)
 (edge2-frame f)
-;; ==> (4 3)
+;; ==> (4 . 3)
+
+;;
+;; Note: we will use the same data model that is used in "SICP Picture Language" for Dr. Racket
+;;
+;; These are the best definitions/constructors/selectors to use for that purpose:
+;;
+(define (make-frame origin edge1 edge2)
+  (list 'frame origin edge1 edge2))
+
+(define (origin-frame f)
+  (cadr f))
+(define (edge1-frame f)
+  (caddr f))
+(define (edge2-frame f)
+  (cadddr f))
