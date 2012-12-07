9f0d1db634357866553717fc2efe0eff1d8edb9f
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-07 12:29:50 -0800

    updating scheme

--- Section-2.3/mit-scheme/exercise2-59.scm
+++ Section-2.3/mit-scheme/exercise2-59.scm
@@ -56,3 +56,79 @@
 ;; ==> (4 1 2 3)
 (union-set '(1 2 3) '(4 5 6))
 ;; ==> (3 2 1 4 5 6)
+
+;;
+;; An even more concise version of "union-set" could be given in terms of accumulate:
+;;
+(define (accumulate op init seq)
+  (if (null? seq)
+      init
+      (op (car seq)
+	  (accumulate op init (cdr seq)))))
+
+(define (union-set s1 s2)
+  (accumulate (lambda (a b) (adjoin-set a b)) s2 s1))
+
+;;
+;; Let's run through the same unit tests again:
+;;
+(union-set '() '())
+;; ==> ()
+(union-set '(1 2 3) '())
+;; ==> (1 2 3)
+(union-set '() '(1 2 3))
+;; ==> (1 2 3)
+(union-set '(1 2 3) '(1 2 3))
+;; ==> (1 2 3)
+(union-set '(1) '(1 2 3))
+;; ==> (1 2 3)
+(union-set '(4) '(1 2 3))
+;; ==> (4 1 2 3)
+(union-set '(1 2 3) '(4 5 6))
+;; ==> (1 2 3 4 5 6)
+
+;; 
+;; Let's compare the performance of these two versions of union.
+;;
+
+;;
+;; The first use case, using the first algorithm:
+;;
+(union-set '(1 2 3) '(5 6 7 8 9))
+(union-set-iter '(1 2 3) '(5 6 7 8 9))
+(union-set-iter '(2 3) (adjoin-set 1 '(5 6 7 8 9)))
+(union-set-iter '(3) (adjoin-set 2 (adjoin-set 1 '(5 6 7 8 9))))
+(union-set-iter '() (adjoin-set 3 (adjoin-set 2 (adjoin-set 1 '(5 6 7 8 9)))))
+(adjoin-set 3 (adjoin-set 2 (adjoin-set 1 '(5 6 7 8 9))))
+(adjoin-set 3 (adjoin-set 2 '(1 5 6 7 8 9)))   ;; 5 steps here to verify that 1 is not a member 
+(adjoin-set 3 '(2 1 5 6 7 8 9))                ;; 6 steps here to verify that 2 is not a member
+'(3 2 1 5 6 7 8 9)                             ;; 7 steps here to verify that 3 is not a member
+
+;;
+;; The total number of steps, then, is 9 + 5 + 6 + 7 = 27
+;;
+
+;;
+;; The same use case, using the second algorithm:
+;;
+(define f (lambda (a b) (adjoin-set a b)))
+(union-set '(1 2 3) '(5 6 7 8 9))
+(accumulate f '(5 6 7 8 9) '(1 2 3))
+(f 1 (accumulate f '(5 6 7 8 9) '(2 3)))
+(f 1 (f 2 (accumulate f '(5 6 7 8 9) '(3))))
+(f 1 (f 2 (f 3 (accumulate f '(5 6 7 8 9) '()))))
+(f 1 (f 2 (f 3 '(5 6 7 8 9))))
+(f 1 (f 2 '(3 5 6 7 8 9)))   ;; 5 steps here to verify that 3 is not a member
+(f 1 '(2 3 5 6 7 8 9))       ;; 6 steps here to verify that 2 is not a member
+'(1 2 3 4 5 6 7 8)           ;; 7 steps here to verify that 1 is not a member
+
+;;
+;; The total number of steps, then, is 10 + 5 + 6 + 7 = 28
+;;
+
+;;
+;; A good deal of the processing time, then, seems to be spent in the "adjoin-set" 
+;; procedure, checking the entire set to make sure that the element to be added
+;; is not already part of the set.
+;;
+
--- Section-2.3/mit-scheme/exercise2-60.scm
+++ Section-2.3/mit-scheme/exercise2-60.scm
@@ -3,3 +3,46 @@
 ;;
 
 ;; --> tghese are bags, or multisets
+
+;;
+;; "element-of-set?" can remain defined as before, since as soon as we find the first element in 
+;; set, we are done:
+;;
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else
+	 (element-of-set? x (cdr set)))))
+
+;;
+;; "adjoin-set" will be changed, since we no longer have to check for membership before 
+;; adjoining/adding a member:
+;;
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1)
+	       (intersection-set (cdr set1) set2)))
+	(else
+	 (intersection-set (cdr set1) set2))))
+
+(define (union-set s1 s2)
+  (define (union-set-iter t1 t2)
+    (if (null? t1)
+	t2
+	(union-set-iter (cdr t1) (adjoin-set (car t1) t2))))
+  (union-set-iter s1 s2))  
+
+;;
+;; Sets are used in relational database technology. 
+;;
+;; For instance, we can return a set of tuples. 
+
+;;
+;; For instance, the field of data mining, Jaccard similarity is an index
+;; that is used for bag.. The Jaccard similarity of two bag/multisets 
+;; is defined as:
+;; 
