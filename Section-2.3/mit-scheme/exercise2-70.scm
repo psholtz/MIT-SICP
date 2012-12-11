31fe688fec76d76d49079468f66c3869c9b3999f
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-10 23:09:21 -0800

    updating scheme

--- Section-2.3/mit-scheme/exercise2-70.scm
+++ Section-2.3/mit-scheme/exercise2-70.scm
@@ -125,10 +125,22 @@
 (encode '(sha na na na na na na na na) tree)
 ;; ==> (1 1 0 0 0 0 0 0 0 0 0)
 (encode '(get a job) tree)
-;; ==>
+;; ==> (1 1 1 1 0 1 1 1 0 1 1 1 1 1 0)
 (encode '(sha ha na na na na na na na na) tree)
-;; ==>
+;; ==> (1 1 0 0 0 0 0 0 0 0 0)
 (encode '(wah yip yip yip yip yip yip yip yip yip) tree)
 ;; ==> (1 1 1 1 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0)
 (encode '(sha boom) tree)
 ;; ==> (1 1 0 1 1 1 1 1 1 0)
+
+;;
+;; Let's see if these decode correctly:
+;;
+(decode '(1 1 1 1 0 1 1 1 0 1 1 1 1 1 0) tree)
+;; ==>
+(decode '(1 1 0 0 0 0 0 0 0 0 0) tree)
+;; ==>
+(decode '(1 1 1 1 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0) tree)
+;; ==>
+(decode '(1 1 0 1 1 1 1 1 1 0) tree)
+;; ==>
