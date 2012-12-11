6a662d0ce283b90ab4bea233cb9c19f9a9672de1
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-10 22:49:17 -0800

    updating shceme

--- Section-2.3/mit-scheme/exercise2-69.scm
+++ Section-2.3/mit-scheme/exercise2-69.scm
@@ -1,7 +1,19 @@
 ;;
 ;; Exercise 2.69
 ;;
-;; [WORKING]
+;; The following procedure takes as its argument a list of symbol-frequency pairs (where no 
+;; symbol appears in more than one pair) and generates a Huffman encoding tree according to 
+;; the Huffman algorithm.
+;;
+;;  (define (generate-huffman-tree pairs)
+;;   (successive-merge (make-leaf-set pairs)))
+;;
+;; Make-leaf-set is the procedure given above that transforms the list of pairs into an ordered 
+;; set of leaves. Successive-merge is the procedure you must write, using make-code-tree to 
+;; successively merge the smallest-weight elements of the set until there is only one element left, 
+;; which is the desired Huffman tree. (This procedure is slightly tricky, but not really complicated. 
+;; If you find yourself designing a complex procedure, then you are almost certainly doing something 
+;; wrong. You can take significant advantage of the fact that we are using an ordered set representation.)
 ;;
 
 ;;
