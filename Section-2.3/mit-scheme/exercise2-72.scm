e99c3f24db20a7ab1d56fb49b9db87b910950a71
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-11 13:53:14 -0800

    updating scheme

---
+++ Section-2.3/mit-scheme/exercise2-72.scm
@@ -0,0 +1,145 @@
+;;
+;; Exercise 2.72
+;;
+;; [WORKING]
+;;
+
+(load "huffman.scm")
+
+;;
+;; Let's look more closely at the "encode-symbol" procedure:
+;;
+(define (encode-symbol symbol tree)
+  (define (encode-symbol-iter working total)
+    (if (leaf? working)
+	total
+	(let ((symbols-left (symbols (left-branch working)))
+	      (symbols-right (symbols (right-branch working))))
+	  (cond ((element-of-set? symbol symbols-left)
+		 (encode-symbol-iter (left-branch working) (cons 0 total)))
+		((element-of-set? symbol symbols-right)
+		 (encode-symbol-iter (right-branch working) (cons 1 total)))
+		(else
+		 (error "Bad symbol: ENCODE-SYMBOL" symbol))))))
+  (encode-symbol-iter tree '()))
+
+;;
+;; Let's also look at some of the other required procedures:
+;;
+(define (left-branch tree (car tree)))
+(define (right-branch tree) (cadr tree))
+(define (leaf? object) (eq? (car object) 'leaf))
+
+;;
+;; Each of these three supporting procedures runs in constant time.
+;;
+
+;;
+;; We will consider only the special case described above, where n = 2^k.
+;; 
+;; For the case k = 4, the tree will look like this:
+;;
+(define tree (generate-huffman-tree '((a 1) (b 2) (c 4) (d 8))))
+
+(encode-symbol 'a tree)
+;; ==> (1 1 1)
+(encode-symbol 'b tree)
+;; ==> (1 1 0) 
+(encode-symbol 'c tree)
+;; ==> (1 0)
+(encode-symbol 'd tree)
+;; ==> (0)
+
+;;
+;; The tree will look something like the following:
+;;
+;;     (root)
+;;      /  \
+;;     /    \ 
+;;  (d 8)   ()
+;;         /  \ 
+;;        /    \
+;;     (c 4)   ()
+;;            /  \
+;;           /    \
+;;        (b 2)  (a 1)
+;;
+
+;;
+;; What is the running time to encode the most frequent symbol?
+;;  
+;; Let's step through the call graph:
+;;
+(encode-symbol 'd tree)
+(encode-symbol-iter '((a 1) (b 2) (c 4) (d 8)) '())
+
+;;
+;; Calls are now made to:
+;;
+;;  1. (leaf? '((a 1) (b 2) (c 4) (d 8)))
+;;
+;; and then:
+;;
+;;  2. (left-branch '((a 1) (b 2) (c 4) (d 8)))
+;;  3. (right-branch '((a 1) (b 2) (c 4) (d 8)))
+;; 
+;; First the "left-branch" is searched, which really only has one element in it.
+;; 
+;; The target element is identified in this branch. We "cons" 0 onto "total", and 
+;; then make the recursive call:
+;;
+(encode-symbol-iter '(leaf d 8) '(0))
+
+;;
+;; Since this is a leaf, we terminate the recursion here.
+;;
+;; "reverse" is applied to the list '(0), but since it too has only 1 element, 
+;; the procedure runs in constant time.
+;;
+
+;;
+;; There are several sub-procedures which are invoked, which, in the general 
+;; case would run in O(n) time, but given the nature of the data structures
+;; we're using, there is no invocation here that does not complete in more
+;; than constant time.
+;;
+;; Hence, no matter how large n is, the encoding of the most frequent symbol
+;; will always terminate in constant time. 
+;;
+
+;;
+;; For the least frequent symbol, we again enter the call graph, and invoke
+;; the procedures "leaf?", "left-branch", "right-branch", all of which execute
+;; in linear time. We must then search for the target symbol in the list of 
+;; symbols, both in the "left" branch, which always has just 1 element and terminates
+;; in constant time, and the "right" branch, which in the first iteration will have 
+;; (n-1) elements (and so runs in O(n) time).
+;; 
+;; The same sequence repeats itself (mainly constant-time operations), except 
+;; again for searching the symbol list, which now has (n-2) elements in it. 
+;; Note too, that in this implementation, the target symbol we are seeking (i.e., 
+;; the least frequency symbol), appears at the end of the symbol list, so the 
+;; entire list must be searched before the symbol is found:
+;;
+(symbols (right-branch tree))
+;; ==> (c b a)
+
+;;
+;; In total, the total number of steps that will be performed when searching
+;; the symbol set, via "element-of-set?", will add up to:
+;;
+;;  (n-1) + (n-2) + ... + 1
+;;
+;; This will total (1/2) * (n-1) * (n-2), or, in other words, be O(n^2).
+;;
+;; Note too that once we have generated our symbol list, which has been 
+;; build up using constant-time "cons" operations, we must reverse it, which 
+;; requires one application of a linear-time procedure. Still, the O(n^2)
+;; time of the symbol search predominates this operation.
+;;
+;; In summary:
+;;
+;; (1) To find the most frequent symbol requires constant time.
+;;
+;; (2) To find the least frequent symbol requires O(n^2) time.
+;; 
