819e3a4365c7ac652724e0eecee81042c52e18d5
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-11 17:17:29 -0800

    updating

--- Section-2.4/mit-scheme/complex-package.scm
+++ Section-2.4/mit-scheme/complex-package.scm
@@ -9,30 +9,6 @@
 	(else
 	  (assoc key (cdr records)))))
 
-(define (lookup key-1 key-2 table)
-  (let ((subtable (assoc key-1 (cdr table))))
-    (if subtable
-        (let ((record (assoc key-2 (cdr subtable))))
-          (if record
-              (cdr record)
-              false))
-        false)))
-
-(define (insert! key-1 key-2 value table)
-  (let ((subtable (assoc key-1 (cdr table))))
-    (if subtable
-        (let ((record (assoc key-2 (cdr subtable))))
-          (if record
-              (set-cdr! record value)
-              (set-cdr! subtable
-                        (cons (cons key-2 value)
-                              (cdr subtable)))))
-        (set-cdr! table
-                  (cons (list key-1
-                              (cons key-2 value))
-			(cdr table)))))
-  'ok)
-
 (define (make-table)
   (let ((local-table (list '*table*)))
     (define (lookup key-1 key-2)
