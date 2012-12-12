304d5645e25d2b8ec4650fa1e7cbde466c3880ec
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-11 18:02:33 -0800

    updating scheme

--- Section-2.4/mit-scheme/exercise2-73.scm
+++ Section-2.4/mit-scheme/exercise2-73.scm
@@ -4,53 +4,14 @@
 ;; [WORKING]
 ;;
 
+;;
+;; Load the necessary libraries:
+;;
 (load "differentiation.scm")
+(load "table.scm")
 
 ;;
-;; First load the supporting table procedures that we're going need:
-;;
-(define (assoc key records)
-  (cond ((null? records) false)
-	((equal? key (caar records)) (car records))
-	(else
-	   (assoc key (cdr records)))))
-
-(define (make-table)
-  (let ((local-table (list '*table*)))
-    (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (cdr record)
-                  false))
-            false)))
-    (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
-            (set-cdr! local-table
-                      (cons (list key-1
-                                  (cons key-2 value))
-                            (cdr local-table)))))
-      'ok)    
-    (define (dispatch m)
-      (cond ((eq? m 'lookup-proc) lookup)
-            ((eq? m 'insert-proc!) insert!)
-            (else (error "Unknown operation -- TABLE" m))))
-    dispatch))
-
-(define operation-table (make-table))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
-
-;;
-;; We also need to "install" the procedures to support symbolic differentiation:
+;; Let's also install the procedures we need to support symbolic differentiation:
 ;;
 (define (install-symbolic-differentiator)
   (put 'deriv '+ (lambda (exp var)
