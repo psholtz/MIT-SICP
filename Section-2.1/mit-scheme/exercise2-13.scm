744d7e4889024c21915202ced4d1d3da55bd1ae3
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-12 12:21:51 -0800

    fixing small typo

--- Section-2.1/mit-scheme/exercise2-13.scm
+++ Section-2.1/mit-scheme/exercise2-13.scm
@@ -3,7 +3,7 @@
 ;;
 ;; Show that under the assumption of small percentage tolerances there is a simple
 ;; formula for the approximate percentage tolerance of the product of two intervals
-;; in terms of the tolerances of the facts. You may simplify the problem by assuming
+;; in terms of the tolerances of the factors. You may simplify the problem by assuming
 ;; that all numbers are positive.
 ;;
 
