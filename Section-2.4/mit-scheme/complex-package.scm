c2f669ab88f7cc9e95115af8233999b90e1e9951
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-13 16:31:59 -0800

    updating scheme

--- Section-2.4/mit-scheme/complex-package.scm
+++ Section-2.4/mit-scheme/complex-package.scm
@@ -149,7 +149,70 @@
 		          (- (angle z1) (angle z2))))
 
 ;;
-;; Run some unit tests:
+;; There are several things about these procedures that are worth noting.
+;;
+;; In the first place, not that we store the four selectors "real-part", 
+;; "imag-part", "magnitude" and "angle" in the table under the token
+;; using a list structure (i.e., '(rectangular), while the two
+;; constructors are stored using an ordinary token (i.e., 'rectangular).
+;;
+;; The reason for this is the way "apply-generic" maps the procedure
+;; "type-tag" onto its arguments. Consider, for instance:
+;;
+(define z1 (make-from-rect-imag 2 3))
+;; ==> (rectangular 2 . 3)
+(define z2 (make-from-mag-ang 4 5))
+;; ==> (polar 4 . 5)
+
+(map type-tag (list z1))
+;; ==> (rectangular)
+(map type-tag (list z2))
+;; ==> (polar)
+(map type-tag (list z1 z2))
+;; ==> (rectangular polar)
+
+;;
+;; Consequently, since the four selectors are accessed using the "apply-generic"
+;; procedure, which uses such a mapping, it's simpler to store the four selectors
+;; in the symbol table using the list representation of the token. 
+;; 
+
+;;
+;; Another point, we are using the "rectangular" representation 
+;; to handle addition and subtraction, and the "polar" representation
+;; to handle addition and multiplication. Let's verify this:
+;;
+(map type-tag (list 
+	       (add-complex z1 z2)
+	       (sub-complex z1 z2)
+	       (mul-complex z1 z2)
+	       (div-complex z1 z2)))
+;; ==> (rectangular rectangular polar polar)
+
+;;
+;; Let's also run the unit test suggested in the text:
+;;
+(define (checker1 z)
+  (let ((test (make-from-real-imag (real-part z) (imag-part z1))))
+    (and (= (real-part z) (real-part test))
+	 (= (imag-part z) (imag-part test)))))
+
+(checker1 z1)
+;; ==> #t
+
+;;
+;; Let's run the same unit test for the "mag-ang" representation:
+;;
+(define (checker2 z)
+  (let ((test (make-from-mag-ang (magnitude z) (angle z))))
+    (and (= (magnitude z) (magnitude test))
+	 (= (angle z) (angle test)))))
+
+(checker2 z2)
+;; ==> #t
+
+;;
+;; Run some additional unit tests:
 ;;
 (define c1 (make-from-mag-ang 1 0))
 ;; ==> (polar 1 . 0)
---
+++ Section-2.4/mit-scheme/exercise2-74.scm
@@ -0,0 +1,194 @@
+;;
+;; Exercise 2.74
+;;
+;; [WORKING]
+;;
+
+;; 
+;; First we require a database:
+;;
+;; ======== 
+;; DATABASE
+;; ======== 
+(define (assoc key records)
+  (cond ((null? records) false)
+	((equal? key (caar records)) (car records))
+	(else
+	   (assoc key (cdr records)))))
+
+(define (make-table-2d)
+  ;; procedure for defining 2-dimensional tables
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define *operations* (make-table-2d))
+(define get (*operations* 'lookup-proc))
+(define put (*operations* 'insert!))
+
+;;
+;; We also require tagging support:
+;;
+;; ===============
+;; TAGGING LIBRARY
+;; ===============
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum -- TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum -- CONTENTS" datum))) 
+
+;;
+;; (a) Implement for headquarters a get-record procedure that retrieves a specified employee's record 
+;;     from a specified personnel file. The procedure should be applicable to any division's file. 
+;;     Explain how the individual divisions' files should be structured. In particular, what type 
+;;     information must be supplied?
+;; 
+
+;;
+;; Let's implement constructor/selectors for each division's personnel file:
+;;
+(define (make-personnel-file division personnel-file)
+  (attach-tag division personnel-file))
+(define (division file)
+  (type-tag file))
+(define (personnel-file file)
+  (contents file))
+
+;;
+;; In this model, the "personnel-file" argument to the constructor
+;; will be a table (or other data structure) of employee records. 
+;; Each department is free to implement this table/data structure
+;;  however it desires.
+;;			     
+
+;;
+;; We can now implement the "get-record" procedure in a generic 
+;; manner as follows:
+;;
+(define (get-record employee file)
+  ((get 'get-record (division file)) employee (personnel-file file)))
+
+;;
+;; The *operations* table in which we have stored the procedure is 
+;; 2-dimensional, and will return a procedure which itself takes
+;; 2 arguments: (a) the employee whose record we want to look up;
+;; and (b) a data structure (i.e., table, list, tree, etc) of 
+;; personnel records for that particular department.
+;;
+
+;;
+;; Suppose for the sake of discussion we have two divisions in the 
+;; company: engineering and marketing. We will tag our records either 
+;; with the token "engineering" or "marketing" accordingly:
+;;
+(define (engineering-personnel-file? file)
+  (eq? (division file) 'engineering))
+(define (marketing-personnel-file? file)
+  (eq? (division file) 'marketing))
+
+;;
+;; [WORKING ENGINEERING]
+;;
+
+;;
+;; Suppose on the contrary, that the marketing department wants to 
+;; implement their data model in a more "simple" manner. For marketing, 
+;; a personnel file will consists of records which are simply list
+;; structures, as follows: (1) name; (2) title; (3) salary. 
+;;
+;; So for instance, a record from the marketing department might 
+;; look like:
+;;
+;;  '(smith '(vp marketing) 110000)
+;;
+;; A very different structure from that used by engineering!
+;;
+;; Let's design a procedure to install these records.
+;;
+(define (generate-marketing-file)
+  (let ((*marketing-records* '()))
+    *marketing-records*))
+
+(define *m
+(define (install-engineering-records)
+  (define (get-record employee
+  (put 'get-record 'engineering
+;;
+;; We can now implement "get-record" as follows:
+;;
+
+		      
+
+(define (make-engineering-record name title)
+  (type-tag 'engineering (list name title)))
+
+(define (make-marketing-record name title age)
+  (type-tag 'marketing (list name title age)))
+
+;;
+;; Define the installation routines:
+;;
+;; ==================== 
+;; ENGINEERING DIVISION
+;; ==================== 
+(define (install-engineering-records)
+  (let ((*records* (make-table))
+	(get-engineering (*records* 'lookup-proc))
+	(put-engineering (*records* 'insert!)))
+    ;;
+    ;; (put 'get-by-name 'engineering (lambda (n) return-record-by-name))
+    ;;
+    
+    
+    'done))
+
+;; ==================
+;; MARKETING DIVISION
+;; ================== 
+(define (install-marketing-records)
+  ;;
+  ;; (put 'get-by-name 'marketing (lambda (name) return-record-by-name))
+  ;;
+  'done)
+
+(define (apply-generic op . args)
+  '())
+
+(define (get-record name) (apply-generic 'get-by-name name))
+
+;;
+;; To use this architecture, you have to have it as 
+;;
+;; (lookup 'get-by-name 'marketing)
+;; (lookup 'get-by-name 'engineering)
