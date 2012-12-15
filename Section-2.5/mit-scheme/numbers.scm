f05ed60c8e31e703bcdd3158b4834365992a15a8
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-15 13:19:07 -0800

    updating scheme

---
+++ Section-2.5/mit-scheme/numbers.scm
@@ -0,0 +1,39 @@
+;;
+;; Define the procedures used in the "generic" numbers package in the text.
+;;
+(load "table.scm")
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+	  (apply proc (map contents args))
+	  (error
+	   "No method for these types -- APPLY-GENERIC" (list op type-tags))))))
+
+;;
+;; Scheme-numbers:
+;;
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+    (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+    (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+    (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+    (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  'done)
+
+(define (make-scheme-number n)
+  ((get 'name 'scheme-number) n))
+
