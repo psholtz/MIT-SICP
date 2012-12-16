e88b53e101d79cfe14046a4abdc6074685bd84b2
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-15 21:47:07 -0800

    updating scheme

--- Recitations-F2007/Recitation-08/mit-scheme/exercise-04.scm
+++ Recitations-F2007/Recitation-08/mit-scheme/exercise-04.scm
@@ -35,6 +35,61 @@
   (list 'not exp))
 (define (not-first exp)
   (cadr exp))
+(define (assoc key records)
+  (cond ((null? records) false)
+	((equal? key (caar records)) (car records))
+	(else
+	  (assoc key (cdr records)))))
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(cdr record)
+	false)))
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(set-cdr! record value)
+	(set-cdr! table
+		    (cons (cons key value) (cdr table))))))
+(define (make-table)
+  (list '*table*))
+(define env (make-table))
+(define (variable-value name environment)
+  (define (variable-value-iter working)
+    (if (null? working)
+	(error "VARIABLE-VALUE: no binding for variable: " name)
+	(let ((value (car working)))
+	    (if (equal? name (car value))
+		      (cdr value)
+		            (variable-value-iter (cdr working))))))
+  (variable-value-iter (cdr environment)))
+(variable-value 'a env)
+(variable-value 'b env)
+(variable-value 'c env)
+(define (eval-boolean exp env)
+  ;; return the boolean value of the argument symbol
+  (define (boolean-value sym)
+    (if (variable? sym)
+	(variable-value sym env)
+	(not (or (null? sym) (eq? sym #f)))))
+
+  ;; evaluate the boolean expression
+  (cond ((or? exp)
+	  (let ((first (or-first exp))
+		       (second (or-second exp)))
+	       (or (boolean-value first) (boolean-value second))))
+
+	((and? exp)
+	  (let ((first (and-first exp))
+		       (second (and-second exp)))
+	       (and (boolean-value first) (boolean-value second))))
+
+	((not? exp)
+	  (let ((first (not-first exp)))
+	       (not (boolean-value first))))
+
+	(else
+	  (error "EVAL - expression is not a boolean expression: " exp))))
 
 ;;
 ;; Exercise 4
