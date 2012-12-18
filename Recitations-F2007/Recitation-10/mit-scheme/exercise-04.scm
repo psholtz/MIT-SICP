5dde11b343e3a4f62683663f0bbe7316fbe87ede
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-17 21:12:28 -0800

    updating scheme

--- Recitations-F2007/Recitation-10/mit-scheme/exercise-04.scm
+++ Recitations-F2007/Recitation-10/mit-scheme/exercise-04.scm
@@ -12,4 +12,8 @@
 (define (dequeue q)
   (cdr q))
 
-(define q (enqueue 4 (enqueue 5 (
+;;(define q (enqueue 4 (enqueue 5 (
+
+;;
+;; Order of growth in both time and space is constant.
+;;
---
+++ Recitations-F2007/Recitation-10/mit-scheme/exercise-05.scm
@@ -0,0 +1,17 @@
+;;
+;; Exercise 5
+;;
+;; [WORKING]
+;;
+
+;;
+;; A simple definition would be:
+;;
+(define (head q)
+  (car q))
+
+;; (working) --> examples
+
+;;
+;; The order of growth is constant in both time and space.
+;;
