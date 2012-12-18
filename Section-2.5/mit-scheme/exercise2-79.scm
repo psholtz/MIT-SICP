b19c3f9065b46557b0481291ffffd70c994fe656
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-18 02:12:39 -0800

    updating scheme

--- Section-2.5/mit-scheme/exercise2-79.scm
+++ Section-2.5/mit-scheme/exercise2-79.scm
@@ -78,6 +78,78 @@
        (lambda (n d) (tag (make-rat n d))))
   'done)
 
+;; New rectangular package:
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+	          (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+  
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+
+  ;; =============
+  ;; New Procedure
+  ;; =============
+  (put 'equ? '(rectangular)
+       (lambda x y) (and (= (real-part x) (real-part y))
+			 (= (imag-part x) (imag-part y))))
+
+  (put 'make-from-mag-ang 'rectangular 
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+;; New polar package:
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+	    (atan y x)))
+  
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+
+  ;; =============
+  ;; New Procedure
+  ;; =============
+  (put 'equ? '(polar) (lambda (x y)
+			(let ((PI 3.141592653589793238462))
+			  (let ((kx (floor (/ (angle x) (* 2 PI))))
+				(ky (floor (/ (angle y) (* 2 PI)))))
+			    (and (= (magnitude x) (magnitude y))
+				 (= (- (angle x) (* kx 2 PI))
+				    (- (angle y) (* ky 2 PI))))))))
+
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
 ;;
 ;; Reinstall the number packages:
 ;;
