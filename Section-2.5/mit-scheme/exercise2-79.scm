675954259832aacbbde70a5fbd9406099696b644
psholtz <paul.sholtz@gmail.com>
2012-12-18 19:41:02 -0800

    Update Section-2.5/mit-scheme/exercise2-79.scm

--- Section-2.5/mit-scheme/exercise2-79.scm
+++ Section-2.5/mit-scheme/exercise2-79.scm
@@ -1,7 +1,7 @@
 ;;
 ;; Exercise 2.79
 ;;
-;; Define a generic equality predicate equ? that tests the equality of two numbers, and 
+;; Define a generic equality predicate "equ?" that tests the equality of two numbers, and 
 ;; install it in the generic arithmetic package. This operation should work for ordinary 
 ;; numbers, rational numbers, and complex numbers.
 ;;
