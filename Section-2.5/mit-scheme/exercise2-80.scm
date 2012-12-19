7ddecc1a287e6eabb169662566cd625e50e18799
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-18 21:27:20 -0800

    updating scheme

--- Section-2.5/mit-scheme/exercise2-80.scm
+++ Section-2.5/mit-scheme/exercise2-80.scm
@@ -46,19 +46,19 @@
       (cons (/ n g) (/ d g))))
   (define (add-rat x y)
     (make-rat (+ (* (numer x) (denom y))
-		   (* (numer y) (denom x)))
-	                  (* (denom x) (denom y))))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
   (define (sub-rat x y)
     (make-rat (- (* (numer x) (denom y))
-		   (* (numer y) (denom x)))
-	                  (* (denom x) (denom y))))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
   (define (mul-rat x y)
     (make-rat (* (numer x) (numer y))
-	                  (* (denom x) (denom y))))
+	      (* (denom x) (denom y))))
   (define (div-rat x y)
     (make-rat (* (numer x) (denom y))
-	                  (* (denom x) (numer y))))
-
+	      (* (denom x) (numer y))))
+  
   ;; interface to rest of the system
   (define (tag x) (attach-tag 'rational x))
   (put 'add '(rational rational)
@@ -174,17 +174,17 @@
   ;; internal procedures
   (define (add-complex z1 z2)
     (make-from-real-imag (+ (real-part z1) (real-part z2))
-			   (+ (imag-part z1) (imag-part z2))))
+			 (+ (imag-part z1) (imag-part z2))))
   (define (sub-complex z1 z2)
     (make-from-real-imag (- (real-part z1) (real-part z2))
-			   (- (imag-part z1) (imag-part z2))))
+			 (- (imag-part z1) (imag-part z2))))
   (define (mul-complex z1 z2)
     (make-from-mag-ang (* (magnitude z1) (magnitude z2))
-		                     (+ (angle z1) (angle z2))))
+		       (+ (angle z1) (angle z2))))
   (define (div-complex z1 z2)
     (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
-		                     (- (angle z1) (angle z2))))
-
+		       (- (angle z1) (angle z2))))
+  
   ;; interface to rest of the system
   (define (tag z) (attach-tag 'complex z))
   (put 'add '(complex complex)
@@ -206,12 +206,12 @@
   ;; New Procedure
   ;; =============
   (put '=zero? '(complex) =zero?)
-
+  
   (put 'make-from-real-imag 'complex
        (lambda (x y) (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'complex
        (lambda (r a) (tag (make-from-mag-ang r a))))
-
+  
   (put 'real-part '(complex) real-part)
   (put 'imag-part '(complex) imag-part)
   (put 'magnitude '(complex) magnitude)
