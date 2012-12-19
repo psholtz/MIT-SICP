b8643c9fae929d2d965939cf6a3777ea62273aaa
psholtz <paul.sholtz@gmail.com>
2012-12-18 19:41:17 -0800

    Update Section-2.5/mit-scheme/exercise2-80.scm

--- Section-2.5/mit-scheme/exercise2-80.scm
+++ Section-2.5/mit-scheme/exercise2-80.scm
@@ -1,7 +1,7 @@
 ;;
 ;; Exercise 2.80
 ;;
-;; Define a generic predicate =zero? that tests if its argument is zero, and install 
+;; Define a generic predicate "=zero?" that tests if its argument is zero, and install 
 ;; it in the generic arithmetic package. This operation should work for ordinary numbers, 
 ;; rational numbers, and complex numbers.
 ;;
