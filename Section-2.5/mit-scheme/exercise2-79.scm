4f23bf1258efbe91438877830969e71dbc83e261
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-18 17:36:55 -0800

    updating scheme

--- Section-2.5/mit-scheme/exercise2-79.scm
+++ Section-2.5/mit-scheme/exercise2-79.scm
@@ -78,76 +78,65 @@
        (lambda (n d) (tag (make-rat n d))))
   'done)
 
-;; New rectangular package:
-(define (install-rectangular-package)
-  ;; internal procedures
-  (define (real-part z) (car z))
-  (define (imag-part z) (cdr z))
-  (define (make-from-real-imag x y) (cons x y))
-  (define (magnitude z)
-    (sqrt (+ (square (real-part z))
-	          (square (imag-part z)))))
-  (define (angle z)
-    (atan (imag-part z) (real-part z)))
-  (define (make-from-mag-ang r a)
-    (cons (* r (cos a)) (* r (sin a))))
-  
-  ;; interface to rest of the system
-  (define (tag x) (attach-tag 'rectangular x))
-  (put 'real-part '(rectangular) real-part)
-  (put 'imag-part '(rectangular) imag-part)
-  (put 'magnitude '(rectangular) magnitude)
-  (put 'angle '(rectangular) angle)
-  (put 'make-from-real-imag 'rectangular
-       (lambda (x y) (tag (make-from-real-imag x y))))
-
-  ;; =============
-  ;; New Procedure
-  ;; =============
-  (put 'equ? '(rectangular)
-       (lambda x y) (and (= (real-part x) (real-part y))
-			 (= (imag-part x) (imag-part y))))
-
-  (put 'make-from-mag-ang 'rectangular 
-       (lambda (r a) (tag (make-from-mag-ang r a))))
-  'done)
-
-;; New polar package:
-(define (install-polar-package)
-  ;; internal procedures
-  (define (magnitude z) (car z))
-  (define (angle z) (cdr z))
-  (define (make-from-mag-ang r a) (cons r a))
-  (define (real-part z)
-    (* (magnitude z) (cos (angle z))))
-  (define (imag-part z)
-    (* (magnitude z) (sin (angle z))))
+;;
+;; For the complex package, we can check equality by making 
+;; sure the "real" and "imag" parts of the complex number are 
+;; equivalent, and we can "intercept" this check at the top
+;; level of complex (i.e., no need to update the particular 
+;; polar and rectangular sub-packages). 
+;;
+;; Note that we will want to unit-test for the equivalency
+;; of complex numbers which are rotated through 2*pi.
+;;
+(define (install-complex-package)
+  ;; constructors
   (define (make-from-real-imag x y)
-    (cons (sqrt (+ (square x) (square y)))
-	    (atan y x)))
-  
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag 'polar x))
-  (put 'real-part '(polar) real-part)
-  (put 'imag-part '(polar) imag-part)
-  (put 'magnitude '(polar) magnitude)
-  (put 'angle '(polar) angle)
-  (put 'make-from-real-imag 'polar
-       (lambda (x y) (tag (make-from-real-imag x y))))
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
 
-  ;; =============
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+			  (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+			  (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+		              (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+		              (- (angle z1) (angle z2))))
+
+  ;; interface to rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  
+  ;; ============= 
   ;; New Procedure
   ;; =============
-  (put 'equ? '(polar) (lambda (x y)
-			(let ((PI 3.141592653589793238462))
-			  (let ((kx (floor (/ (angle x) (* 2 PI))))
-				(ky (floor (/ (angle y) (* 2 PI)))))
-			    (and (= (magnitude x) (magnitude y))
-				 (= (- (angle x) (* kx 2 PI))
-				    (- (angle y) (* ky 2 PI))))))))
+  (put 'equ? '(complex)
+       (lambda (x y) (and (= (real-part x) (real-part y))
+			  (= (imag-part x) (imag-part y)))))
 
-  (put 'make-from-mag-ang 'polar
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
        (lambda (r a) (tag (make-from-mag-ang r a))))
+
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+
   'done)
 
 ;;
@@ -155,3 +144,4 @@
 ;;
 (install-scheme-number-package)
 (install-rational-package)
+(install-complex-package)
