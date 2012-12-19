8dea0802852174ee45133a6b089e481caafe9e54
Paul Sholtz <paul.sholtz@gmail.com>
2012-12-18 18:07:48 -0800

    updating scheme

--- Section-2.5/mit-scheme/exercise2-79.scm
+++ Section-2.5/mit-scheme/exercise2-79.scm
@@ -1,7 +1,9 @@
 ;;
 ;; Exercise 2.79
 ;;
-;; [WORKGIN]
+;; Define a generic equality predicate equ? that tests the equality of two numbers, and 
+;; install it in the generic arithmetic package. This operation should work for ordinary 
+;; numbers, rational numbers, and complex numbers.
 ;;
 
 ;; =================
