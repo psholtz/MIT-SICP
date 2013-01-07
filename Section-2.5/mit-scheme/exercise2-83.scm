e22193e1131c574742aa65ef62fa7fa17c707246
psholtz <paul.sholtz@gmail.com>
2013-01-07 11:43:49 -0800

    updating scheme

--- Section-2.5/mit-scheme/exercise2-83.scm
+++ Section-2.5/mit-scheme/exercise2-83.scm
@@ -10,6 +10,35 @@
 ;;
 ;;  integer --> rational --> real --> complex
 ;;
+;; We do not presently have an "integer" or "real" number package. Indeed, the 
+;; "scheme-number" package could be used as either, perhaps more appropriately 
+;; as "real" (since it handles reals), but to simply "substitute" the type 
+;; "scheme-number" in the above tower at two places would introduce ungainly 
+;; complications and cycles in our tower graph.
+;;
+;; Let's instead simply agree to call the our present scheme-number package 
+;; as our "real" type, and we'll define a new "integer" package which 
+;; supports integer operations. Not that the division operation is not supported, 
+;; since integers in general are not closed under division.
+;;
+(define (install-integer-package)
+  (define (tag x)
+    (attach-tag 'integer x))
+  
+  (put 'add '(integer integer)
+       (lambda (x y) (tag (+ x y))))
+
+;;
+;; We will also define a coercion procedure, to coerce the integers into 
+;; scheme-numbers, should this be required:
+;;
+(define (integer->scheme-number n)
+  (make-scheme-number n))
+
+(put-coercion 'integer 'scheme-number integer->scheme-number)
+
+
+;;
 ;; However, we have not hitherto generated "integer" and "real" packages, and, 
 ;; on the contrary, both "integer" and "real" types are handled uniformly by 
 ;; our "scheme-number" package. 
@@ -27,11 +56,27 @@
 ;; rational representation to a real-number representation as a scheme-number).
 ;;
 
+;; The scheme number will be our "real".
+
+
+(define (install-integer-package)
+  (define (tag x)
+    (attach-tag 'integer x))
+  
+  (put 'add '(integer integer)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(integer integer)
+       (lambda (x y)
+;;
+;; Let's also coerce integers into being scheme-numbers:
+;;
+
+
 ;;
 ;; This method uses the "exact-integer?" procedure which is built into MIT Scheme.
 ;;
 (define (raise-scheme-number->rational n)
-  ;; Helper method
+  ;; Helper method (go straight to complex)
   (define (raise-scheme-number->complex n)
     (make-complex-from-real-imag n 0.0))
 
@@ -68,20 +113,10 @@
 ;;
 ;; Run some unit tests:
 ;;
-(raise-scheme-number->complex (make-scheme-number 3))
-;; ==> (complex rectangular 3 . 0.)
-(raise-scheme-number->complex (make-scheme-number 3.14))
-;; ==> (complex rectangular 3.14 . 0.)
-
 (raise-scheme-number->rational (make-scheme-number 3))
 ;; ==> (rational 3 . 1)
 (raise-scheme-number->rational (make-scheme-number 3.14))
 ;; ==> (complex rectangular 3.14 . 0.)
-
-(lower-rational->scheme-number (make-rational 3 1))
-;; ==> 3.
-(lower-rational->scheme-number (make-rational 5 4))
-;; ==> 1.25
 
 (raise-rational->complex (make-rational 3 1))
 ;; ==> (complex rectangular 3. . 0.)
@@ -97,5 +132,6 @@
 ;; Update the operations table:
 ;;
 (put 'raise '(scheme-number) raise-scheme-number->rational)
-(put 'raise '(rational) raise-rational->complex)
+(put 'raise '(rational)
+     (lambda (z) (attach-tag 'complex (raise-rational->complex z))))
     
