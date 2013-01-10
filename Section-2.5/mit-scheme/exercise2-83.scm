e8d0eb254c570b0f35f1c0648c3b70992374b3de
psholtz <paul.sholtz@gmail.com>
2013-01-10 15:53:40 -0800

    updating scheme

--- Section-2.5/mit-scheme/exercise2-83.scm
+++ Section-2.5/mit-scheme/exercise2-83.scm
@@ -34,8 +34,13 @@
        (lambda (x y) (= x y)))
   (put '=zero? '(integer)
        (lambda (p) (= p 0)))
+  ;;
+  ;; Check to make sure its an integer, 
+  ;; otherwise round to nearest integer:
+  ;;
   (put 'make 'integer
-       (lambda (x) (tag x)))
+       (lambda (x) (tag (if (integer? x) x (round x)))))
+
   'done)
 
 (define (make-integer n)
@@ -53,7 +58,11 @@
 
 ;;
 ;; We will also define a coercion procedure, to coerce the integers into 
-;; scheme-numbers, should this be required:
+;; scheme-numbers, should this be required (strictly speaking, this is 
+;; overkill, since for both "scheme-number" and "integer" the tags are 
+;; removed and the interpreter manipulates these symbols as per the rules
+;; of standard arithmetic .. but for the sake of completeness and consistency,
+;; let's define and install these methods):
 ;;
 (define (integer->scheme-number n)
   (make-scheme-number n))
@@ -65,107 +74,128 @@
 ;;
 (define i1 (make-integer 2))
 (define i2 (make-integer 3))
+(define i3 (make-integer 0))
 
 (add i1 i2)
-;; ==>
+;; ==> 5
 (sub i1 i2)
-;; ==>
+;; ==> -1
 (mul i1 i2)
-;; ==>
-
-
-;;
-;; However, we have not hitherto generated "integer" and "real" packages, and, 
-;; on the contrary, both "integer" and "real" types are handled uniformly by 
-;; our "scheme-number" package. 
-;;
-;; Accordingly, we will define two hierarchies that looks something more like:
-;;
-;;  scheme-number ---> rational ---> complex
-;;
-;;
-;; [WORKING --> rewrite this part]
-;;
-;; We will moreover define "helper" procedures that raise a scheme-number 
-;; directly to a complex (if the scheme number is not a rational number), and 
-;; which "lower" a rational number to a scheme-number (i.e., convert the 
-;; rational representation to a real-number representation as a scheme-number).
-;;
-
-;; The scheme number will be our "real".
-
-
-(define (install-integer-package)
-  (define (tag x)
-    (attach-tag 'integer x))
-  
-  (put 'add '(integer integer)
-       (lambda (x y) (tag (+ x y))))
-  (put 'sub '(integer integer)
-       (lambda (x y)
-;;
-;; Let's also coerce integers into being scheme-numbers:
-;;
-
+;; ==> 6
+(exp i1 i2)
+;; ==> 8
+(equ? i1 i2)
+;; ==> #f
+(equ? i1 i1)
+;; ==> #t
+(=zero? i1)
+;; ==> #f
+(=zero? i3)
+;; ==> #t
 
 ;;
-;; This method uses the "exact-integer?" procedure which is built into MIT Scheme.
+;; The "coercion" allows us to divide integers anyway (even though we didn't
+;; define the procedure for this type):
 ;;
-(define (raise-scheme-number->rational n)
-  ;; Helper method (go straight to complex)
-  (define (raise-scheme-number->complex n)
-    (make-complex-from-real-imag n 0.0))
-
-  ;; Raise depending on the type
-  (if (exact-integer? n)
-      (make-rational n 1)
-      (raise-scheme-number->complex n)))
+(div i1 i2)
+;; ==> 2/3
 
 ;;
-;; Raise a rational to complex by first converting it to a real 
-;; (i.e., scheme-number):
+;; Let's see if we only get "integers" when constructing integers:
 ;;
-(define (raise-rational->complex r)
-  ;; Helper method (determines if the argument is of type rational)
-  (define (is-rational? x)
-    (if (pair? x)
-	(let ((kind (car x)))
-	  (eq? kind 'rational))
-	#f))
-
-  ;; Helper method (lower the rational down to real/scheme-number)
-  (define (lower-rational->scheme-number x)
-    (let ((n (numer x))
-	  (d (denom x)))
-      (make-scheme-number (/ (* 1.0 n) (* 1.0 d)))))
-
-  ;; Raise to complex
-  (if (is-rational? r)
-      (let ((x (lower-rational->scheme-number r)))
-	(make-complex-from-real-imag x 0.0))
-      ;; default, just return the value
-      r))
+(make-integer 2)
+;; ==> 2
+(make-integer 2.1)
+;; ==> 2.
+(make-integer 2.6)
+;; ==> 3.
 
 ;;
-;; Run some unit tests:
+;; Let's see if we can combine integers with scheme-numbers (of course we can, 
+;; for the reasons cited above, but still run the unit tests):
 ;;
-(raise-scheme-number->rational (make-scheme-number 3))
-;; ==> (rational 3 . 1)
-(raise-scheme-number->rational (make-scheme-number 3.14))
-;; ==> (complex rectangular 3.14 . 0.)
+(define s1 (make-scheme-number 2))
+(define s2 (make-scheme-number 3))
 
-(raise-rational->complex (make-rational 3 1))
-;; ==> (complex rectangular 3. . 0.)
-(raise-rational->complex (make-rational 5 4))
-;; ==> (complex rectangular 1.25 . 0.)
+(add i1 s2)
+;; ==> 5
+(add s2 i1)
+;; ==> 5
+
+(sub i1 s2)
+;; ==> -1
+(sub s2 i1)
+;; ==> 1
+
+(mul i1 s2)
+;; ==> 6
+(mul s2 i1)
+;; ==> 6
+
+(exp i1 s2)
+;; ==> 8
+(exp s2 i1)
+;; ==> 9
+
+(equ? i1 s2)
+;; ==> #f
+(equ? s2 i1)
+;; ==> #f
+
+(equ? i1 s1)
+;; ==> #t
+(equ? s1 i1)
+;; ==> #t
 
 ;;
-;; We define the generic "raise" procedure:
+;; As per "coercion", we can even perform division:
+;;
+(div i1 s2)
+;; ==> 2/3
+(div s2 i1)
+;; ==> 3/2
+
+;;
+;; Now let's define our "raise" procedures:
+;;
+(define (raise-integer->rational n)
+  (make-rational n 1))
+
+(define (raise-rational->scheme-number r)
+  (let ((n (numer r))
+	(d (denom r)))
+    (make-scheme-number (exact->inexact (/ n d)))))
+
+(define (raise-scheme-number->complex n)
+  (make-complex-from-real-imag n 0))
+
+;;
+;; Run through some unit tests:
+;;
+(raise-integer->rational (make-integer 2))
+;; ==> (rational 2 . 1)
+(raise-integer->rational (make-integer 2.1))
+;; ==> (rational 2. . 1.)
+(raise-integer->rational (make-integer 2.6))
+;; ==> (rational 3. . 1.)
+
+(raise-rational->scheme-number (make-rational 1 2))
+;; ==> 0.5
+(raise-rational->scheme-number (make-rational 3 1))
+;; ==> 3.
+
+(raise-scheme-number->complex 3)
+;; ==> (complex rectangular 3 . 0)
+(raise-scheme-number->complex 3.14)
+;; ==> (complex rectangular 3.14 . 0)
+
+;;
+;; We now define the generic "raise" procedure:
 ;;
 (define (raise x) (apply-generic 'raise x))
 
 ;;
-;; Update the operations table:
+;; And we update the operations table:
 ;;
 (put 'raise '(scheme-number) raise-scheme-number->rational)
 (put 'raise '(rational)
