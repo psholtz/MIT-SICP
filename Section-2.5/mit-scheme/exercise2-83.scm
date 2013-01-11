2643f5cb677918d6aff4308f90ef442d993d14a8
psholtz <paul.sholtz@gmail.com>
2013-01-11 13:10:38 -0800

    updating swcheme

--- Section-2.5/mit-scheme/exercise2-83.scm
+++ Section-2.5/mit-scheme/exercise2-83.scm
@@ -1,8 +1,11 @@
 ;; 
 ;; Exercise 2.83
 ;;
-;; Suppose you are designing a generic arithmetic system for dealing with ...
-;; [WORKING]
+;; Suppose you are designing a generic arithmetic system for dealing with the tower of 
+;; types shown in Figure 2.25: integer, rational, real, complex. For each type (except
+;; complex), design a procedure that raises objects of that type one level in the tower.
+;; Show how to install a generic "raise" operation that will work for each type 
+;; (except complex).
 ;;
 
 ;;
