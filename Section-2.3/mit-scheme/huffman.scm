9b83911a0150bd3a9998833c7d296b3f5eb30beb
Paul Sholtz <paul.sholtz@gmail.com>
2013-06-05 19:18:34 -0700

    updating huffman

--- Section-2.3/mit-scheme/huffman.scm
+++ Section-2.3/mit-scheme/huffman.scm
@@ -1,5 +1,14 @@
 ;;
-;; Procedures for generating Huffman trees:
+;; Huffman codes are an effective technique for compressing data, savings of 20% to 90% are typical. 
+;; The Huffman algorithm constructs a variable-length prefix-code based on a table of the frequencies
+;; of occurrence of each character in a file, and builds up an optimal way of representing each character
+;; as a (variable-length) binary string. Frequently-occurring characters are given short codes, while 
+;; rarely-occurring characters are given long codes. The optimal code for a given file is always represented 
+;; as a FULL binary tree, where every non-leaf node has two children. 
+;;
+
+;;
+;; Procedures for generating the leaves of a Huffman tree:
 ;;
 (define (make-leaf symbol weight)
   (list 'leaf symbol weight))
@@ -8,17 +17,8 @@
 (define (symbol-leaf x) (cadr x))
 (define (weight-leaf x) (caddr x))
 
-(define (symbols tree)
-  (if (leaf? tree)
-      (list (symbol-leaf tree))
-      (caddr tree)))
-(define (weight tree)
-  (if (leaf? tree)
-      (weight-leaf tree)
-      (cadddr tree)))
-
 ;;
-;; Constructing and manipulating the tree:
+;; Procedures for constructing and manipulating the Huffman tree:
 ;;
 (define (make-code-tree left right)
   (list left
@@ -28,6 +28,14 @@
 
 (define (left-branch tree) (car tree))
 (define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
 (define (choose-branch bit branch)
   (cond ((= bit 0) (left-branch branch))
 	((= bit 1) (right-branch branch))
@@ -35,7 +43,7 @@
 	 (error "Bad bit -- CHOOSE BRANCH" bit))))
 
 ;;
-;; Decoding using the tree:
+;; Procedures for decoding messages using a Huffman tree:
 ;;
 (define (decode bits tree)
   (define (decode-1 bits current-branch)
@@ -50,7 +58,7 @@
   (decode-1 bits tree))
 
 ;;
-;; Encoding using the tree:
+;; Procedures for encoding messages using a Huffman tree:
 ;;
 (define (element-of-set? x set)
   (cond ((null? set) false)
@@ -58,6 +66,7 @@
 	(else 
 	 (element-of-set? x (cdr set)))))
 
+;; (If your Scheme doesn't ship with a "reverse" procedure, this should work:
 (define (reverse items)
   (define (reverse-iter lst1 lst2)
     (if (null? lst1)
@@ -66,18 +75,18 @@
   (reverse-iter items '()))
 
 (define (encode-symbol symbol tree)
-  (define (encode-symbol-iter working total)
-    (if (leaf? working)
-	(reverse total)
-	(let ((symbols-left (symbols (left-branch working)))
-	      (symbols-right (symbols (right-branch working))))
+  (define (encode-1 symbol-list encoded)
+    (if (leaf? symbol-list)
+	(reverse encoded)
+	(let ((symbols-left (symbols (left-branch symbol-list)))
+	      (symbols-right (symbols (right-branch symbol-list))))
 	  (cond ((element-of-set? symbol symbols-left)
-		 (encode-symbol-iter (left-branch working) (cons 0 total)))
+		 (encode-1 (left-branch symbol-list) (cons 0 encoded)))
 		((element-of-set? symbol symbols-right)
-		 (encode-symbol-iter (right-branch working) (cons 1 total)))
-		(else 
+		 (encode-1 (right-branch symbol-list) (cons 1 encoded)))
+		(else
 		 (error "Bad symbol: ENCODE-SYMBOL" symbol))))))
-  (encode-symbol-iter tree '()))
+  (encode-1 tree '()))
 
 (define (encode message tree)
   (if (null? message)
@@ -86,7 +95,7 @@
 	      (encode (cdr message) tree))))
 
 ;;
-;; Generating the tree itself
+;; Procedures for generating the Huffman tree itself:
 ;;
 (define (adjoin-set x set)
   (cond ((null? set) (list x))
@@ -103,18 +112,27 @@
 			       (cadr pair))  ;; frequency
 		    (make-leaf-set (cdr pairs))))))
 
-(define (accumulate op init seq)
-  (if (null? seq)
-      init
-      (op (car seq)
-	  (accumulate op init (cdr seq)))))
+;;(define (accumulate op init seq)
+;;  (if (null? seq)
+;;      init
+;;      (op (car seq)
+;;	  (accumulate op init (cdr seq)))))
+
+;;(define (successive-merge pairs)
+;;  (define (successive-merge-lambda a b)
+;;    (if (null? b)
+;;	a
+;;	(make-code-tree a b)))
+;;  (accumulate successive-merge-lambda '() (reverse pairs)))
 
 (define (successive-merge pairs)
-  (define (successive-merge-lambda a b)
-    (if (null? b)
-	a
-	(make-code-tree a b)))
-  (accumulate successive-merge-lambda '() (reverse pairs)))
+  (if (= (length pairs) 1)
+      (car pairs)
+      (let ((first (car pairs))
+	    (second (cadr pairs))
+	    (rest (cddr pairs)))
+	(successive-merge (adjoin-set (make-code-tree first second)
+				      rest)))))
 
 (define (generate-huffman-tree pairs)
   (successive-merge (make-leaf-set pairs)))
