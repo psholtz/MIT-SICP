316fe98b904b028b504f7d25d101ca3d6b1e4457
Paul Sholtz <paul.sholtz@gmail.com>
2013-06-05 23:05:07 -0700

    updating

--- Section-2.3/mit-scheme/exercise2-69.scm
+++ Section-2.3/mit-scheme/exercise2-69.scm
@@ -17,7 +17,11 @@
 ;;
 
 ;;
-;; First let's import all the prior procedures:
+;; First let's reimport all the same Huffman tree code we used before.
+;;
+
+;;
+;; Procedures for generating the leaves of a Huffman tree:
 ;;
 (define (make-leaf symbol weight)
   (list 'leaf symbol weight))
@@ -26,86 +30,74 @@
 (define (symbol-leaf x) (cadr x))
 (define (weight-leaf x) (caddr x))
 
-(define (symbols tree)
-  (if (leaf? tree)
-      (list (symbol-leaf tree))
-      (caddr tree)))
-
-(define (weight tree)
-  (if (leaf? tree)
-      (weight-leaf tree)
-      (cadddr tree)))
-
+;;
+;; Procedures for constructing and manipulating the Huffman tree:
+;;
 (define (make-code-tree left right)
   (list left
 	right
 	(append (symbols left) (symbols right))
 	(+ (weight left) (weight right))))
-
 (define (left-branch tree) (car tree))
 (define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
 
 ;;
 ;; Procedures for decoding symbols:
 ;;
-(define (choose-branch bit branch)
-  (cond ((= bit 0) (left-branch branch))
-	((= bit 1) (right-branch branch))
-	(else
-	   (error "Bad bit -- CHOOSE BRANCH" bit))))
-
 (define (decode bits tree)
   (define (decode-1 bits current-branch)
     (if (null? bits)
 	'()
 	(let ((next-branch 
-	                     (choose-branch (car bits) current-branch)))
-	      (if (leaf? next-branch)
-		        (cons (symbol-leaf next-branch)
-			              (decode-1 (cdr bits) tree))
-			            (decode-1 (cdr bits) next-branch)))))
+	       (choose-branch (car bits) current-branch)))
+	  (if (leaf? next-branch)
+	      (cons (symbol-leaf next-branch)
+		    (decode-1 (cdr bits) tree))
+	      (decode-1 (cdr bits) next-branch)))))
   (decode-1 bits tree))
 
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+	((= bit 1) (right-branch branch))
+	(else
+	 (error "Bad bit -- CHOOSE BRANCH" bit))))
+
 ;;
-;; Define the encoding procedures:
+;; Procedures for encoding messages using a Huffman tree:
 ;;
-(define (element-of-set? x set)
-  (cond ((null? set) false)
-	((equal? x (car set)) true)
-	(else 
-	  (element-of-set? x (cdr set)))))
-  
-(define (encode-symbol symbol tree)
-
-  ;; Version of reverse that runs in O(n) linear time
-  (define (reverse items)
-    (define (reverse-iter lst1 lst2)
-      (if (null? lst1)
-	    lst2
-	      (reverse-iter (cdr lst1) (cons (car lst1) lst2))))
-    (reverse-iter items '()))
-
-  ;; Build up the encoding using constant time "cons", 
-  ;; and then reverse the list once done.. should be 
-  ;; faster than repeatedly invoking "append".
-  (define (encode-symbol-iter working total)
-    (if (leaf? working)
-	(reverse total)
-	(let ((symbols-left (symbols (left-branch working)))
-	            (symbols-right (symbols (right-branch working))))
-	    (cond ((element-of-set? symbol symbols-left)
-		    (encode-symbol-iter (left-branch working) (cons 0 total)))
-		  ((element-of-set? symbol symbols-right)
-		    (encode-symbol-iter (right-branch working) (cons 1 total)))
-		  (else 
-		    (error "Bad symbol: ENCODE-SYMBOL" symbol))))))
-  (encode-symbol-iter tree '()))
-
 (define (encode message tree)
   (if (null? message)
       '()
       (append (encode-symbol (car message) tree)
-	            (encode (cdr message) tree))))
+	      (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (define (encode-1 symbol-list encoded)
+    (if (leaf? symbol-list)
+	(reverse encoded)
+	(let ((symbols-left (symbols (left-branch symbol-list)))
+	      (symbols-right (symbols (right-branch symbol-list))))
+	  (cond ((element-of-set? symbol symbols-left)
+		 (encode-1 (left-branch symbol-list) (cons 0 encoded)))
+		((element-of-set? symbol symbols-right)
+		 (encode-1 (right-branch symbol-list) (cons 1 encoded)))
+		(else
+		 (error "Bad symbol: ENCODE-SYMBOL" symbol))))))
+  (encode-1 tree '()))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else 
+	   (element-of-set? x (cdr set)))))
 
 ;;
 ;; In addition, we require two methods defined in the text for building up leaf-sets:
