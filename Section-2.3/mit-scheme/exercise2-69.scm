9191cb14057b88a562e01b31833fb72f4923d5bf
Paul Sholtz <paul.sholtz@gmail.com>
2013-06-07 16:18:50 -0700

    updating

--- Section-2.3/mit-scheme/exercise2-69.scm
+++ Section-2.3/mit-scheme/exercise2-69.scm
@@ -201,4 +201,4 @@
 (successive-merge (adjoin-set (make-code-tree (leaf a 4) ((leaf b 2) ((leaf d 1) (leaf c 1) (d c) 2) (b d c) 4)) ()))
 (successive-merge (adjoin-set ((leaf a 4) ((leaf b 2) ((leaf d 1) (leaf c 1) (d c) 2) (b d c) 4) (a b d c) 8) ()))
 (successive-merge (((leaf a 4) ((leaf b 2) ((leaf d 1) (leaf c 1) (d c) 2) (b d c) 4) (a b d c) 8)))
-(((leaf a 4) ((leaf b 2) ((leaf d 1) (leaf c 1) (d c) 2) (b d c) 4) (a b d c) 8))
+;; ==> (((leaf a 4) ((leaf b 2) ((leaf d 1) (leaf c 1) (d c) 2) (b d c) 4) (a b d c) 8))
