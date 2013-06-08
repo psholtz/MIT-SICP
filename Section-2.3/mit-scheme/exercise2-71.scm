08845a284394d309027b454df892da6545cef0c0
Paul Sholtz <paul.sholtz@gmail.com>
2013-06-08 12:37:58 -0700

    updating

--- Section-2.3/mit-scheme/exercise2-71.scm
+++ Section-2.3/mit-scheme/exercise2-71.scm
@@ -54,66 +54,49 @@
 ;;
 (successive-merge '((leaf a 1) (leaf b 2) (leaf c 4) (leaf d 8) (leaf e 16)))
 
-
-
-(define tree2 (generate-huffman-tree '((a 1) (b 2) (c 4) (d 8) (e 16) (f 32) (g 64) (h 128) (i 256) (j 512))))
-
-(encode-symbol 'a tree2)
-;; ==> (1 1 1 1 1 1 1 1 1)
-(encode-symbol 'b tree2)
-;; ==> (1 1 1 1 1 1 1 1 0)
-(encode-symbol 'c tree2)
-;; ==> (1 1 1 1 1 1 1 0)
-(encode-symbol 'd tree2)
-;; ==> (1 1 1 1 1 1 0)
-(encode-symbol 'e tree2)
-;; ==> (1 1 1 1 1 0)
-(encode-symbol 'f tree2)
-;; ==> (1 1 1 1 0)
-(encode-symbol 'g tree2)
-;; ==> (1 1 1 0)
-(encode-symbol 'h tree2)
-;; ==> (1 1 0)
-(encode-symbol 'i tree2)
-;; ==> (1 0)
-(encode-symbol 'j tree2)
-;; ==> (0)
+;;
+;; Recall the definition of "successive-merge":
+;;
+(define (successive-merge pairs)
+  (if (= (length pairs) 1)
+      (car pairs)
+      (let ((first (car pairs))
+	    (second (cadr pairs))
+	    (rest (cddr pairs)))
+	(successive-merge (adjoin-set (make-code-tree first second)
+				      rest)))))
 
 ;;
-;; The tree looks like this:
+;; Evaluating the formation of the Huffman tree:
 ;;
-;;     (root) 
-;;      /  \
-;;     /    \
-;; (j 512)  () 
-;;         /  \
-;;        /    \
-;;    (i 256)  () 
-;;            /  \
-;;           /    \
-;;       (h 128)  ()
-;;               /  \
-;;              /    \
-;;          (g 64)   () 
-;;                  /  \
-;;                 /    \
-;;              (f 32)  ()
-;;                     /  \
-;;                    /    \
-;;                 (e 16)  ()
-;;                        /  \
-;;                       /    \
-;;                    (d 8)   () 
-;;                           /  \
-;;                          /    \
-;;                       (c 4)   ()
-;;                              /  \
-;;                             /    \
-;;                          (b 2)  (a 1)
+(successive-merge '((leaf a 1) (leaf b 2) (leaf c 4) (leaf d 8) (leaf e 16)))
+;; first <- (leaf a 1)
+;; second <- (leaf b 2)
+;; rest <- ((leaf c 4) (leaf d 8) (leaf e 16))
+(successive-merge '(((leaf a 1) (leaf b 2) (a b) 3) (leaf c 4) (leaf d 8) (leaf e 16)))
+
+;;
+;; After the first iteration, we can see that when the two smallest leaves are combined (i.e., Leaf A with 
+;; weight 1, and Leaf B with weight 2), the combined weight of the new combination is only 3, which is one
+;; less than the next leaf (i.e., Leaf C with weight 4). This pattern will repeat until the procedure
+;; terminates, that is, the result of each successive combination will be a combined leaf whose accumulated
+;; weight is just one short of the weight of the next leaf in the (priority) queue. 
+;;
+;; Consequently, the structure of the Huffman tree structure will always be similar to that given above.
+;;
+;; Finishing the evaluation:
+;; 
+(successive-merge '((((leaf a1) (leaf b2) (a b) 3) (leaf c 4) (a b c) 7) (leaf d 8) (leaf e 16)))
+(successive-merge '(((((leaf a 1) (leaf b 2) (a b) 3) (leaf c 4) (a b c) 7) (leaf d 8) (a b c d) 15) (leaf e 16)))
+(successive-merge '((((((leaf a 1) (leaf b 2) (a b) 3) (leaf c 4) (a b c) 7) (leaf d 8) (a b c d) 15) (leaf e 16) (a b c d e) 31)))
+;; ==> ((((((leaf a 1) (leaf b 2) (a b) 3) (leaf c 4) (a b c) 7) (leaf d 8) (a b c d) 15) (leaf e 16) (a b c d e) 31))
+
+;;
+;; At each iteration, the combined weight of the smallest node is 1 less than the weight of the next leaf
+;; in the priority queue.
 ;;
 
 ;;
-;; For a general tree of size n, (n-1) bits are required to encode the 
-;; least frequent symbol, and 1 bit is required to encode the most 
-;; frequent symbol.
+;; For a general tree of size n, (n-1) bits are required to encode the least frequent symbol, and 1 bit is 
+;; required to encode the most frequent symbol.
 ;;
