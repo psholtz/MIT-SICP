ea965395652568fe8ce8ada6730b3888d70ca3c3
Paul Sholtz <paul.sholtz@gmail.com>
2014-12-30 00:51:15 -0800

    Updating emacs

--- Section-2.1/emacs/exercise2-08.el
+++ Section-2.1/emacs/exercise2-08.el
@@ -41,32 +41,32 @@
 ;;
 ;; Now let us construct the substraction procedure. 
 ;;
-;; First we will define a "negative" procedure, that creates an arithmetical 
+;; First we will define a "neg-internal" procedure, that creates an arithmetical 
 ;; inverse for a given interval. Then we will add this "negative" to the 
 ;; first element of the combination, to arrive at the difference between the two elements.
 ;;
-(defun negative (interval)
+(defun neg-interval (interval)
   (make-interval (* -1 (upper-bound interval))
 		 (* -1 (lower-bound interval))))
 
 ;;
 ;; Do a few use cases, to verify that we are getting the negative:
 ;;
-(negative (make-interval 3 5))
+(neg-interval (make-interval 3 5))
 ;; ==> (-5 . -3)
 
-(negative (make-interval 0 3))
+(neg-interval (make-interval 0 3))
 ;; ==> (-3 . 0)
 
-(negative (make-interval -5 -3))
+(neg-interval (make-interval -5 -3))
 ;; ==> (3 . 5)
 
-(negative (make-interval -3 0))
+(neg-interval (make-interval -3 0))
 ;; ==> (0 . 3)
 
 ;;
 ;; Now define the subtraction procedure:
 ;;
 (defun sub-interval (x y)
-  (let ((z (negative y)))
+  (let ((z (neg-interval y)))
     (add-interval x z)))
