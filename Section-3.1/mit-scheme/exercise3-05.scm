2168265bc79dd1316b4fd2b8eba8a04f39d70d5c
Paul Sholtz <paul.sholtz@gmail.com>
2015-02-16 20:33:23 -0800

    Updating scheme

--- Section-3.1/mit-scheme/exercise3-05.scm
+++ Section-3.1/mit-scheme/exercise3-05.scm
@@ -9,28 +9,97 @@
     (+ low (random range))))
 
 ;;
+;; Define supporting procedures:
+;;
+(define (square x) (* x x))
+
+;;
+;; Define circle predicates. 
+;;
+;; The following procedure returns a predicate for testing whether the 
+;; point (x,y) is within the circle of radius r centered at point (a,b):
+;;
+(define (predicate-for-circle a b r)
+  (lambda (x y)
+    (let ((dx (- a x))
+	  (dy (- b y)))
+      (let ((distance-from-center (sqrt (+ (square dx) (square dy)))))
+
+	;; Inside the circle only if distance-to-center is less than radius
+	(<= distance-from-center r)))))
+
+(define predicate-for-unit-circle
+  (predicate-for-circle 0 0 1))
+
+;;
+;; Import geometry package from Section 2.1:
+;;
+(define (make-point x y)
+  (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (make-segment start-point end-point)
+  (cons start-point end-point))
+(define (start-segment s)
+  (car s))
+(define (end-segment s)
+  (cdr s))
+(define (distance-points p1 p2)
+  (sqrt (+ (square (- (x-point p1) (x-point p2)))
+	   (square (- (y-point p1) (y-point p2))))))
+(define (length-segment s)
+  (distance-points (start-segment s) (end-segment s)))
+  
+(define (make-rect upper-left-x upper-left-y lower-right-x lower-right-y)
+  (cons (make-point upper-left-x upper-left-y)
+	(make-point lower-right-x lower-right-y)))
+(define (upper-left-rect rect)
+  (car rect))
+(define (lower-right-rect rect)
+  (cdr rect))
+(define (width-rect rect)
+  (let ((p1 (upper-left-rect rect))
+	(p2 (lower-right-rect rect)))
+    (let ((p3 (make-point
+	       (x-point p2)
+	       (y-point p1))))
+      (length-segment (make-segment p1 p3)))))
+(define (height-rect rect)
+  (let ((p1 (upper-left-rect rect))
+	(p2 (lower-right-rect rect)))
+    (let ((p3 (make-point
+	       (x-point p1)
+	       (y-point p2))))
+      (length-segment (make-segment p1 p3)))))
+(define (perimeter-rect rect)
+  (+ (* 2 (width-rect rect)) (* 2 (height-rect rect))))
+(define (area-rect rect)
+  (* (width-rect rect) (height-rect rect)))
+
+;;
 ;; Define predicate for a circle (at origin) with a given radius:
 ;;
-(define (circle-with-radius radius)
-  (lambda (x y)
-    (let ((distance (sqrt (+ (* x x) (* y y)))))
-      (<= distance radius))))
+;;(define (circle-with-radius radius)
+;;  (lambda (x y)
+;;    (let ((distance (sqrt (+ (* x x) (* y y)))))
+;;      (<= distance radius))))
 
 ;;
 ;; Define predicate for the unit circle:
 ;;
-(define unit-circle (circle-with-radius 1))
+;;(define unit-circle (circle-with-radius 1))
 
 ;;
 ;; Use "interval arthimetic" of Chapter 2 for representating intervals:
 ;;
-(define (make-interval a b)
-  (cond ((< a b) (cons a b))
-	(else
-	 (display "error constructing interval!"))))
-(define (lower-bound x) (car x))
-(define (upper-bound x) (cdr x))
-(define (range x) (- (upper-bound x) (lower-bound x)))
+;;(define (make-interval a b)
+;;  (cond ((< a b) (cons a b))
+;;	(else
+;;	 (display "error constructing interval!"))))
+;;(define (lower-bound x) (car x))
+;;(define (upper-bound x) (cdr x))
+;;(define (range x) (- (upper-bound x) (lower-bound x)))
 
 ;;
 ;; Define the monte-carlo integration:
