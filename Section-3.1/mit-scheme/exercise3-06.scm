4a4a3ce7d7184aa9f6d046198bc8a390cfcb62d2
Paul Sholtz <paul.sholtz@gmail.com>
2015-02-17 21:24:38 -0800

    updating scheme

--- Section-3.1/mit-scheme/exercise3-06.scm
+++ Section-3.1/mit-scheme/exercise3-06.scm
@@ -1,4 +1,15 @@
-;; [working]
+;; 
+;; Exercise 3.6
+;;
+;; It is useful to be able to reset a random-number generate to produce a
+;; sequence starting from a given value. Design a new "rand" procedure that
+;; is called with an argument that is either the symbol "generate" or the 
+;; symbol "reset" and behaves as follows: (rand 'generate) produces a new 
+;; random number; ((rand 'reset) <new-value>) resets the internal state 
+;; variable to the designated <new-value>. Thus, by resetting the state, 
+;; one can generate repeatable sequences. These are very handy to have when
+;; testing and debugging programs that use random numbers.
+;;
 
 ;; 
 ;; One simple way to implement a PRNG is to use a linear congruential 
