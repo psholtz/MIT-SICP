f84c38edbf0c798f2573cdb86866367e8f8ad6e9
Paul Sholtz <paul.sholtz@gmail.com>
2015-02-19 17:12:46 -0800

    updating scheme

--- Section-3.1/mit-scheme/exercise3-07.scm
+++ Section-3.1/mit-scheme/exercise3-07.scm
@@ -1,4 +1,23 @@
-;; [working]
+;;
+;; Exercise 3.7
+;;
+;; Consider the bank account objects created by "make-account", with the 
+;; password modification described in exercise 3.3. Suppose that our banking
+;; system requires the ability to make joint accounts. Define a procedure 
+;; "make-joint" that accomplishes this. "make-joint" should take three 
+;; arguments. The first is a password-protected account. The second argument
+;; must match the password with which the account was defined in order for 
+;; the "make-joint" operation to proceed. The third argument is a new password.
+;; "make-joint" is to create an additional access to the original account 
+;; using the new password. For example, if "peter-acc" is a bank account with
+;; password "open-sesame", then
+;;
+;;  (define paul-acc (make-joint peter-acc 'open-sesame 'rosebud))
+;;
+;; will allow one to make transactions on "peter-acc" using the name "paul-acc"
+;; and the password "rosebud". You may wish to modify your solution to 
+;; exercise 3.3 to accommodate this new feature.
+;;
 
 ;;
 ;; We make the following modifications to the original "make-account" procedure:
