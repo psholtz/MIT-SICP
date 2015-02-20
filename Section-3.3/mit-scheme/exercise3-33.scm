ac2f869362c98e52147b83f35a197296bc6659a7
Paul Sholtz <paul.sholtz@gmail.com>
2015-02-19 23:39:29 -0800

    updating

--- Section-3.3/mit-scheme/exercise3-33.scm
+++ Section-3.3/mit-scheme/exercise3-33.scm
@@ -96,3 +96,73 @@
 (define (connect connector new-constraint)
   ((connector 'connect) new-constraint))
 
+(define (inform-about-value constraint)
+  (constraint 'I-have-a-value))
+(define (inform-about-not-value constraint)
+  (constraint 'I-lost-my-value))
+
+(define (make-connector)
+  (let ((value false) 
+	(informant false)
+	(constraints '()))
+    (define (set-my-value newval setter)
+      (cond ((not (has-value? me))
+	     (set! value newval)
+	     (set! informant setter)
+	     (for-each-except setter
+			      inform-about-value
+			      constraints))
+	    ((not (= value newval))
+	     (error "Contradiction" (list value newval)))
+	    (else 
+	     'ignored)))
+
+    (define (forget-my-value retractor)
+      (if (eq? retractor informant)
+	  (begin (set! informant false)
+		 (for-each-except retractor
+				  inform-about-no-value
+				  constraints))
+	  'ignored))
+
+    (define (connect new-constraint)
+      (if (not (memq new-constraint constraints))
+	  (set! constraints
+		(cons new-constraint constraints)))
+      (if (has-value? me)
+	  (inform-about-value new-constraints))
+      'done)
+    (define (me request)
+      (cond ((eq? request 'has-value?)
+	     (if informant true false))
+	    ((eq? request 'value) value)
+	    ((eq? request 'set-value!) set-my-value)
+	    ((eq? request 'forget) forget-my-value)
+	    ((eq? request 'connect) connect)
+	    (else
+	     (error "Unknown operation -- CONNECTOR" request))))
+    me))
+
+(define (for-each-except exception procedure list)
+  (define (loop items)
+    (cond ((null? items) 'done)
+	  ((eq? (car items) exception) (loop (cdr items)))
+	  (else
+	   (procedure (car items))
+	   (loop (cdr items)))))
+  (loop list))
+
+;;
+;; Define the average network:
+;;
+(define a (make-connector))
+(define b (make-connector))
+(define c (make-connector))
+
+(define (averager a b c)
+  (let ((x (make-connector))
+	(y (make-connector)))
+    (adder a b x)
+    (multiplier x y c)
+    (constant 0.5 y)
+    'ok))
