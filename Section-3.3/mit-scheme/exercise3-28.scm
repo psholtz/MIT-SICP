76ecbf772a56ff6363fcb667c4c25446961f6d27
Paul Sholtz <psholtz@beatsmusic.com>
2015-03-09 17:50:07 -0700

    updating scheme

--- Section-3.3/mit-scheme/exercise3-28.scm
+++ Section-3.3/mit-scheme/exercise3-28.scm
@@ -62,9 +62,129 @@
 ;; ==> Invalid signal 2 2
 
 ;;
-;; Cannot unit test the or-gate yet, since we do not have definitions for:
-;; 
-;;  add-action!
-;;  set-signal!
-;;  after-delay
+;; To unit test the procedure, let's import the "make-wire" procedure from
+;; the text, along with its corresponding accessors and mutators:
 ;;
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+	  (begin (set! signal-value new-value)
+		 (call-each action-procedures))
+	  'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+	    ((eq? m 'set-signal!) set-my-signal!)
+	    ((eq? m 'add-action!) accept-action-procedure!)
+	    (else
+	     (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+	((car procedures))
+	(call-each (cdr procedures)))))
+
+;;
+;; Accessors and mutators for "make-wire":
+;;
+(define (get-signal wire)
+  (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+;;
+;; Define "after-delay" procedure:
+;;
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+		  action
+		  the-agenda))
+
+;;
+;; The supporting "agenda" data structure. 
+;;
+;; An agenda is made up of time segments. Each time segment is a pair 
+;; consisting of a number (the time) and a queue that holds the 
+;; procedures that are scheduled to be run during that time segment.
+;;
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) (car (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+;;
+;; (desc of sorting table)
+;;
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+	(< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+	(insert-queue! (segment-queue (car segments))
+		       action)
+	(let ((rest (cdr segments)))
+	  (if (belongs-before? rest)
+	      (set-cdr! 
+	       segments
+	       (cons (make-new-time-segment time action)
+		     (cdr segments)))
+	      (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+	(set-segments!
+	 agenda
+	 (cons (make-new-time-segment time action)
+	       segments))
+	(add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+	(set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+	(set-current-time! agenda (segment-time first-seg))
+	(front-queue (segment-queue first-seg)))))
+
+
+
+;;
+;; Finally, the "propagate" procedure which actually runs the simulation:
+;;
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+	(first-item)
+	(remove-first-agenda-item! the-agenda)
+	(propagate))))
