7ec497f8a41c864ae462b760446da981e05eb84c
Paul Sholtz <psholtz@beatsmusic.com>
2015-03-09 22:31:34 -0700

    updating scheme

--- Section-3.3/mit-scheme/exercise3-28.scm
+++ Section-3.3/mit-scheme/exercise3-28.scm
@@ -149,6 +149,44 @@
   (set-cdr! agenda segments))
 
 ;;
+;; Constructors, selectors and accessors for queue data structure:
+;;
+(define (make-queue)
+  (cons '() '()))
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+	   (set-front-ptr! queue new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue)
+	  (else
+	   (set-cdr! (rear-ptr queue) new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue))))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+	 (error "DELETE! called with an empty queue" queue))
+	(else
+	 (set-front-ptr! queue (cdr (front-ptr queue)))
+	 queue)))
+
+(define (front-ptr queue)
+  (car queue))
+(define (rear-ptr queue)
+  (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+
+;;
 ;; (desc of sorting table)
 ;;
 (define (add-to-agenda! time action agenda)
