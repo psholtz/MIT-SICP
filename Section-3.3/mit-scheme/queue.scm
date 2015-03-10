68675e34e587fe87ffd3693f7ec5ac8ae1f7ee2c
Paul Sholtz <psholtz@beatsmusic.com>
2015-03-10 16:48:12 -0700

    updating scheme

--- Section-3.3/mit-scheme/queue.scm
+++ Section-3.3/mit-scheme/queue.scm
@@ -1,6 +1,8 @@
 ;;
-;; Supporting procedures for queue operations:
+;; Queue is specified as a pair: 
 ;;
+(define (make-queue)
+  (cons '() '()))
 (define (front-ptr queue)
   (car queue))
 (define (rear-ptr queue)
@@ -11,16 +13,18 @@
   (set-cdr! queue item))
 
 ;;
-;; Queue API:
+;; Supporting Procedures:
 ;;
-(define (make-queue)
-  (cons '() '()))
 (define (empty-queue? queue)
   (null? (front-ptr queue)))
 (define (front-queue queue)
   (if (empty-queue? queue)
       (error "FRONT called with an empty queue" queue)
       (car (front-ptr queue))))
+
+;;
+;; Queue Insert/Delete Procedures:
+;;
 (define (insert-queue! queue item)
   (let ((new-pair (cons item '())))
     (cond ((empty-queue? queue)
@@ -31,6 +35,7 @@
 	   (set-cdr! (rear-ptr queue) new-pair)
 	   (set-rear-ptr! queue new-pair)
 	   queue))))
+
 (define (delete-queue! queue)
   (cond ((empty-queue? queue)
 	 (error "DELETE! called with an empty queue" queue))
