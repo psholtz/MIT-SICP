9701ddcfe3883edf4d81e7136ef7ba3aa4164c58
Paul Sholtz <psholtz@beatsmusic.com>
2015-03-11 17:41:56 -0700

    updating scheme

--- Section-3.3/mit-scheme/exercise3-21.scm
+++ Section-3.3/mit-scheme/exercise3-21.scm
@@ -5,6 +5,10 @@
 ;;
 
 ;;
+;; [explanation]
+;;
+
+;;
 ;; Import the queue module:
 ;;
 (load "queue.scm")
@@ -16,3 +20,25 @@
   (newline)
   (display (front-ptr queue)))
 			 
+;;
+;; Instruction sequence used by Ben Bitdiddle:
+;;
+(define q1 (make-queue))
+(print-queue q1)
+;; ==> ()
+
+(insert-queue! q1 'a)
+(print-queue q1) 
+;; ==> (a)
+
+(insert-queue! q1 'b)
+(print-queue q1)
+;; ==> (a b)
+
+(delete-queue! q1)
+(print-queue q1)
+;; ==> (b)
+
+(delete-queue! q1)
+(print-queue q1)
+;; ==> ()
